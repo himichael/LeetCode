7e49d6395f32b9ab9b8ec72b18d1e9486eb665a3
michael <michaelwangg@qq.com>
2019-05-08 14:06:50 +0800

    增加代码

---
+++ src/0015-3Sum/3Sum.py
@@ -0,0 +1,14 @@
+﻿class Solution(object):
+	def threeSum(self, nums):
+		if(len(nums) < 3):
+			return []
+		nums.sort()
+		res = set()
+		for i,v in enumerate(nums):
+			d = {}
+			for x in nums[i+1:]:
+				if(x not in d):
+					d[-v-x] = 1
+				else:
+					res.add((v,x,-v-x))
+		return map(list,res)
