1182f13e4e6c06fd433b604ab1580ca9d854ad8c
michaelwng <michaelwangg@qq.com>
2019-05-08 22:21:11 +0800

    。。

---
+++ src/977_Squares_of_a_Sorted_Array/Squares_of_a_Sorted_Array.py
@@ -0,0 +1,9 @@
+﻿class Solution(object):
+    def sortedSquares(self, A):
+        """
+        :type A: List[int]
+        :rtype: List[int]
+        """
+        arr = map(lambda x:x*x,A)
+        arr.sort()
+        return arr
