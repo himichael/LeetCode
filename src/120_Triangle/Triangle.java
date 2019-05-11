a93924cfb2ecadaafc6ce91f6df65ea79dd476ba
michaelwng <michaelwangg@qq.com>
2019-05-11 15:01:00 +0800

    。。

---
+++ src/120_Triangle/Triangle.java
@@ -0,0 +1,17 @@
+﻿class Solution {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        if(triangle==null || triangle.size()==0) {
+			return 0;
+		}
+		int size = triangle.get(triangle.size()-1).size();
+		int[] arr = new int[size+1];
+		
+		for(int i=triangle.size()-1;i>=0;i--) {
+			List<Integer> list = triangle.get(i);
+			for(int j=0;j<list.size();j++) {
+				arr[j] = Math.min(arr[j],arr[j+1])+list.get(j);
+			}
+		}
+		return arr[0];
+    }
+}
