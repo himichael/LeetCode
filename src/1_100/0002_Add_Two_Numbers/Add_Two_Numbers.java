bc93daf9ce653e7bec6dbcac96cdc0fa9442da6d
michael <michaelwangg@qq.com>
2019-05-13 14:23:23 +0800

    增加注释

--- src/1_100/0002_Add_Two_Numbers/Add_Two_Numbers.java
+++ src/1_100/0002_Add_Two_Numbers/Add_Two_Numbers.java
@@ -11,6 +11,10 @@ class Solution {
         ListNode head = new ListNode(-1);
         ListNode p = head;
         boolean isPlus = false;
+		
+		//遍历两个链表，同时相加a，b两个链表的val
+		//如果val>9，说明要进位，记录进位标志，下一次迭代的时候计算
+		//a.val+b.val+1 这里的1是进位过来的，如果这个数还是>9继续进位，否则直接记录下来即可
         while(a!=null && b!=null) {
             int tmp = a.val+b.val;
             if(isPlus) {
@@ -37,6 +41,8 @@ class Solution {
             b = b.next;
         }
 
+		//如果两个链表遍历完了，但是a或者b还没结束，则将a或b后面的值加入到合并的链表中
+		//注意这里也需要考虑进位问题
         if(a!=null) {
             while(a!=null) {
                 int tmp = a.val;
@@ -90,6 +96,8 @@ class Solution {
                 b = b.next;
             }
         }
+		
+		//如果a，b一样长，都遍历完了，但是有进位情况，就再增加一个Node，node的值记为1即可
         if(isPlus) {
             ListNode node = new ListNode(1);
             p.next = node;
