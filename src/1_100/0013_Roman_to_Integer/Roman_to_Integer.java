0f6759c5f671161a7557cd3831eb09c6fa5ca55e
michael <michaelwangg@qq.com>
2019-05-17 14:44:01 +0800

    罗马数字转整数

---
+++ src/1_100/0013_Roman_to_Integer/Roman_to_Integer.java
@@ -0,0 +1,71 @@
+﻿class Solution {
+    public int romanToInt(String s) {
+    	StringBuilder sb = new StringBuilder();
+    	int num = 0;
+    	int len = s.length();
+		
+		//遍历每个字符，然后看对应的是哪个罗马字母，如果是C,X,I，还要再取下一个字符比较
+    	for(int i=0;i<len;i++) {
+    		char c = s.charAt(i);
+    		switch(c) {
+    			case 'M':
+    				num += 1000;
+    				break;
+    			case 'C':
+    				if(i<len-1 && s.charAt(i+1)=='M') {
+    					num += 900;
+    					i++;
+    					break;
+    				}
+    				if(i<len-1 && s.charAt(i+1)=='D') {
+    					num += 400;
+    					i++;
+    					break;
+    				}
+    				num += 100;
+    				break;
+    			case 'D':
+    				num += 500;
+    				break;
+    			case 'X':
+    				if(i<len-1 && s.charAt(i+1)=='C') {
+    					num += 90;
+    					i++;
+    					break;
+    				}
+    				if(i<len-1 && s.charAt(i+1)=='L') {
+    					num += 40;
+    					i++;
+    					break;
+    				}
+    				num += 10;
+    				break;
+    			case 'L':
+    				num += 50;
+    				break;
+    			case 'I':
+    				if(i<len-1 && s.charAt(i+1)=='X') {
+    					num += 9;
+    					i++;
+    					break;
+    				}
+    				if(i<len-1 && s.charAt(i+1)=='V') {
+    					num += 4;
+    					i++;
+    					break;
+    				}
+    				num += 1;
+    				break;
+    			case 'V':
+    				num += 5;
+    				break;
+    			default:
+    				break;
+    				
+    		}
+    				
+    	}
+    	
+    	return num;
+    }
+}
