c8486405ffda86e0ddef8ce26866e2eb1ccfaa4f
michael <michaelwangg@qq.com>
2019-05-22 13:30:04 +0800

    增加python的实现

---
+++ src/1_100/0031_Next_Permutation/Next_Permutation.py
@@ -0,0 +1,24 @@
+﻿class Solution(object):
+	def nextPermutation(self, nums):
+		"""
+		:type nums: List[int]
+		:rtype: None Do not return anything, modify nums in-place instead.
+		"""
+		if(nums==None or len(nums)==0 or len(nums)==1):
+			return
+			
+		i = len(nums)-2
+		while(i>=0 and nums[i+1]<=nums[i]):
+			i -= 1
+		if(i >= 0):
+			j = len(nums)-1
+			while(j>0 and nums[j]<=nums[i]):
+				j -= 1
+			nums[i],nums[j] = nums[j],nums[i]
+		#reverse
+		p = i+1
+		q = len(nums)-1
+		while(p < q):
+			nums[p],nums[q] = nums[q],nums[p]
+			p += 1
+			q -= 1
