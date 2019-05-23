0fe61fc320d343f03ddfb8f3cb48cbb0abcaf328
michael <michaelwangg@qq.com>
2019-05-23 19:42:45 +0800

    合并k个排序链表

---
+++ src/1_100/0023_Merge_k_Sorted_Lists/Merge_k_Sorted_Lists.java
@@ -0,0 +1,32 @@
+﻿/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        PriorityQueue<ListNode> queue = new PriorityQueue(new Comparator<ListNode>() {
+            public int compare(ListNode o1, ListNode o2) {
+                return (o1.val - o2.val);
+            }
+        });
+        for(int i=0;i<lists.length;i++) {
+            while(lists[i] != null) {
+                queue.add(lists[i]);
+                lists[i] = lists[i].next;
+            }
+        }
+        ListNode dummy = new ListNode(-1);
+        ListNode head = dummy;
+        while( !queue.isEmpty() ) {
+            ListNode node = queue.poll();
+            dummy.next = node;
+            dummy = dummy.next;
+            dummy.next = null;
+        }
+        return head.next;
+    }
+}
