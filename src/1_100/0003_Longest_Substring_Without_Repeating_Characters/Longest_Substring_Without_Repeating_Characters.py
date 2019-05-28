5a47647e1c4266caaaa057b206bf4676c4c86e08
michael <michaelwangg@qq.com>
2019-05-28 18:59:25 +0800

    无重复字符的最长子串

---
+++ src/1_100/0003_Longest_Substring_Without_Repeating_Characters/Longest_Substring_Without_Repeating_Characters.py
@@ -0,0 +1,19 @@
+﻿class Solution(object):
+	def lengthOfLongestSubstring(self, s):
+		"""
+		:type s: str
+		:rtype: int
+		"""
+		res,i,j = 0,0,0
+		letter_set = set()
+		size = len(s)
+		# 这里使用滑动窗口模式 [i..j] 窗口内的字符，就是最长字串
+		while(i<size and j<size):
+			if(s[j] not in letter_set):
+				letter_set.add(s[j])
+				j += 1
+				res = max(res,j-i)
+			else:
+				letter_set.remove(s[i])
+				i += 1
+		return res
