6907a52d1c28d66727034070d76d3e1e318e107c
michael <michaelwangg@qq.com>
2019-05-30 11:19:30 +0800

    生产括号

---
+++ src/1_100/0022_Generate_Parentheses/Generate_Parentheses.py
@@ -0,0 +1,19 @@
+﻿class Solution(object):
+    def generateParenthesis(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        if(n == 0):
+            return []
+        res = []
+        def recursion(left,right,content):
+            if(left==n and right==n):
+                res.append(content)
+                return
+            if(left>n or right>n or left<right):
+                return
+            recursion(left+1, right, content+"(")
+            recursion(left, right+1, content+")")
+        recursion(0, 0, "")
+        return res
