5aa21c8d4135f68cc1cb8ec96b3b738856a207c6
michaelwng <michaelwangg@qq.com>
2019-05-31 00:03:37 +0800

    二叉树的后序遍历

---
+++ src/101_200/0145_Binary_Tree_Postorder_Traversal/Binary_Tree_Postorder_Traversal.py
@@ -0,0 +1,23 @@
+﻿# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def postorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        res = []
+        def recursion(root):
+            if(root == None):
+                return
+            recursion(root.left)
+            recursion(root.right)
+            res.append(root.val)
+        recursion(root)
+        return res
+        
