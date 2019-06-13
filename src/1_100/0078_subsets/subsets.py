b83a59f6e31befecdfcd661ad3653d448fbf89cc
michael <michaelwangg@qq.com>
2019-06-13 21:31:44 +0800

    子集

---
+++ src/1_100/0078_subsets/subsets.py
@@ -0,0 +1,15 @@
+﻿class Solution(object):
+    def subsets(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        res = []
+        def recursive(i,tmp):
+            res.append(tmp)
+            for j in range(i,len(nums)):
+                x = list(tmp)
+                x.append(nums[j])
+                recursive(j+1, x)
+        recursive(0,[])
+        return res
