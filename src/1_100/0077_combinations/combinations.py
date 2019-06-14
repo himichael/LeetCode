be13a90feae293aa3e2634331b45782e9d3df0a3
michael <michaelwangg@qq.com>
2019-06-14 13:55:15 +0800

    组合

---
+++ src/1_100/0077_combinations/combinations.py
@@ -0,0 +1,23 @@
+﻿class Solution(object):
+    def combine(self, n, k):
+        """
+        :type n: int
+        :type k: int
+        :rtype: List[List[int]]
+        """
+        arr = []
+        for i in range(1,n+1):
+            arr.append(i)
+        res = []
+        
+        def recursive(i,tmp):
+            if(len(tmp)==k):
+                res.append(list(tmp))
+                return
+            
+            for j in range(i,n):
+                tmp.append(arr[j])
+                recursive(j+1,tmp)
+                tmp.pop()
+        recursive(0, [])
+        return res
