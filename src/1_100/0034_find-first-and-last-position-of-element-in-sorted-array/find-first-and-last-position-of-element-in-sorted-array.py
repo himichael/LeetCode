3d1a44838b9a6903c65b447c747a3c321aad4c5a
michael <michaelwangg@qq.com>
2019-06-24 20:53:24 +0800

    在排序数组中查找元素的第一个和最后一个位置

---
+++ src/1_100/0034_find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
@@ -0,0 +1,41 @@
+﻿class Solution(object):
+    def searchRange(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        if(nums==0 or len(nums)==0):
+            return [-1,-1]
+        def findFirst():
+            start = 0
+            end = len(nums)-1
+            while(start <= end):
+                mid = (start+end)/2
+                if(nums[mid]>target):
+                    end = mid-1
+                elif(nums[mid]<target):
+                    start = mid+1
+                else:
+                    if(mid==0 or nums[mid-1]!=target):
+                        return mid
+                    else:
+                        end = mid-1
+            return -1
+        
+        def findLast():
+            start = 0
+            end = len(nums)-1
+            while(start <= end):
+                mid = (start+end)/2
+                if(nums[mid] > target):
+                    end = mid-1
+                elif(nums[mid]<target):
+                    start = mid+1
+                else:
+                    if(mid==len(nums)-1 or nums[mid+1]!=target):
+                        return mid
+                    else:
+                        start = mid+1
+            return -1
+        return [findFirst(), findLast() ]
