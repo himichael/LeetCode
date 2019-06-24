8ffc1ad2d133445b4c1362fd8cd5c421c44085da
michael <michaelwangg@qq.com>
2019-06-24 17:06:07 +0800

    接雨水

---
+++ src/1_100/0042_trapping-rain-water/Rotate_Image.py
@@ -0,0 +1,24 @@
+﻿class Solution(object):
+    def trap(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if(nums==None or len(nums)==0):
+            return 0
+        left = 0
+        right = len(nums)-1
+        res = 0
+        while(left < right):
+            cur_min = min(nums[left],nums[right])
+            if(cur_min == nums[left]):
+                left += 1
+                while(left<right and nums[left]<=cur_min):
+                    res += cur_min - nums[left]
+                    left += 1
+            else:
+                right -= 1
+                while(left<right and nums[right]<=cur_min):
+                    res += cur_min - nums[right]
+                    right -= 1
+        return res
