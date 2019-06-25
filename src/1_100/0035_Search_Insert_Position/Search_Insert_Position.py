5fe5821de572b99fa4e461b536b052682f0d4b45
michaelwng <michaelwangg@qq.com>
2019-06-25 21:51:19 +0800

    新的实现方式

--- src/1_100/0035_Search_Insert_Position/Search_Insert_Position.py
+++ src/1_100/0035_Search_Insert_Position/Search_Insert_Position.py
@@ -11,3 +11,23 @@
             nums.append(target)
             nums.sort()
             return nums.index(target)
+			
+	#新的实现方式		
+    def searchInsert_2(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        start = 0
+        end = len(nums)-1
+        while(start <= end):
+            mid = (start+end)/2
+            if(nums[mid] > target):
+                end = mid-1
+            elif(nums[mid] < target):
+                start = mid+1
+            else:
+                return mid
+        return start
+    
