0cbf302e85b79e58407bb57bc998dd2bfb0f3fb4
michael <michaelwangg@qq.com>
2019-07-02 17:09:20 +0800

    合并两个有序链表重新实现

--- src/1_100/0021_Merge_Two_Sorted_Lists/Merge_Two_Sorted_Lists.py
+++ src/1_100/0021_Merge_Two_Sorted_Lists/Merge_Two_Sorted_Lists.py
@@ -1,39 +1,33 @@
-﻿class Solution(object):
-# Definition for singly-linked list.
+﻿# Definition for singly-linked list.
 # class ListNode(object):
 #     def __init__(self, x):
 #         self.val = x
 #         self.next = None
 
 class Solution(object):
-    def mergeTwoLists(self, a1, a2):
-        """
-        :type l1: ListNode
-        :type l2: ListNode
-        :rtype: ListNode
-        """
-        if a1==None and a2==None:
-            return []
-        elif a1==None:
-            return a2
-        elif a2==None:
-            return a1
-        s=[]
-        index=0
-        while a1!=None and a2!=None:
-            if a1.val <= a2.val:
-                s.append(a1.val)
-                a1=a1.next
-            else:
-                s.append(a2.val)
-                a2=a2.next
+	def mergeTwoLists(self, a1, a2):
+		"""
+		:type l1: ListNode
+		:type l2: ListNode
+		:rtype: ListNode
+		"""
+		if(a1==None):
+			return a2
+		if(a2==None):
+			return a1
+		head = ListNode(-1)
+		p = head
+		while(a1!=None and a2!=None):
+			if(a1.val < a2.val):
+				p.next = a1
+				a1 = a1.next
+			else:
+				p.next = a2
+				a2 = a2.next
+			p = p.next
+		if(a1!=None):
+			p.next = a1
+		else:
+			p.next = a2
+		return head.next
         
-        if a1!=None:
-            while a1:
-                s.append(a1.val)
-                a1=a1.next
-        if a2!=None:
-            while a2:
-                s.append(a2.val)
-                a2=a2.next
-        return s
