03203f37237a62ce3dcf81e5b25bf1292623910b
michaelwng <michaelwangg@qq.com>
2019-07-06 12:08:02 +0800

    增加一道自定义的题目，打印三角形

---
+++ src/other/print_triangle/print_triangle.py
@@ -0,0 +1,23 @@
+﻿class Solution(object):
+    def print_triangle(self,n):
+        index = 1
+        content = ""
+        res = []
+        for i in range(n):
+            for j in range(i+1):
+               content = content+str(index)+"*"
+               index += 1 
+            content = content[:len(content)-1]
+            res.append(content)
+            #print content
+            content = ""
+        index -= 1
+        for i in range(n):
+            for j in range(i,n):
+                content = "*" + str(index) + content
+                index -= 1
+            content = content[1:len(content)]
+            #print content
+            res.append(content)
+            content = ""   
+        return res
