a5fd9569cb8baa72b70b331cba28fc7d3496d4a7
michaelwng <michaelwangg@qq.com>
2019-07-12 22:47:48 +0800

    回文链表的另一种解法

--- src/201_300/0234_Palindrome_Linked_List/Palindrome_Linked_List.py
+++ src/201_300/0234_Palindrome_Linked_List/Palindrome_Linked_List.py
@@ -26,3 +26,36 @@ class Solution(object):
             if a[i] != b[i]:
                 return False
         return True
+		
+    def isPalindrome_2(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        if(head==None or head.next==None):
+            return True
+        p = ListNode(-1)
+        p.next = head
+        low = p
+        fast = p
+        while(fast!=None and fast.next!=None):
+            low = low.next
+            fast = fast.next.next
+            
+        second = low.next
+        low.next = None 
+        #reverse
+        second_cur = second
+        second_pre = None
+        while(second_cur!=None):
+            tmp = second_cur.next
+            second_cur.next = second_pre
+            second_pre = second_cur
+            second_cur = tmp
+        #comp second_pre head
+        while(second_pre != None):
+            if(second_pre.val != head.val):
+                return False    
+            second_pre = second_pre.next
+            head = head.next
+        return True		
