bc1f1218b071db72d7236a2ca0fe4d5b73935e51
michaelwng <michaelwangg@qq.com>
2019-07-12 22:19:20 +0800

    最长有效括号

---
+++ src/1_100/0032_longest-valid-parentheses/longest-valid-parentheses.py
@@ -0,0 +1,20 @@
+﻿class Solution(object):
+    def longestValidParentheses(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        if(s==None or len(s)==0):
+            return 0
+        stack = [-1]
+        res = 0
+        for i in range(len(s)):
+            if(s[i] == '('):
+                stack.append(i)
+            else:
+                stack.pop()
+                if(not stack):
+                    stack.append(i)
+                else:
+                    res = max(i-stack[len(stack)-1], res)
+        return res
