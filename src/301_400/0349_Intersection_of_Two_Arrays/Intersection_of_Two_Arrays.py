0b2a10ac66595e2fe66c37e71198310091695b4a
michaelwng <michaelwangg@qq.com>
2019-07-13 17:02:21 +0800

    两个数的交集，另一种实现方式

--- src/301_400/0349_Intersection_of_Two_Arrays/Intersection_of_Two_Arrays.py
+++ src/301_400/0349_Intersection_of_Two_Arrays/Intersection_of_Two_Arrays.py
@@ -6,3 +6,20 @@
         :rtype: List[int]
         """
         return list(set(nums1).intersection(set(nums2)))
+
+    def intersection_2(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        if(nums1==None or nums2==None):
+            return []
+        s = set()
+        res = set()
+        for i in nums1:
+            s.add(i)
+        for j in nums2:
+            if(j in s):
+                res.add(j)
+        return list(res)
