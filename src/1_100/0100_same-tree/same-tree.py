9971f0c6c4456ef6eb95f6dc6c8e8fac6f473c7b
michaelwng <michaelwangg@qq.com>
2019-07-13 17:37:45 +0800

    相同的树，递归改为内部函数方式

--- src/1_100/0100_same-tree/same-tree.py
+++ src/1_100/0100_same-tree/same-tree.py
@@ -23,5 +23,26 @@ class Solution(object):
         if( not self.isSameTree(p.right, q.right) ):
             return False
         return True
+		
+		
+    def isSameTree_2(self, p, q):
+        """
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: bool
+        """
+        def recursion(p,q):
+            if(p==None and q==None):
+                return True
+            if( (p==None and q!=None) or (p!=None and q==None) ):
+                return False
+            if(p.val != q.val):
+                return False
+            if( not recursion(p.left, q.left) ):
+                return False
+            if( not recursion(p.right, q.right) ):
+                return False
+            return True
+        return recursion(p,q)		
     
         
