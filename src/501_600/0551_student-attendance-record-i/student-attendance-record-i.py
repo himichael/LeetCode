cee2b8865fe5659e4ef213230873fcf84ebc5434
michael <michaelwangg@qq.com>
2019-07-15 19:30:31 +0800

    学生出勤记录 I

---
+++ src/501_600/0551_student-attendance-record-i/student-attendance-record-i.py
@@ -0,0 +1,17 @@
+﻿class Solution(object):
+    def checkRecord(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        if(s==None or len(s)==0):
+            return True
+        absent = 0
+        if(s.find("LLL")>-1):
+            return False
+        for i in s:
+            if(i=='A'):
+                absent += 1
+                if(absent >= 2):
+                    return False
+        return True
