98cb56e82908ee8c144c914d845550773979dace
michael <michaelwangg@qq.com>
2019-07-19 11:16:03 +0800

    2的冥

---
+++ src/201_300/0231_power-of-two/power-of-two.py
@@ -0,0 +1,14 @@
+﻿class Solution(object):
+	def isPowerOfTwo(self, n):
+		"""
+		:type n: int
+		:rtype: bool
+		"""
+		if n==1:
+			return True
+		while(n > 1):
+			n = n/2.0
+		if n == 1.0 :
+			return True
+		else:
+			return False
