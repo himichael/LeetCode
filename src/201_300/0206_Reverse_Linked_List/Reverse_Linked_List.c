719f838362e0e2fab83f6a9fee50b745e1636b4a
michaelwng <michaelwangg@qq.com>
2019-07-20 22:01:34 +0800

    反转链表 c语言实现

---
+++ src/201_300/0206_Reverse_Linked_List/Reverse_Linked_List.c
@@ -0,0 +1,19 @@
+﻿/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+
+struct ListNode* reverseList(struct ListNode* head){
+	if(head==NULL || head->next==NULL) {
+		return head;
+	}
+	struct ListNode *cur = reverseList(head->next);
+	head->next->next = head;
+	head->next = NULL;
+	return cur;
+}
+
