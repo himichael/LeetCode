a5ca20e02cd191faa4eafcded9b53bc2ea148039
michaelwng <michaelwangg@qq.com>
2019-07-20 18:31:16 +0800

    搜索旋转排序数组，重新实现了一遍

--- src/1_100/0033_search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ src/1_100/0033_search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -7,24 +7,24 @@
         """
         if(nums==None or len(nums)==0):
             return -1
-        if(len(nums)==1):
-            if(nums[0]==target):
-                return 0
-            else:
-                return -1
-        def find_rotate_index(begin,end):
-            if(nums[begin] < nums[end]):
-                return 0
-            while(begin <= end):
-                pivot = (begin+end)/2
-                if(nums[pivot]>nums[pivot+1]):
-                    return pivot+1
-                else:
-                    if(nums[pivot]<nums[begin]):
-                        end = pivot-1
-                    else:
-                        begin = pivot+1
+        if(len(nums) == 1):
+            return 0 if(nums[0]==target) else -1
 
+        def findMin():
+            begin = 0
+            end = len(nums)-1
+            if(nums[begin] < nums[end]):
+                return begin
+            while(begin <= end):
+                mid = (begin+end)/2
+                if(nums[mid] > nums[mid+1]):
+                    return mid+1
+                else:
+                    if(nums[mid] < nums[begin]):
+                        end = mid-1
+                    else:
+                        begin = mid+1
+        
         def binary_search(begin,end):
             while(begin <= end):
                 mid = (begin+end)/2
@@ -35,12 +35,14 @@
                 else:
                     return mid
             return -1
-        
-        index = find_rotate_index(0, len(nums)-1)
-        if(nums[index] == target):
-            return index
-        if(index == 0):
+
+        min_num_pos = findMin()
+        print "min->" + str(min_num_pos)
+        if(nums[min_num_pos]==target):
+            return min_num_pos
+        if(min_num_pos == 0):
             return binary_search(0,len(nums)-1)
         if(target < nums[0]):
-            return binary_search(index,len(nums)-1)
-        return binary_search(0,index)
+            return binary_search(min_num_pos,len(nums)-1)
+        else:
+            return binary_search(0,min_num_pos)
