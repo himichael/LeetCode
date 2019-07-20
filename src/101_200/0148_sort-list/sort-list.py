2ad7e03533ecc1ee10bfc4c84b99bc3b20df18ed
michaelwng <michaelwangg@qq.com>
2019-07-20 16:46:09 +0800

    排序链表

---
+++ src/101_200/0148_sort-list/sort-list.py
@@ -0,0 +1,43 @@
+﻿# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def sortList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        def split_merge(p):
+            if(p==None or p.next==None):
+                return p
+            h = ListNode(-1)
+            h.next = p
+            first = h
+            second = h
+            while(second!=None and second.next!=None):
+                first = first.next
+                second = second.next.next
+            a = h.next
+            b = first.next
+            first.next = None
+            split_a = split_merge(a)
+            split_b = split_merge(b)
+            return merge(split_a,split_b)
+            
+        
+        def merge(pa,pb):
+            if pa==None:
+                return pb
+            if pb==None:
+                return pa
+            if(pa.val < pb.val):
+                pa.next = merge(pa.next,pb)
+                return pa
+            else:
+                pb.next = merge(pa,pb.next)
+                return pb
+        
+        return split_merge(head)
