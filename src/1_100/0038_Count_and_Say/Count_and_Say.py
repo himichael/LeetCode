6e94308511c3fd57cd54b20ec9d5e50b81565e53
michaelwng <michaelwangg@qq.com>
2019-07-20 22:24:55 +0800

    重构 报数

--- src/1_100/0038_Count_and_Say/Count_and_Say.py
+++ src/1_100/0038_Count_and_Say/Count_and_Say.py
@@ -4,10 +4,9 @@
         :type n: int
         :rtype: str    
         """
-        if(n==1): return "1"
         #解析n-1的情况，然后拼接成一个字符串再递归调用n，最后返回
         def recursion(num,res):
-            if(num == n-1):
+            if(num == n):
                 return res
             i,j = 0,1
             size = len(res)
@@ -15,8 +14,8 @@
             while(i < size):
                 while(j<size and res[i]==res[j]):
                     j += 1
-                x = j - i
-                tmp = tmp + str(x) + str(res[i])
+                x = j-i
+                tmp = tmp + str(x) + res[i]
                 i = j
             return recursion(num+1,tmp)
-        return recursion(0,"1") 
+        return recursion(1,"1")
