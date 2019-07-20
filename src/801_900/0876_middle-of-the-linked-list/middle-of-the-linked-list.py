5e269cb012350077a7d3deedcc82e78c3190e9e4
michaelwng <michaelwangg@qq.com>
2019-07-20 15:24:52 +0800

    链表的中间结点

---
+++ src/801_900/0876_middle-of-the-linked-list/middle-of-the-linked-list.py
@@ -0,0 +1,24 @@
+﻿# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def middleNode(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if(head==None or head.next==None):
+            return head
+        p = ListNode(-1)
+        p.next = head
+        first = p
+        second = p
+        while(second!=None and second.next!=None):
+            first = first.next
+            second = second.next.next
+        if(second != None):
+            return first.next
+        return first
