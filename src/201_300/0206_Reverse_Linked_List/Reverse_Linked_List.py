8e069992e05cc8e93989c48d6972a94f8f49adc7
michaelwng <michaelwangg@qq.com>
2019-07-21 18:02:04 +0800

    反转链表的递归解法

--- src/201_300/0206_Reverse_Linked_List/Reverse_Linked_List.py
+++ src/201_300/0206_Reverse_Linked_List/Reverse_Linked_List.py
@@ -18,3 +18,16 @@ class Solution(object):
             cur = tmp
             #cur.next, pre, cur = pre, cur, cur.next
         return pre
+		
+	#递归解法	
+    def reverseList_2(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if(head==None and head.next==None):
+            return head
+        cur = self.reverseList(head.next)
+        head.next.next = head
+        head.next = None
+        return cur		
