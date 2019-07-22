aa3f6e9684700f84ab4f50d5885fcd0fc468e885
michaelwng <michaelwangg@qq.com>
2019-07-22 21:57:34 +0800

    两两交换链表递归解法

---
+++ src/1_100/0024_Swap_Nodes_in_Pairs/Swap_Nodes_in_Pairs.py
@@ -0,0 +1,18 @@
+﻿# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def swapPairs(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if(head==None or head.next==None):
+            return head
+        next_node = head.next
+        head.next = self.swapPairs(next_node.next)
+        next_node.next = head
+        return next_node
