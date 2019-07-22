eb3de0b67b0d76a72acd86927cad0bf988743939
michael <michaelwangg@qq.com>
2019-07-22 13:53:28 +0800

    sqrt的新实现方式

--- src/1_100/0069_Sqrt_x_/_Sqrt_x_.py
+++ src/1_100/0069_Sqrt_x_/_Sqrt_x_.py
@@ -1,8 +1,17 @@
 ﻿class Solution(object):
-    def mySqrt(self, x):
-        """
-        :type x: int
-        :rtype: int
-        """
-        import math
-        return int(math.sqrt(x))
+	def mySqrt(self, x):
+		"""
+		:type x: int
+		:rtype: int
+		"""
+		if(x == 0):
+			return 0
+		begin = 1
+		end = x/2
+		while(begin < end):
+			mid = (begin+end+1)/2
+			if(mid*mid > x):
+				end = mid-1
+			else:
+				begin = mid
+		return begin
