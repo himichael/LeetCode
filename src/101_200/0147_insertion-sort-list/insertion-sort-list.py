31162c12cf2152450e081ac7c52a9115ef8649b2
michael <michaelwangg@qq.com>
2019-07-23 12:03:18 +0800

    修改路径

---
+++ src/101_200/0147_insertion-sort-list/insertion-sort-list.py
@@ -0,0 +1,33 @@
+﻿# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def insertionSortList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        node = ListNode(-1)
+        cur = node
+        while(head != None):
+            tmp = head
+            head = head.next
+            tmp.next = None
+            cur = node
+            found = False
+            if(cur.next==None):
+                cur.next = tmp
+                continue
+            while(cur.next!=None):
+                if(cur.next.val >= tmp.val):
+                    tmp.next = cur.next
+                    cur.next = tmp
+                    found = True
+                    break
+                cur = cur.next
+            if(not found):
+                cur.next = tmp
+        return node.next
