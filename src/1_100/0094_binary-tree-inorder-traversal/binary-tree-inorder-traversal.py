2625da0e1af31bc0a4ee460bd1335d3e70641e89
michael <michaelwangg@qq.com>
2019-07-24 12:45:34 +0800

    二叉树的中序遍历

---
+++ src/1_100/0094_binary-tree-inorder-traversal/binary-tree-inorder-traversal.py
@@ -0,0 +1,22 @@
+﻿# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def inorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        res = []
+        def recursion(root):
+            if(root==None):
+                return
+            recursion(root.left)
+            res.append(root.val)
+            recursion(root.right)
+        recursion(root)
+        return res
