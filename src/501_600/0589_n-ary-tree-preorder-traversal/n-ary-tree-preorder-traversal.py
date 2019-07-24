b772f75bba0240a9e6ba5cf319be9f337e4ce9da
michael <michaelwangg@qq.com>
2019-07-24 12:39:07 +0800

    N叉树的前序遍历

---
+++ src/501_600/0589_n-ary-tree-preorder-traversal/n-ary-tree-preorder-traversal.py
@@ -0,0 +1,23 @@
+﻿"""
+# Definition for a Node.
+class Node(object):
+    def __init__(self, val, children):
+        self.val = val
+        self.children = children
+"""
+class Solution(object):
+    def preorder(self, root):
+        """
+        :type root: Node
+        :rtype: List[int]
+        """
+        res = []
+        def recursion(root):
+            if(root==None):
+                return
+            res.append(root.val)
+            childrens = root.children
+            for node in childrens:
+                recursion(node)
+        recursion(root)
+        return res
