29698de574ffc7f900fae65becb2b2674219df9d
michael <michaelwangg@qq.com>
2019-07-24 12:31:55 +0800

    N叉树的后序遍历

---
+++ src/501_600/0590_n-ary-tree-postorder-traversal/n-ary-tree-postorder-traversal.py
@@ -0,0 +1,25 @@
+﻿"""
+# Definition for a Node.
+class Node(object):
+    def __init__(self, val, children):
+        self.val = val
+        self.children = children
+"""
+class Solution(object):
+    def postorder(self, root):
+        """
+        :type root: Node
+        :rtype: List[int]
+        """
+        res = []
+        def recursion(root):
+            if(root==None):
+                return
+            chilrens = root.children
+            for node in chilrens:
+                recursion(node) 
+            res.append(root.val)
+        recursion(root)
+        return res
+    
+    
