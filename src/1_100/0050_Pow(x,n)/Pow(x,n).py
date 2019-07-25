20fb85d5a1a1ba04353008446fb2f0d955a6eefa
michael <michaelwangg@qq.com>
2019-07-25 10:14:03 +0800

    pow的分治解法

--- src/1_100/0050_Pow(x,n)/Pow(x,n).py
+++ src/1_100/0050_Pow(x,n)/Pow(x,n).py
@@ -6,3 +6,23 @@
         :rtype: float
         """
         return x**n
+
+    #分治的实现方式
+	def myPow_2(self, x, n):
+        """
+        :type x: float
+        :type n: int
+        :rtype: float
+        """
+        if(n == 0):
+            return 1.0
+        if(n == 1):
+            return x
+        if(n < 0):
+            n = -n
+            return 1 / self.myPow(x, n)
+        else:
+            if(n%2 ==0):
+                return self.myPow(x*x, n/2)
+            else:
+                return x * self.myPow(x, n-1)
