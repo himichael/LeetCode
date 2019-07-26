db15305843a550e5c4ed8020154e394f2c5014a3
michael <michaelwangg@qq.com>
2019-07-26 13:59:44 +0800

    求众数的hash实现方式优化

--- src/101_200/0169_Majority_Element/Majority_Element.py
+++ src/101_200/0169_Majority_Element/Majority_Element.py
@@ -23,3 +23,23 @@
         :rtype: int
         """
         return sorted(nums)[len(nums)/2]
+
+    #hash实现方式的优化
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if(len(nums)==1):
+            return nums[0]
+        m = {}
+        half = len(nums)/2
+        for i in range(len(nums)):
+            if(m.has_key(nums[i])):
+                m[nums[i]]+=1
+                if(m[nums[i]] > half):
+                    return nums[i]
+            else:
+                m[nums[i]] = 1
+        return -1
+        
