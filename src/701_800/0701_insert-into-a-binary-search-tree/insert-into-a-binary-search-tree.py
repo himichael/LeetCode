87e9368424a6b75ca6741e97a8dcfaa3ec67248f
michael <michaelwangg@qq.com>
2019-07-26 09:17:04 +0800

    二叉搜索树中的插入操作

---
+++ src/701_800/0701_insert-into-a-binary-search-tree/insert-into-a-binary-search-tree.py
@@ -0,0 +1,30 @@
+﻿# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+	def insertIntoBST(self, root, val):
+		"""
+		:type root: TreeNode
+		:type val: int
+		:rtype: TreeNode
+		"""
+		if(root==None):
+			root = TreeNode(val)
+			return root
+		def recursion(node):
+			if(node.val > val):
+				if(node.left != None):
+					recursion(node.left)
+				else:
+					node.left = TreeNode(val)
+			else:
+				if(node.right != None):
+					recursion(node.right)
+				else:
+					node.right = TreeNode(val)
+		recursion(root)
+		return root
