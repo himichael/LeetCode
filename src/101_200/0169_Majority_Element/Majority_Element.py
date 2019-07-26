c590ea098b3e36ac25aedbb8f4efd4c84b902689
michael <michaelwangg@qq.com>
2019-07-26 14:19:06 +0800

    求众数，摩尔投票算法

--- src/101_200/0169_Majority_Element/Majority_Element.py
+++ src/101_200/0169_Majority_Element/Majority_Element.py
@@ -42,4 +42,34 @@
             else:
                 m[nums[i]] = 1
         return -1
+
+    #摩尔投票算法    
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        candidate = nums[0]
+        count = 1
+        for i in range(1,len(nums)):
+            if(count == 0):
+                candidate = nums[i]
+                count = 1
+            elif(candidate == nums[i]):
+                count += 1
+            else:
+                count -= 1
+        return candidate
+
+        
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
         
