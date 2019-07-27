f1049cd23aa99390f138c7598317a5f1da7b42c5
michaelwng <michaelwangg@qq.com>
2019-07-27 16:25:34 +0800

    N叉树的层序遍历

---
+++ src/401_500/0429_n-ary-tree-level-order-traversal/n-ary-tree-level-order-traversal.py
@@ -0,0 +1,30 @@
+﻿"""
+# Definition for a Node.
+class Node(object):
+    def __init__(self, val, children):
+        self.val = val
+        self.children = children
+"""
+class Solution(object):
+    def levelOrder(self, root):
+        """
+        :type root: Node
+        :rtype: List[List[int]]
+        """
+        if(root==None):
+            return []
+        res = []
+        queue = []
+        queue.append(root)
+        while(len(queue) > 0):
+            size = len(queue)
+            tmp = []
+            for _ in range(size):
+                node = queue.pop(0)
+                tmp.append(node.val)
+                childrens = node.children
+                for c in childrens:
+                    queue.append(c)
+            res.append(tmp)       
+        return res
+    
