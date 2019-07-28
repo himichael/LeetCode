484fcbeab4613e96d2ff6cfbc751d8c0f4f8f0fe
michaelwng <michaelwangg@qq.com>
2019-07-28 17:57:41 +0800

    生产括号的新实现方式

--- src/1_100/0022_Generate_Parentheses/Generate_Parentheses.py
+++ src/1_100/0022_Generate_Parentheses/Generate_Parentheses.py
@@ -17,3 +17,21 @@
             recursion(left, right+1, content+")")
         recursion(0, 0, "")
         return res
+		
+    def generateParenthesis_2(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        res = []
+        def recursion(left,right,content):
+            print "left->"+str(left)+"  right->"+str(right)+" content->"+content
+            if(left==n and right==n):
+                res.append(content)
+                return
+            if(left < n):
+                recursion(left+1, right, content+"(")
+            if(left>right and right<n):
+                recursion(left, right+1, content+")")
+        recursion(0,0,"")
+        return res
