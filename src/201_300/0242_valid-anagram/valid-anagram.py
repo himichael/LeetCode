9dd7b95ae703442b3bebed001742f92eed29c7e8
michaelwng <michaelwangg@qq.com>
2019-07-28 21:08:47 +0800

    有效的字母异位词

---
+++ src/201_300/0242_valid-anagram/valid-anagram.py
@@ -0,0 +1,23 @@
+﻿class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if(s==None or len(s)==0 or t==None or len(t)==0):
+            return True if(s==t) else False   
+        d = dict()
+        for i in s:
+            if(d.has_key(i)):
+                d[i] += 1
+            else:
+                d[i] = 1
+        for j in t:
+            if(d.has_key(j)):
+                d[j] -= 1
+                if(d[j] == 0):
+                    del d[j]
+            else:
+                return False
+        return len(d)==0
