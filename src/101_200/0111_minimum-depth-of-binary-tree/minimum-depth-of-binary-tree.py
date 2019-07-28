9e128902963ed646e586ec3b64454684331ba56e
michaelwng <michaelwangg@qq.com>
2019-07-28 15:19:27 +0800

    二叉树的最小深度，新实现方式

--- src/101_200/0111_minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.py
+++ src/101_200/0111_minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.py
@@ -21,3 +21,16 @@ class Solution(object):
 		if(root.right != None):
 			res = min(self.minDepth(root.right),res)
 		return res+1
+		
+    def minDepth_2(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if(root == None):
+            return 0
+        left = self.minDepth(root.left)
+        right = self.minDepth(root.right)
+        if(left==0 or right==0):
+            return left+right+1
+        return min(left,right)+1
