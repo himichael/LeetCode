5c62156184d323d9e0ffca829772af36e33be004
michaelwng <michaelwangg@qq.com>
2019-07-30 22:36:34 +0800

    N皇后2

---
+++ src/1_100/0052_n-queens-ii/n-queens-ii.py
@@ -0,0 +1,17 @@
+﻿class Solution(object):
+    def totalNQueens(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        res = []
+        def DFS(columns,xy_diff,xy_sum):
+            p = len(columns)
+            if(p == n):
+                res.append(columns)
+                return
+            for q in range(n):
+                if(q not in columns) and (p-q not in xy_diff) and (p+q not in xy_sum):
+                    DFS(columns+[q], xy_diff+[p-q], xy_sum+[p+q])
+        DFS([],[],[])
+        return len(res)
