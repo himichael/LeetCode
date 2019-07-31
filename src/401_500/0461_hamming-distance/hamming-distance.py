dc3570c58bcd82c6c550f1d6c6ed47a71224d453
michael <michaelwangg@qq.com>
2019-07-31 19:38:50 +0800

    汉明距离

---
+++ src/401_500/0461_hamming-distance/hamming-distance.py
@@ -0,0 +1,24 @@
+﻿class Solution(object):
+    def hammingDistance(self, x, y):
+        """
+        :type x: int
+        :type y: int
+        :rtype: int
+        """
+        a = bin(x)[2:]
+        b = bin(y)[2:]
+        if(len(a) != len(b)):
+            fill_zero = abs(len(a) - len(b))*"0"
+            if(len(a) < len(b)):
+                a = fill_zero + a
+            if(len(a) > len(b)):
+                b = fill_zero + b
+        i = len(a)-1
+        j = len(b)-1
+        result = 0
+        while(i>=0 and j>=0):
+            if(a[i] != b[j]):
+                result += 1
+            i -= 1
+            j -= 1
+        return result
