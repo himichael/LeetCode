1c9c6f1b27d9e840c0fc891caa177c910123b2c8
michael <michaelwangg@qq.com>
2019-08-01 21:01:24 +0800

    环形链表 II

---
+++ src/101_200/0142_linked-list-cycle-ii/linked-list-cycle-ii.py
@@ -0,0 +1,21 @@
+﻿# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def detectCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if(head == None):
+            return None
+        s = set()
+        while(head != None):
+            if(head in s):
+                return head
+            s.add(head)
+            head = head.next
+        return None
