1b82294c7dc0700010d5ab534b27a158746e8829
michaelwng <michaelwangg@qq.com>
2019-08-03 19:04:37 +0800

    机器人能否返回原点

---
+++ src/601_700/0657_robot-return-to-origin/robot-return-to-origin.py
@@ -0,0 +1,18 @@
+﻿class Solution(object):
+    def judgeCircle(self, moves):
+        """
+        :type moves: str
+        :rtype: bool
+        """
+        if(moves==None or len(moves)==0):
+            return True
+        x = 0
+        y = 0
+        x_dict = {"L":-1,"R":1}
+        y_dict = {"U":1,"D":-1}
+        for i in moves:
+            if(x_dict.has_key(i)):
+                x += x_dict[i]
+            if(y_dict.has_key(i)):
+                y += y_dict[i]
+        return (x==0) and (y==0)
