40c68e320275806806cc18db86fa5710c752a725
michaelwng <michaelwangg@qq.com>
2019-08-03 23:08:17 +0800

    反转字符串中的单词 III

---
+++ src/501_600/0557_reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.py
@@ -0,0 +1,22 @@
+﻿class Solution(object):
+    def reverseWords(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        if(s==None or len(s)==0):
+            return s
+        arr = s.split(" ")
+        res = ""
+        for n in range(len(arr)):
+            k = list(arr[n])
+            i = 0
+            j = len(k)-1
+            while(i <= j):
+                k[i],k[j] = k[j],k[i]
+                i += 1
+                j -= 1
+            res = res + "".join(k)
+            if(n < len(arr)-1):
+                res += " "
+        return res
