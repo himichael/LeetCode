23f3f37c34034e3b68d1799aa23ad6449877624f
michaelwng <michaelwangg@qq.com>
2019-08-04 20:05:42 +0800

    x 的平方根 返回浮点结果

--- src/1_100/0069_Sqrt_x_/_Sqrt_x_.py
+++ src/1_100/0069_Sqrt_x_/_Sqrt_x_.py
@@ -15,3 +15,21 @@
 			else:
 				begin = mid
 		return begin
+		
+	#返回浮点结果	
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        if(x==0 or x==1):
+            return x
+        begin = 1
+        end = x
+        while(begin < end):
+            mid = (begin+end+1)/2
+            if(mid*mid > x):
+                end = mid-1
+            else:
+                begin = mid
+        return begin
