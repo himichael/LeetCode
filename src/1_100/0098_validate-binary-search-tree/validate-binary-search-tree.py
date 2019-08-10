bf6d6af6df7363fbbb31edbf51c2a30e9c78c284
michaelwng <michaelwangg@qq.com>
2019-08-10 10:22:35 +0800

    验证二叉搜索树

---
+++ src/1_100/0098_validate-binary-search-tree/validate-binary-search-tree.py
@@ -0,0 +1,49 @@
+﻿# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def isValidBST(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        def check_left(value,root):
+            if(root==None):
+                return True
+            if(root.val < value):
+                if( check_left(value,root.left) ):
+                    return check_left(value,root.right)
+                else:
+                    return False
+            else:
+                return False
+
+        def check_right(value,root):
+            if(root==None):
+                return True
+            if(root.val > value):
+                if( check_right(value,root.left) ):
+                    return check_right(value,root.right)
+                else:
+                    return False
+            else:
+                return False
+        
+        def recursion(root):
+            if(root==None):
+                return True
+            if(root.left!=None):
+                if(not check_left(root.val,root.left)):
+                    return False
+            if(root.right!=None):
+                if(not check_right(root.val,root.right)):
+                    return False
+            res = recursion(root.left)
+            if(res):
+                return recursion(root.right)
+            return res
+        return recursion(root)
