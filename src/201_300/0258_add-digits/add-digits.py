87ba1184ddc259eb6d6ffaaabe3f92e8dbee756b
michaelwng <michaelwangg@qq.com>
2019-08-11 18:36:36 +0800

    各位相加，迭代实现

--- src/201_300/0258_add-digits/add-digits.py
+++ src/201_300/0258_add-digits/add-digits.py
@@ -13,3 +13,18 @@
                 n /= 10
             return recursion(tmp)
         return recursion(num)
+		
+		
+		
+    def addDigits(self, num):
+        """
+        :type num: int
+        :rtype: int
+        """
+        while(num >= 10):
+            tmp = 0
+            while(num > 0):
+                tmp += num%10
+                num /= 10
+            num = tmp
+        return num  
