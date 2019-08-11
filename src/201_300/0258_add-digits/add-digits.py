0d119ac9ca709d60bea23fd9f77479b666632a60
michaelwng <michaelwangg@qq.com>
2019-08-11 16:30:21 +0800

    各位相加

---
+++ src/201_300/0258_add-digits/add-digits.py
@@ -0,0 +1,15 @@
+﻿class Solution(object):
+    def addDigits(self, num):
+        """
+        :type num: int
+        :rtype: int
+        """
+        def recursion(n):
+            if(n < 10):
+                return n
+            tmp = 0
+            while(n > 0):
+                tmp += n%10
+                n /= 10
+            return recursion(tmp)
+        return recursion(num)
