6f75fa42232c38a5b888b80c25c5596694fd4824
michael <michaelwangg@qq.com>
2019-08-14 10:19:36 +0800

    完全二叉树的节点个数

---
+++ src/201_300/0222_count-complete-tree-nodes/count-complete-tree-nodes.py
@@ -0,0 +1,24 @@
+﻿# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+	def countNodes(self, root):
+		"""
+		:type root: TreeNode
+		:rtype: int
+		"""
+		self.res = 0
+		
+		def recursion(root):
+			if(root == None):
+				return
+			self.res += 1
+			recursion(root.left)
+			recursion(root.right)
+		recursion(root)
+		return self.res
+        
