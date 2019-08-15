350822d228a41037d56685487d25942dbe8a4463
michael <michaelwangg@qq.com>
2019-08-15 12:10:28 +0800

    二叉树的右视图

---
+++ src/101_200/0199_binary-tree-right-side-view/binary-tree-right-side-view.py
@@ -0,0 +1,28 @@
+﻿# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+	def rightSideView(self, root):
+		"""
+		:type root: TreeNode
+		:rtype: List[int]
+		"""
+		if(root==None):
+			return []
+		res = []
+		queue = []
+		queue.append(root)
+		while(queue):
+			res.append(queue[-1].val)
+			size = len(queue)
+			for i in range(size):
+				tmp = queue.pop(0)
+				if(tmp.left != None):
+					queue.append(tmp.left)
+				if(tmp.right != None):
+					queue.append(tmp.right)
+		return res
