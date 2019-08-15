682cd872734c9f769e4963300e6310103ef9ba2a
michael <michaelwangg@qq.com>
2019-08-15 20:11:11 +0800

    填充每个节点的下一个右侧节点指针 II

---
+++ src/101_200/0117_populating-next-right-pointers-in-each-node-ii/populating-next-right-pointers-in-each-node-ii.py
@@ -0,0 +1,32 @@
+﻿"""
+# Definition for a Node.
+class Node(object):
+    def __init__(self, val, left, right, next):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+class Solution(object):
+	def connect(self, root):
+		"""
+		:type root: Node
+		:rtype: Node
+		"""
+		if(root==None):
+			return
+		queue = []
+		queue.append(root)
+		while(queue):
+			tmp = queue[0]
+			for i in range(1,len(queue)):
+				tmp.next = queue[i]
+				tmp = queue[i]
+			size = len(queue)
+			for i in range(size):
+				tmp = queue.pop(0)
+				if(tmp.left != None):
+					queue.append(tmp.left)
+				if(tmp.right != None):
+					queue.append(tmp.right)
+		return root
