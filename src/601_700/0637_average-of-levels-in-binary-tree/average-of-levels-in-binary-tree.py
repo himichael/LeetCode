adb2b7659df8cc11a64b9f8fea4f20aad7ff1e7e
michael <michaelwangg@qq.com>
2019-08-15 21:05:01 +0800

    二叉树的层平均值

---
+++ src/601_700/0637_average-of-levels-in-binary-tree/average-of-levels-in-binary-tree.py
@@ -0,0 +1,32 @@
+﻿# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+	def averageOfLevels(self, root):
+		"""
+		:type root: TreeNode
+		:rtype: List[float]
+		"""
+		if(root==None):
+			return 0
+		queue = []
+		res = []
+		queue.append(root)
+		while(queue):
+			tmp = 0.0
+			size = len(queue)
+			for i in range(size):
+				tmp += queue[i].val
+			tmp /= size
+			res.append(tmp)
+			for i in range(size):
+				tmp = queue.pop(0)
+				if(tmp.left != None):
+					queue.append(tmp.left)
+				if(tmp.right != None):
+					queue.append(tmp.right)
+		return res
