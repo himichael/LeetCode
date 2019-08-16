1ea1cad2d4bc7765e36aa73af278420c426d59e8
michaelwng <michaelwangg@qq.com>
2019-08-16 23:08:37 +0800

    二叉树的层次遍历 II

---
+++ src/101_200/0107_binary-tree-level-order-traversal-ii/binary-tree-level-order-traversal-ii.py
@@ -0,0 +1,33 @@
+﻿# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+	def levelOrderBottom(self, root):
+		"""
+		:type root: TreeNode
+		:rtype: List[List[int]]
+		"""
+		if(root==None):
+			return []
+		res = []
+		queue = []
+		queue.append(root)
+		while(queue):
+			size = len(queue)
+			tmp = []
+			for i in range(size):
+				tmp.append(queue[i].val)
+			res.append(tmp)
+			for _ in range(size):
+				tmp = queue.pop(0)
+				if(tmp.left != None):
+					queue.append(tmp.left)
+				if(tmp.right != None):
+					queue.append(tmp.right)
+		res.reverse()
+		return res
+        
