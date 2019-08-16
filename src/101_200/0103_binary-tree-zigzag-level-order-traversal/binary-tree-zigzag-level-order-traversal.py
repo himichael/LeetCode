6a02f37bcb80387f98d7305f177d6fb4624db094
michael <michaelwangg@qq.com>
2019-08-16 10:06:07 +0800

    二叉树的锯齿形层次遍历

---
+++ src/101_200/0103_binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.py
@@ -0,0 +1,38 @@
+﻿# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def zigzagLevelOrder(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        if(root==None):
+            return []
+        queue = []
+        res = []
+        queue.append(root)
+        is_reverse = False
+        while(queue):
+            size = len(queue)
+            tmp = []
+            for _ in range(size):
+                node = queue.pop(0)
+                if(node.left != None):
+                    queue.append(node.left)
+                if(node.right != None):
+                    queue.append(node.right)
+                tmp.append(node.val)
+            if(is_reverse):
+                tmp.reverse()
+                res.append(tmp)
+                is_reverse = False
+            else:
+                res.append(tmp)
+                is_reverse = True
+        return res
+        
