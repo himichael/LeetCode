44ddee9c1b10182767750afa06de641ebe24777d
michaelwng <michaelwangg@qq.com>
2019-08-17 14:27:04 +0800

    二叉树的坡度

---
+++ src/501_600/0563_binary-tree-tilt/binary-tree-tilt.py
@@ -0,0 +1,24 @@
+﻿# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def findTilt(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        self.res = 0
+        def recursion(root):
+            if(root==None):
+                return 0
+            left = recursion(root.left)
+            right = recursion(root.right)
+            self.res += abs(left - right)
+            return root.val + left + right
+        recursion(root)
+        return self.res
+        
