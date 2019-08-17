24b93b1471a84dbc693f4133f32e1b0e8b7483f8
michaelwng <michaelwangg@qq.com>
2019-08-17 15:37:45 +0800

    二叉树的堂兄弟节点

---
+++ src/901_1000/0993_cousins-in-binary-tree/cousins-in-binary-tree.py
@@ -0,0 +1,46 @@
+﻿# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def isCousins(self, root, x, y):
+        """
+        :type root: TreeNode
+        :type x: int
+        :type y: int
+        :rtype: bool
+        """
+        if(root==None):
+            return False
+        if(root.left!=None and (root.left.val==x or root.left.val==y) ):
+            return False
+        if(root.right!=None and (root.right.val==x or root.right.val==y) ):
+            return False
+        self.x_depth = 0
+        self.y_depth = 0
+        def find_node(parent,num,depth,is_x):
+            if(parent==None or (parent.left==None and parent.right==None)):
+                return None
+            if(parent.left!=None and parent.left.val==num):
+                if(is_x):
+                    self.x_depth = depth
+                else:
+                    self.y_depth = depth
+                return parent
+            if(parent.right!=None and parent.right.val==num):
+                if(is_x):
+                    self.x_depth = depth
+                else:
+                    self.y_depth = depth
+                return parent
+            node = find_node(parent.left,num,depth+1,is_x)
+            if(node != None):
+                return node
+            return find_node(parent.right,num,depth+1,is_x)
+        parent_x = find_node(root,x,0,True)
+        parent_y = find_node(root,y,0,False)
+        return (parent_x != parent_y) and (self.x_depth == self.y_depth)  
+    
