a15d40e7e436897311f69f9be7106e2ba4776528
michaelwng <michaelwangg@qq.com>
2019-08-17 17:55:54 +0800

    二叉树的所有路径

---
+++ src/201_300/0257_binary-tree-paths/binary-tree-paths.py
@@ -0,0 +1,31 @@
+﻿# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def binaryTreePaths(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[str]
+        """
+        if(root == None):
+            return []
+        res = []
+        def recursion(root,stack):
+            if(root==None):
+                return
+            if(root!=None and root.left==None and root.right==None):
+                stack.append(root.val)
+                res.append(list(stack))
+                stack.pop()
+                return
+            recursion(root.left,stack+[root.val])
+            recursion(root.right,stack+[root.val])
+        recursion(root,[])
+        result = []
+        for i in range(len(res)):
+            result.append( str(res[i])[1:-1].replace(" ","").replace(",","->") )
+        return result
