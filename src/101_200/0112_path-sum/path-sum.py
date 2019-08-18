95eb93bad462f4fb015f18e7dd1b4f4fddeefef7
michaelwng <michaelwangg@qq.com>
2019-08-18 13:07:26 +0800

    路径总和

---
+++ src/101_200/0112_path-sum/path-sum.py
@@ -0,0 +1,28 @@
+﻿# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def hasPathSum(self, root, sum):
+        """
+        :type root: TreeNode
+        :type sum: int
+        :rtype: bool
+        """
+        res = []
+        def recursion(root,value,stack):
+            if(root!=None and root.left==None and root.right==None):
+                if(value+root.val == sum):
+                    res.append(list(stack+[root.val]))
+                    return
+            if(root==None):
+                return
+            recursion(root.left,value+root.val,stack+[root.val])
+            recursion(root.right,value+root.val,stack+[root.val])
+        recursion(root,0,[])
+        if(len(res) > 0):
+            return True
+        return False
