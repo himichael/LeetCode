1c0a26b391f60f64db9bb52080694101f8040bd4
michael <michaelwangg@qq.com>
2019-08-19 19:41:18 +0800

    缺失数字

---
+++ src/201_300/0268_missing-number/missing-number.py
@@ -0,0 +1,13 @@
+﻿class Solution(object):
+	def missingNumber(self, nums):
+		"""
+		:type nums: List[int]
+		:rtype: int
+		"""
+		nums = sorted(nums)
+		i = 0
+		while(i<len(nums)):
+			if(i != nums[i]):
+				return i
+			i +=1
+		return i
