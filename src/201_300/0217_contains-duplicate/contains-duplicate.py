d20107a56286c9492f25cf6c3b262528ad70b7a8
michael <michaelwangg@qq.com>
2019-08-26 10:39:58 +0800

    存在重复元素

---
+++ src/201_300/0217_contains-duplicate/contains-duplicate.py
@@ -0,0 +1,14 @@
+﻿class Solution(object):
+	def containsDuplicate(self, nums):
+		"""
+		:type nums: List[int]
+		:rtype: bool
+		"""
+		if(not nums or len(nums)==0):
+			return False
+		s = set()
+		for i in nums:
+			if(i in s):
+				return True
+			s.add(i)
+		return False
