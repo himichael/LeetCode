92d9e038ec0d41e1a8f3ffc86a517be1eeb47cf0
michaelwng <michaelwangg@qq.com>
2019-08-27 22:22:29 +0800

    比特位计数

--- src/301_400/0338_counting-bits/counting-bits.py
+++ src/301_400/0338_counting-bits/counting-bits.py
@@ -11,3 +11,15 @@
 			else:
 				res[i] = res[i/2]
 		return res
+		
+		
+		
+	def countBits_2(self, num):
+		"""
+		:type num: int
+		:rtype: List[int]
+		"""
+		res = [0]*(num+1)
+		for i in xrange(1,num+1):
+			res[i] += res[i&(i-1)] +1
+		return res
