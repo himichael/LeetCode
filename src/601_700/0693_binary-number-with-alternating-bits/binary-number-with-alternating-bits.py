1fc954a4a13889191d5904a453cf93f2dfce1103
michael <michaelwangg@qq.com>
2019-08-27 14:29:35 +0800

    交替位二进制数

---
+++ src/601_700/0693_binary-number-with-alternating-bits/binary-number-with-alternating-bits.py
@@ -0,0 +1,16 @@
+﻿class Solution(object):
+	def hasAlternatingBits(self, n):
+		"""
+		:type n: int
+		:rtype: bool
+		"""
+		mask = 1
+		sign = n&1
+		n = n>>1
+		while( n>0 ):
+			tmp = n&1
+			if(tmp == sign):
+				return False
+			sign = tmp	
+			n = n>>1
+		return True
