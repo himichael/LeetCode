326530c9c7d0e88c472fbdafc75ef839da8b3955
michael <michaelwangg@qq.com>
2019-08-27 18:33:14 +0800

    位1的个数，新实现方式

--- src/101_200/0191_number-of-1-bits/number-of-1-bits.py
+++ src/101_200/0191_number-of-1-bits/number-of-1-bits.py
@@ -5,3 +5,18 @@
 		:rtype: int
 		"""
 		return bin(n).count("1")
+		
+		
+		
+	def hammingWeight_2(self, n):
+		"""
+		:type n: int
+		:rtype: int
+		"""
+		mask = 1
+		res = 0
+		for _ in xrange(32):
+			if( n&mask ):
+				res +=1
+			mask = mask<<1
+		return res		
