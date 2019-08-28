4685a8f3237f96ecb467dfbd3ddeca4c43abcbdc
michael <michaelwangg@qq.com>
2019-08-28 10:23:21 +0800

    补提交

--- src/301_400/0326_power-of-three/power-of-three.py
+++ src/301_400/0326_power-of-three/power-of-three.py
@@ -1,27 +1,13 @@
-﻿# Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
-
-class Solution(object):
-	def oddEvenList(self, head):
+﻿class Solution(object):
+	def isPowerOfThree(self, n):
 		"""
-		:type head: ListNode
-		:rtype: ListNode
+		:type n: int
+		:rtype: bool
 		"""
-		if(head==None):
-			return head
-		odd = ListNode(-1)
-		event = ListNode(-1)
-		a = head
-		b = head.next
-		odd.next = a
-		event.next = b
-		while(b!=None and b.next!=None):
-			a.next = b.next
-			b.next = a.next.next
-			a = a.next
-			b = b.next
-		a.next = event.next
-		return odd.next
+		while( n>1 ):
+			if( (n%3) != 0 ):
+				return False
+			n = n/3
+		if( n==1 ):
+			return True
+		return False
