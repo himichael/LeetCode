e41466be5cbe0b2a8c0b3312ae548b7551a4f312
michael <michaelwangg@qq.com>
2019-08-30 13:24:14 +0800

    颜色分类新实现方式

--- src/1_100/0075_sort-colors/sort-colors.py
+++ src/1_100/0075_sort-colors/sort-colors.py
@@ -9,6 +9,7 @@
         nums.sort()
 
 
+
     #颜色分类的新实现方式
     def sortColors_2(self, nums):
         """
@@ -37,3 +38,30 @@
         while( i<two+one+zero ):
             nums[i] = 2
             i += 1		
+
+
+
+    #类似快速排序分割的方式
+    def sortColors(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        if(not nums):
+            return 
+        i = 0
+        j = 0
+        n = len(nums)
+        while( i<n ):
+            if( nums[i]==0 ):
+                nums[i],nums[j] = nums[j],nums[i]
+                j += 1
+            i += 1
+        i = j
+        while( i<n ):
+            if( nums[i]==1 ):
+                nums[i],nums[j] = nums[j],nums[i]
+                j += 1
+            i += 1
+        #print nums
+			
