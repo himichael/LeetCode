e870769f1cc2f80dc889cdace087b04f0afaaa4e
michael <michaelwangg@qq.com>
2019-08-30 13:10:14 +0800

    颜色分类新实现方式

--- src/1_100/0075_sort-colors/sort-colors.py
+++ src/1_100/0075_sort-colors/sort-colors.py
@@ -7,3 +7,33 @@
         if(not nums):
             return 
         nums.sort()
+
+
+    #颜色分类的新实现方式
+    def sortColors_2(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        if(not nums):
+            return 
+        zero = 0
+        one = 0
+        two = 0
+        for i in nums:
+            if( i==0 ):
+                zero +=1
+            elif( i==1 ):
+                one += 1
+            else:
+                two += 1
+        i = 0
+        while( i<zero ):
+            nums[i] = 0
+            i += 1
+        while( i<one+zero ):
+            nums[i] = 1
+            i += 1
+        while( i<two+one+zero ):
+            nums[i] = 2
+            i += 1		
