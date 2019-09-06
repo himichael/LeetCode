efb8b8e05fd8ffb8628129a0fae0533ccdd69763
michael <michaelwangg@qq.com>
2019-09-06 12:35:20 +0800

    二叉搜索树中第K小的元素

---
+++ src/201_300/0230_kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.py
@@ -0,0 +1,31 @@
+﻿# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+	def kthSmallest(self, root, k):
+		"""
+		:type root: TreeNode
+		:type k: int
+		:rtype: int
+		"""
+		if( k<1):
+			return None
+		res = []
+		def recursion(root):
+			if(not root):
+				return None
+			recursion(root.left)
+			res.append(root.val)
+			recursion(root.right)
+		recursion(root)
+		if( k>len(res) ):
+			return None
+		return res[k-1]
+    
+    
+    
+    
