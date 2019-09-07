63bcf437993533b21e85b4737c781649546371f2
michaelwng <michaelwangg@qq.com>
2019-09-07 12:35:40 +0800

    活字印刷

---
+++ src/1001_1100/1079_letter-tile-possibilities/letter-tile-possibilities.py
@@ -0,0 +1,35 @@
+﻿class Solution(object):
+    def numTilePossibilities(self, tiles):
+        """
+        :type tiles: str
+        :rtype: int
+        """
+        res = set()
+        
+        def rotate(s):
+            stack = []
+            queue = []
+            for i in s:
+                queue.append(i)
+            def rotate_assemble(queue,stack):
+                if(not queue):
+                    res.add( "".join(list(stack)) )
+                    return
+                size = len(queue)
+                for _ in xrange(size):
+                    stack.append(queue.pop(0))
+                    rotate_assemble(queue, stack)
+                    queue.append(stack.pop())
+            rotate_assemble(queue, stack)
+            
+        
+        def recursive(i,tmp):
+            #res.add(tmp)
+            rotate(tmp)
+            for j in range(i,len(tiles)):
+                x = tmp
+                recursive(j+1, x+tiles[j])
+        recursive(0, "")
+        return len(res)-1
+    
+    
