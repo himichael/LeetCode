b7e5f474ebf7c0a606c6a655ff5e883ecc6f2a0a
michaelwng <michaelwangg@qq.com>
2019-09-15 18:18:02 +0800

    增加一题，添加与搜索单词 - 数据结构设计

---
+++ src/201_300/0211_add-and-search-word-data-structure-design/add-and-search-word-data-structure-design.py
@@ -0,0 +1,26 @@
+﻿class WordDictionary(object):
+    def __init__(self):
+        self.root = {}
+
+    def addWord(self, word):
+        node = self.root
+        for w in word:
+            node = node.setdefault(w,{})
+        node["#"] = {}      
+
+    def search(self, word):
+        node = self.root
+        def dfs(word,node):
+            if(not word):
+                if("#" in node):
+                    return True
+                return False
+            if(word[0]=="."):
+                for n in node:
+                    if dfs(word[1:],node[n]):
+                        return True
+            elif(word[0] in node):
+                if dfs(word[1:],node[word[0]]):
+                    return True
+            return False
+        return dfs(word,node)
