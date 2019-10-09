91d21d924cfd5a6679da478cc8091d05d074d578
michael <michaelwangg@qq.com>
2019-10-09 18:29:08 +0800

    两数相加增加python实现

---
+++ src/1_100/0002_Add_Two_Numbers/Add_Two_Numbers.py
@@ -0,0 +1,12 @@
+﻿class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        a,b,p,carry = l1,l2,None,0
+        while a or b:
+            val = (a.val if a else 0)+(b.val if b else 0)+carry
+            carry,val = val/10 if val>=10 else 0,val%10
+            p,p.val = a if a else b,val
+            a,b = a.next if a else None,b.next if b else None
+            p.next = a if a else b
+        if carry:
+            p.next = ListNode(carry)
+        return l1
