2ecff177077b65911a63b20802af68e2c6484f8e
michaelwng <michaelwangg@qq.com>
2019-10-10 22:44:30 +0800

    删除链表中的节点，增加python实现

---
+++ src/201_300/0237_Delete_Node_in_a_Linked_List/Delete_Node_in_a_Linked_List.py
@@ -0,0 +1,15 @@
+﻿# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def deleteNode(self, node):
+        """
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        """
+        if not (node and node.next):
+            return
+        node.val,node.next = node.next.val,node.next.next
