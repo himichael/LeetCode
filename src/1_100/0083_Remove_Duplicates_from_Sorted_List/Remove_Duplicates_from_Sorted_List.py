15abae886341245c0239d2ffd0ae6f53c88a0f05
michael <michaelwangg@qq.com>
2019-10-10 19:48:58 +0800

    删除排序链表中的重复元素，增加python实现

---
+++ src/1_100/0083_Remove_Duplicates_from_Sorted_List/Remove_Duplicates_from_Sorted_List.py
@@ -0,0 +1,22 @@
+﻿# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+	def deleteDuplicates(self, head):
+		"""
+		:type head: ListNode
+		:rtype: ListNode
+		"""
+		if not (head and head.nxt):
+			return head
+		i,j = head,head
+		while j:
+			if i.val!=j.val:
+				i = i.next
+				i.val = j.val
+			j = j.next
+		i.next = None
+		return head
