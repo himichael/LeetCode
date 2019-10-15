d269efef8ed08a065d5f54ad1baa77b10ea83876
michael <michaelwangg@qq.com>
2019-10-15 14:57:22 +0800

    增加一题，相交链表

---
+++ src/101_200/160_intersection-of-two-linked-lists/intersection-of-two-linked-lists.py
@@ -0,0 +1,24 @@
+﻿# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+	# 利用set实现
+	def getIntersectionNode(self, headA, headB):
+		"""
+		:type head1, head1: ListNode
+		:rtype: ListNode
+		"""
+		s,p,q = set(),headA,headB
+		while p:
+			_,p = s.add(p),p.next
+		while q:
+			if q in s:
+				return q
+			q = q.next
+		return None
+		
+		
+	
