3befd730133ecd74a480602b1dc7e0e63666fa22
michael <michaelwangg@qq.com>
2019-10-15 15:01:04 +0800

    增加一个新实现方式

--- src/101_200/160_intersection-of-two-linked-lists/intersection-of-two-linked-lists.py
+++ src/101_200/160_intersection-of-two-linked-lists/intersection-of-two-linked-lists.py
@@ -21,4 +21,15 @@ class Solution(object):
 		return None
 		
 		
-	
+	# O(1)空间复杂度实现方式，当a遍历结束后从headB从新遍历，b遍历结束后从headA重新遍历
+	# 直到找到a和b相等为止
+	def getIntersectionNode(self, headA, headB):
+		"""
+		:type head1, head1: ListNode
+		:rtype: ListNode
+		"""
+		a,b = headA,headB
+		while a!=b:
+			a = a.next if a else headB
+			b = b.next if b else headA
+		return a	
