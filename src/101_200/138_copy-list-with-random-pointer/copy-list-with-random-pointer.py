2c34507b3bd4836411e64262d654414ad51cbc92
michaelwng <michaelwangg@qq.com>
2019-10-20 09:01:40 +0800

    复制带随机指针的链表 增加python实现

---
+++ src/101_200/138_copy-list-with-random-pointer/copy-list-with-random-pointer.py
@@ -0,0 +1,22 @@
+﻿"""
+# Definition for a Node.
+class Node(object):
+    def __init__(self, val, next, random):
+        self.val = val
+        self.next = next
+        self.random = random
+"""
+class Solution(object):
+    def copyRandomList(self, head):
+        """
+        :type head: Node
+        :rtype: Node
+        """
+        p,q,d = head,head,dict()
+        while q:
+            d[q],q = Node(q.val,None,None),q.next
+        while p:
+            d[p].next = d[p.next] if p.next else None
+            d[p].random = d[p.random] if p.random else None
+            p = p.next
+        return d[head] if head else None
