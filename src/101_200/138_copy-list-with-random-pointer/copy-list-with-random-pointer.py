9e5001c70c450ac46ec715811a5d1777d7950774
michaelwng <michaelwangg@qq.com>
2019-10-20 15:15:43 +0800

    复制带随机指针的链表 新实现方式

--- src/101_200/138_copy-list-with-random-pointer/copy-list-with-random-pointer.py
+++ src/101_200/138_copy-list-with-random-pointer/copy-list-with-random-pointer.py
@@ -7,6 +7,7 @@ class Node(object):
         self.random = random
 """
 class Solution(object):
+	# hash表实现方式
     def copyRandomList(self, head):
         """
         :type head: Node
@@ -20,3 +21,27 @@ class Solution(object):
             d[p].random = d[p.random] if p.random else None
             p = p.next
         return d[head] if head else None
+		
+		
+		
+	# 增加冗余节点 O(1)空间复杂度实现方式
+    def copyRandomList(self, head):
+        """
+        :type head: Node
+        :rtype: Node
+        """
+        if not head:
+            return head
+        q = Node(-1,None,None)
+        p,p2,p3,cur = head,head,head,q
+        while p:
+            x = Node(p.val,None,None)
+            x.next = p.next
+            p.next,p = x,x.next
+        while p2:
+            p2.next.random = p2.random.next if p2.random else None
+            p2 = p2.next.next
+        while p3:
+            cur.next,cur = p3.next,p3.next
+            p3.next,p3 = cur.next,cur.next
+        return q.next
