b9620a0698b93377ff091a68310f974006681d52
michael <michaelwangg@qq.com>
2019-11-01 13:01:31 +0800

    颜色分类的java实现

---
+++ src/1_100/0075_sort-colors/sort-colors.java
@@ -0,0 +1,28 @@
+﻿class Solution {
+    public void sortColors(int[] nums) {
+		if(nums==null || nums.length==0) {
+			return;
+		}
+		int p0 = 0;
+		int p2 = nums.length-1;
+		int cur = 0;
+		while(cur<=p2) {
+			if(nums[cur]==0) {
+				swap(nums,cur,p0);
+				++cur;
+				++p0;
+			} else if(nums[cur]==2) {
+				swap(nums,cur,p2);
+				--p2;
+			} else {
+				++cur;
+			}
+		}
+    }
+	
+	void swap(int[] arr,int i,int j) {
+		int tmp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = tmp;
+	}
+}	
