be6e4c5feacdc90dd8fb3148b0da1495a6e100a1
michael <michaelwangg@qq.com>
2019-11-12 10:40:46 +0800

    增加一题， 替换后的最长重复字符

---
+++ src/401_500/0424_longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -0,0 +1,22 @@
+﻿class Solution(object):
+    def characterReplacement(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: int
+        """
+        if not s or k<0:
+            return 0
+        left,right,res = 0,0,0
+        d = dict()
+        max_frequecy = 0
+        while right<len(s):
+            c = s[right]
+            d[c] = d.get(c,0)+1
+            max_frequecy = max(max_frequecy,d[c])
+            if (right-left+1-max_frequecy)>k:
+                d[s[left]] -=1
+                left += 1
+            res = max(res,right-left+1)
+            right += 1
+        return res
