22827ed522f390f6f9ed6768e45a837d98e50acb
michael <michaelwangg@qq.com>
2019-11-13 10:03:47 +0800

    增加一题，第三大的数

---
+++ src/401_500/0414_third-maximum-number/third-maximum-number.py
@@ -0,0 +1,21 @@
+﻿class Solution(object):
+	def thirdMax(self, nums):
+		"""
+		:type nums: List[int]
+		:rtype: int
+		"""
+		one,two,three = -2**63,-2**63,-2**63
+		one = nums[0]
+		for i in xrange(len(nums)):
+			cur = nums[i]
+			if one==cur or two==cur or three==cur:
+				continue
+			if one<cur:
+				three,two,one = two,one,cur
+			elif two<cur:
+				three,two = two,cur
+			elif three<cur:
+				three = cur
+		if three==(-2**63):
+			return one
+		return three
