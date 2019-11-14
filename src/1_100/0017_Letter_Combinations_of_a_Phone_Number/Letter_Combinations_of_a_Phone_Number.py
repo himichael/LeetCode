5db2a49550ea4fe965a8d34274e3b16116728ff9
michael <michaelwangg@qq.com>
2019-11-14 20:46:10 +0800

    增加python实现

---
+++ src/1_100/0017_Letter_Combinations_of_a_Phone_Number/Letter_Combinations_of_a_Phone_Number.py
@@ -0,0 +1,43 @@
+﻿class Solution(object):
+	
+	# 队列实现
+	def letterCombinations(self, digits):
+		"""
+		:type digits: str
+		:rtype: List[str]
+		"""	
+		if not digits:
+			return []
+		d = [" ","*","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"]
+		
+		res = [""]
+		for i in digits:
+			#tmp = res.pop(0)
+			size = len(res)
+			letters = d[ord(i)-48]
+			
+			for _ in xrange(size):
+				tmp = res.pop(0)
+				for j in letters:
+					res.append(tmp+j)
+		return res
+		
+	# 递归实现
+	def letterCombinations(self, digits):
+		"""
+		:type digits: str
+		:rtype: List[str]
+		"""
+		if not digits:
+			return []
+		d = [" ","*","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"]		
+		res = []
+		def dfs(s,tmp):
+			if not s:
+				res.append(tmp)
+				return
+			letter = d[ord(s[0])-48]
+			for i in letter:
+				dfs(s[1:],tmp+i)
+		dfs(digits,"")
+		return res
