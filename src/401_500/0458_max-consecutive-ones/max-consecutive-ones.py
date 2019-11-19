bc9f1bee059ffc6fbde1815a108dc1958d5b0afd
michael <michaelwangg@qq.com>
2019-11-19 12:44:22 +0800

    增加题目，485. 最大连续1的个数

---
+++ src/401_500/0458_max-consecutive-ones/max-consecutive-ones.py
@@ -0,0 +1,17 @@
+﻿class Solution(object):
+	def findMaxConsecutiveOnes(self, nums):
+		"""
+		:type nums: List[int]
+		:rtype: int
+		"""
+		if not nums:
+			return 0
+		left,right = 0,0
+		res = 0
+		while right<len(nums):
+			if nums[right]==1:
+				right += 1
+				res = max(res,right-left)
+			else:
+				right = left = right+1
+		return res
