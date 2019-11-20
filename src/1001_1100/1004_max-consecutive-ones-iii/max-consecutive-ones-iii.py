3fec69f2ccf6ebefd86b6a9a2a3d2bb669e27af4
michael <michaelwangg@qq.com>
2019-11-20 21:11:54 +0800

    增加一题，最大连续1的个数 III

---
+++ src/1001_1100/1004_max-consecutive-ones-iii/max-consecutive-ones-iii.py
@@ -0,0 +1,25 @@
+﻿class Solution(object):
+	def longestOnes(self, A, K):
+		"""
+		:type A: List[int]
+		:type K: int
+		:rtype: int
+		"""
+		if not A:
+			return 0
+		left,right = 0,0
+		windows = []
+		n = K
+		res = 0
+		count = 0
+		while right<len(A):
+			c = A[right]
+			if c==0:
+				count += 1
+			while count>K:
+				if A[left]==0:
+					count -= 1
+				left += 1
+			right += 1
+			res = max(res,right-left)
+		return res
