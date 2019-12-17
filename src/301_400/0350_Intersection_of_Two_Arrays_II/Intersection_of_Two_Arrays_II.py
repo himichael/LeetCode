be582cc470c750c929208b5c8e164f3f4f0b0f23
michaelwng <michaelwangg@qq.com>
2019-12-17 22:28:19 +0800

    两个数组的交集 II，增加新实现方式

--- src/301_400/0350_Intersection_of_Two_Arrays_II/Intersection_of_Two_Arrays_II.py
+++ src/301_400/0350_Intersection_of_Two_Arrays_II/Intersection_of_Two_Arrays_II.py
@@ -5,3 +5,23 @@
         for i in inter:
             res += [i] * min(nums1.count(i), nums2.count(i))  
         return res
+
+	def intersect(self, nums1, nums2):
+		"""
+		:type nums1: List[int]
+		:type nums2: List[int]
+		:rtype: List[int]
+		"""		
+		nums1.sort()
+		nums2.sort()
+		n1,n2 = 0,0
+		res = []
+		while n1<len(nums1) and n2<len(nums2):
+			if nums1[n1]<nums2[n2]:
+				n1 += 1
+			elif nums1[n1]>nums2[n2]:
+				n2 += 1
+			else:
+				res.append(nums1[n1])
+				n1,n2 = n1+1,n2+1
+		return res
