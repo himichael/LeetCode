6d34515a20ea6a103dadf115bedbbbfa6e33e83a
michael <michaelwangg@qq.com>
2019-12-18 14:03:22 +0800

    增加一题，寻找比目标字母大的最小字母

---
+++ src/701_800/0744_find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.py
@@ -0,0 +1,15 @@
+﻿class Solution(object):
+	def nextGreatestLetter(self, letters, target):
+		"""
+		:type letters: List[str]
+		:type target: str
+		:rtype: str
+		"""	
+		begin,end = 0,len(letters)-1
+		while begin<=end:
+			mid = begin+(end-begin)/2
+			if letters[mid]<=target:
+				begin = mid+1
+			else:
+				end = mid-1
+		return letters[begin%len(letters)]
