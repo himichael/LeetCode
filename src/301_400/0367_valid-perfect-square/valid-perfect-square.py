b5a582633698dbb4d1f5fb7ef952e5d414c2874c
michaelwng <michaelwangg@qq.com>
2019-12-18 08:48:45 +0800

    增加一题，有效的完全平方数

---
+++ src/301_400/0367_valid-perfect-square/valid-perfect-square.py
@@ -0,0 +1,20 @@
+﻿class Solution(object):
+	def isPerfectSquare(self, num):
+		"""
+		:type num: int
+		:rtype: bool
+		"""
+		if num<0:
+			return False
+		if num==0 or num==1:
+			return True
+		begin,end = 1,num
+		while begin<=end:
+			mid = (begin+end)/2
+			if mid*mid>num:
+				end = mid-1
+			elif mid*mid<num:
+				begin = mid+1
+			else:
+				return True
+		return False
