cbbef53f7e911bdf2a296078aa728c670ce21bf7
michaelwng <michaelwangg@qq.com>
2019-12-18 20:01:37 +0800

    增加一题，山脉数组的峰顶索引

---
+++ src/801_900/0852_peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.py
@@ -0,0 +1,16 @@
+﻿class Solution(object):
+	def peakIndexInMountainArray(self, A):
+		"""
+		:type A: List[int]
+		:rtype: int
+		"""		
+		begin,end = 0,len(A)-1
+		while begin<=end:
+			mid = begin+(end-begin)/2
+			left,right,n = A[mid-1],A[mid+1],A[mid]
+			if left<n<right:
+				begin = mid+1
+			elif left>n>right:
+				end = mid-1
+			elif left<n>right:
+				return mid
