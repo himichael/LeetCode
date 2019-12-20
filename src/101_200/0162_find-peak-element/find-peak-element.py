ad1d68b79453db02dd8397a78f3f1ec4d1553bdd
michael <michaelwangg@qq.com>
2019-12-20 12:22:02 +0800

    增加一题，寻找峰值

---
+++ src/101_200/0162_find-peak-element/find-peak-element.py
@@ -0,0 +1,15 @@
+﻿class Solution(object):
+	def findPeakElement(self, nums):
+		"""
+		:type nums: List[int]
+		:rtype: int
+		"""
+		begin,end = 0,len(nums)-1
+		while begin<end:
+			mid = begin+(end-begin)/2
+			if nums[mid]>nums[mid+1]:
+				end = mid
+			else:
+				begin = mid+1
+	
+		return begin
