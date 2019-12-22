220088c12e6372bc4eee26c2e6dc56e1e476cd00
michaelwng <michaelwangg@qq.com>
2019-12-22 16:58:27 +0800

    增加一题，寻找旋转排序数组中的最小值

---
+++ src/101_200/0153_find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
@@ -0,0 +1,20 @@
+﻿class Solution(object):
+	def findMin(self, nums):
+		"""
+		:type nums: List[int]
+		:rtype: int
+		"""	
+		if not nums or len(nums)==1:
+			return -1 if not nums else nums[0]
+		begin,end = 0,len(nums)-1
+		if nums[begin]< nums[end]:
+			return nums[begin]
+		while begin<=end:
+			mid = begin+(end-begin)/2
+			if nums[mid]>nums[mid+1]:
+				return nums[mid+1]
+			elif nums[mid]>=nums[0]:
+				begin = mid+1
+			else:
+				end = mid-1
+		return nums[begin]
