a5d5e52a1018fdf93fb0be95869b837723690895
michael <michaelwangg@qq.com>
2020-01-02 13:00:37 +0800

    增加一题

---
+++ src/201_300/0209_minimum-size-subarray-sum/minimum-size-subarray-sum.py
@@ -0,0 +1,16 @@
+﻿class Solution(object):
+	def minSubArrayLen(self, s, nums):
+		"""
+		:type s: int
+		:type nums: List[int]
+		:rtype: int
+		"""
+		n = len(nums)
+		left,total,res = 0,0,2**31-1
+		for i in xrange(n):
+			total += nums[i]
+			while total >= s:
+				res = min(res,i-left+1)
+				total -= nums[left]
+				left += 1
+		return res if res!=(2**31-1) else 0
