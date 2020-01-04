78fa7f3491c3a69b4f1071f8b16a5ad03a45c941
michaelwng <michaelwangg@qq.com>
2020-01-04 17:14:36 +0800

    1

---
+++ src/201_300/0274_h-index/h-index.py
@@ -0,0 +1,23 @@
+﻿class Solution(object):
+	def hIndex(self, citations):
+		"""
+		:type citations: List[int]
+		:rtype: int
+		"""
+		if not citations:
+			return 0
+		arr = sorted(citations)
+		n = len(arr)
+		self.res = 0
+		def binary_search():
+			begin,end = 0,n-1
+			while begin<=end:
+				mid = begin+(end-begin)/2
+				h_index = n-mid
+				if arr[mid]>=h_index:
+					self.res = h_index
+					end = mid-1
+				else:
+					begin = mid+1
+		binary_search()
+		return self.res
