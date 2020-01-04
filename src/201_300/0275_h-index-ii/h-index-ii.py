49550d1b01f06a4d6bad21cb86c665b71b93fcee
michaelwng <michaelwangg@qq.com>
2020-01-04 17:15:35 +0800

    2

---
+++ src/201_300/0275_h-index-ii/h-index-ii.py
@@ -0,0 +1,19 @@
+﻿class Solution(object):
+	def hIndex(self, citations):
+		"""
+		:type citations: List[int]
+		:rtype: int
+		"""
+		if not citations:
+			return 0
+		begin,end = 0,len(citations)-1
+		n,res = len(citations),0
+		while begin<=end:
+			mid = begin+(end-begin)/2
+			h_index = n-mid
+			if citations[mid]>=h_index:
+				res = h_index
+				end = mid-1
+			else:
+				begin = mid+1
+		return res
