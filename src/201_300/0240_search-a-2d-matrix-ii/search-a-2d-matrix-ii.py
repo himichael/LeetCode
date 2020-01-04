c06ca32b1348157395244faf1d6f7c6de4c5fe3e
michaelwng <michaelwangg@qq.com>
2020-01-04 14:55:51 +0800

    增加一题

---
+++ src/201_300/0240_search-a-2d-matrix-ii/search-a-2d-matrix-ii.py
@@ -0,0 +1,21 @@
+﻿class Solution(object):
+	def searchMatrix(self, matrix, target):
+		"""
+		:type matrix: List[List[int]]
+		:type target: int
+		:rtype: bool
+		"""
+		if not matrix or not matrix[0]:
+			return False
+		heigh = len(matrix)
+		width = len(matrix[0])
+		# p = matrix[heigh-1][0]
+		row,column = heigh-1,0
+		while row>=0 and column<width:
+			if matrix[row][column]>target:
+				row -= 1
+			elif matrix[row][column]<target:
+				column += 1
+			else:
+				return True
+		return False
