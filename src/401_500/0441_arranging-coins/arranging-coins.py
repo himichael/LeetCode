9795dd57c343c5913a7e7c03a02cf4b4fb0550ed
michaelwng <michaelwangg@qq.com>
2020-01-05 12:16:27 +0800

    增加一题

---
+++ src/401_500/0441_arranging-coins/arranging-coins.py
@@ -0,0 +1,15 @@
+﻿class Solution(object):
+	def arrangeCoins(self, n):
+		"""
+		:type n: int
+		:rtype: int
+		"""	
+		if n ==0:
+			return 0	
+		k,i = n,1
+		res = 0
+		while i<=k:
+			k -= i
+			res = i
+			i += 1
+		return res
