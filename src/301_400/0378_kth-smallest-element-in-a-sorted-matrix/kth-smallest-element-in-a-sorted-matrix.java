53247c2cbbb1139b6001dcda8cf0302ec635446f
michael <michaelwangg@qq.com>
2020-01-06 19:23:13 +0800

    增加一题

---
+++ src/301_400/0378_kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.java
@@ -0,0 +1,18 @@
+﻿class Solution {
+    public int kthSmallest(int[][] matrix, int k) {
+     	java.util.PriorityQueue<Integer> queue = new java.util.PriorityQueue<Integer>(new Comparator<Integer>() {
+     		public int compare(Integer o1, Integer o2) {
+				return o2-o1;
+		}});
+    	for(int i=0;i<matrix.length;i++) {
+    		for(int j=0;j<matrix[i].length;j++) {
+    			queue.add(matrix[i][j]);
+    			if(queue.size()>k) {
+    				queue.poll();
+    			}
+    		}
+    	}
+    	System.out.println(queue);
+    	return queue.peek();
+    }
+}
