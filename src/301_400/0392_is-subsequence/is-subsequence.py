d13b9268af3dcd73fe1437f3e48fb310ef1728e4
michael <michaelwangg@qq.com>
2020-01-07 20:24:11 +0800

    增加新解法

--- src/301_400/0392_is-subsequence/is-subsequence.py
+++ src/301_400/0392_is-subsequence/is-subsequence.py
@@ -12,5 +12,27 @@
 			else:
 				j += 1
 		return True if i==len(s) else False
-		
-		
+	
+	# 另一种解法
+	def isSubsequence(self, s, t):
+		"""
+		:type s: str
+		:type t: str
+		:rtype: bool
+		"""
+		if not s:
+			return True
+		if not t:
+			return False
+		i,j = 0,0
+		while i<len(s):
+			cur = s[i]
+			while j<len(t):
+				if cur==t[j]:
+					i,j = i+1,j+1
+					break
+				j += 1
+			if j==len(t) and i<len(s):
+				return False
+			if i==len(s):
+				return True
