2c42195f391f396d4eb841509ebe6b151c1c6e0f
michael <michaelwangg@qq.com>
2020-01-09 19:55:24 +0800

    寻找右区间

---
+++ src/401_500/0436_find-right-interval/find-right-interval.java
@@ -0,0 +1,14 @@
+﻿class Solution {
+    public int[] findRightInterval(int[][] intervals) {
+		java.util.TreeMap<Integer,Integer> map = new java.util.TreeMap<Integer,Integer>();
+		int[] res = new int[intervals.length];
+		for(int i=0;i<intervals.length;i++) {
+			map.put(intervals[i][0],i);		
+		}
+		for(int i=0;i<intervals.length;i++) {
+			java.util.Map.Entry<Integer,Integer> entry = map.ceilingEntry(intervals[i][intervals[i].length-1]);
+			res[i] = (entry==null? -1 : entry.getValue());
+		}
+		return res;
+    }
+}
