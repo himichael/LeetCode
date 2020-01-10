4f4459ef0cdefa0f4c8ddae64ca45db127cf0250
michael <michaelwangg@qq.com>
2020-01-10 13:05:58 +0800

    按权重随机选择

---
+++ src/501_600/0528_random-pick-with-weight/random-pick-with-weight.py
@@ -0,0 +1,30 @@
+﻿class Solution(object):
+	def __init__(self, w):
+		"""
+		:type w: List[int]
+		"""
+		self.total = 0
+		self.res = []
+		for i in w:
+			self.total += i
+			self.res.append(self.total)
+		
+
+	def pickIndex(self):
+		"""
+		:rtype: int
+		"""
+		target = random.randint(0,self.total-1)
+		begin,end = 0,len(self.res)-1
+		while begin!=end:
+			mid = begin+(end-begin)/2
+			if self.res[mid]<=target:
+				begin = mid+1
+			else:
+				end = mid
+		return begin
+
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(w)
+# param_1 = obj.pickIndex()
