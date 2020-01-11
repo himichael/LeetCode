67bee22370d1c40a8feb5999697c3e1a49b86c21
michaelwng <michaelwangg@qq.com>
2020-01-11 11:13:00 +0800

    最长上升子序列

---
+++ src/201_300/0300_longest-increasing-subsequence/longest-increasing-subsequence.py
@@ -0,0 +1,17 @@
+﻿class Solution(object):
+	def lengthOfLIS(self, nums):
+		"""
+		:type nums: List[int]
+		:rtype: int
+		"""
+		if not nums:
+			return 0
+		dp = [1 for i in xrange(len(nums))]
+		res = 0
+		for i in xrange(len(nums)):
+			for j in xrange(0,i):
+				if nums[i]>nums[j]:
+					dp[i] = max(dp[i],dp[j]+1)
+		
+			res = max(res,dp[i])
+		return res
