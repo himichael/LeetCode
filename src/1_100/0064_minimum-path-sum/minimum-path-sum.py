b6ac3c71fdd580c616f6e0addcacc6c8dc4dd33f
michael <michaelwangg@qq.com>
2020-01-15 19:32:32 +0800

    最小路径和

---
+++ src/1_100/0064_minimum-path-sum/minimum-path-sum.py
@@ -0,0 +1,20 @@
+﻿class Solution(object):
+	def minPathSum(self, grid):
+		"""
+		:type grid: List[List[int]]
+		:rtype: int
+		"""		
+		if not grid:
+			return 0
+		n,m = len(grid),len(grid[0])
+		if n==1:
+			return sum(grid[0])
+		for i in xrange(1,m):
+			grid[0][i] = grid[0][i-1] + grid[0][i]
+		for i in xrange(1,n):
+			grid[i][0] = grid[i-1][0] + grid[i][0]
+		for i in xrange(1,n):
+			for j in xrange(1,m):
+				tmp = grid[i][j]
+				grid[i][j] = min(grid[i-1][j]+tmp,grid[i][j-1]+tmp)
+		return grid[-1][-1]
