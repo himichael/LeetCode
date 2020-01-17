2028785a309500b4588ea2be7693f6dae83b2131
michael <michaelwangg@qq.com>
2020-01-17 14:15:14 +0800

    找到所有数组中消失的数字，增加新实现

--- src/401_500/0448_find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.py
+++ src/401_500/0448_find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.py
@@ -11,3 +11,20 @@
 			if i not in s:
 				res.append(i)
 		return res
+		
+		
+	# O(n)时间复杂度解法
+	def findDisappearedNumbers_2(self, nums):
+		"""
+		:type nums: List[int]
+		:rtype: List[int]
+		"""
+		res = []
+		for i in xrange(len(nums)):
+			new_index = abs(nums[i])-1
+			if nums[new_index]>0:
+				nums[new_index] *= -1
+		for i in xrange(1,len(nums)+1):
+			if nums[i-1]>0:
+				res.append(i)
+		return res
