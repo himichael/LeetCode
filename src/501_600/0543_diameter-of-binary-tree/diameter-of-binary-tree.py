a3c72762fcc37741fe604a87aecd3dc8afd0fec7
michaelwng <michaelwangg@qq.com>
2020-01-17 21:32:39 +0800

    二叉树的直径

---
+++ src/501_600/0543_diameter-of-binary-tree/diameter-of-binary-tree.py
@@ -0,0 +1,19 @@
+﻿class Solution(object):
+	def diameterOfBinaryTree(self, root):
+		"""
+		:type root: TreeNode
+		:rtype: int
+		"""
+		self.res = 0 
+		def dfs(root):
+			if not root or not (root.left or root.right):
+				return 0
+			left,right = 0,0
+			if root.left:
+				left = dfs(root.left)+1
+			if root.right:
+				right = dfs(root.right)+1
+			self.res = max(self.res,left+right)
+			return max(left,right)
+		dfs(root)
+		return self.res
