2d1f99ebeaec316c676f8b2ddb71a21dd2589bfa
michaelwng <michaelwangg@qq.com>
2020-02-01 15:04:42 +0800

    增加新实现

--- src/101_200/0119_pascals-triangle-ii/pascals-triangle-ii.py
+++ src/101_200/0119_pascals-triangle-ii/pascals-triangle-ii.py
@@ -15,3 +15,19 @@
 					tmp[j] = pre[j-1]+pre[j]
 			res,pre = tmp,tmp
 		return res
+		
+		
+	# 另一种解法	
+	def getRow(self, rowIndex):
+		"""
+		:type rowIndex: int
+		:rtype: List[int]
+		"""
+		pre,res = 1,[1]
+		for i in xrange(1,rowIndex+1):
+			for j in xrange(1,i):
+				tmp = res[j]
+				res[j] += pre
+				pre = tmp
+			res.append(1)
+		return res
