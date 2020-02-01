05fc1dadad052e8ee220e2e212b16dcddba5e3c3
michaelwng <michaelwangg@qq.com>
2020-02-01 14:33:28 +0800

    杨辉三角

---
+++ src/101_200/0118_pascals-triangle/pascals-triangle.py
@@ -0,0 +1,13 @@
+﻿class Solution(object):
+	def generate(self, numRows):
+		"""
+		:type numRows: int
+		:rtype: List[List[int]]
+		"""
+		res = []
+		for i in xrange(numRows):
+			res.append([1]*(i+1))
+			for j in xrange(i+1):
+				if j>0 and j<i:
+					res[i][j] = res[i-1][j-1]+res[i-1][j]
+		return res
