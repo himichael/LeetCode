721b213fefa2b30e430b8ace38ee2f8d18391a92
michaelwng <michaelwangg@qq.com>
2020-02-03 11:58:30 +0800

    最小路径和修改实现

--- src/1_100/0064_minimum-path-sum/minimum-path-sum.py
+++ src/1_100/0064_minimum-path-sum/minimum-path-sum.py
@@ -3,18 +3,16 @@
 		"""
 		:type grid: List[List[int]]
 		:rtype: int
-		"""		
+		"""
 		if not grid:
 			return 0
-		n,m = len(grid),len(grid[0])
-		if n==1:
-			return sum(grid[0])
+		m = len(grid)
+		n = len(grid[0])
 		for i in xrange(1,m):
-			grid[0][i] = grid[0][i-1] + grid[0][i]
-		for i in xrange(1,n):
-			grid[i][0] = grid[i-1][0] + grid[i][0]
-		for i in xrange(1,n):
-			for j in xrange(1,m):
-				tmp = grid[i][j]
-				grid[i][j] = min(grid[i-1][j]+tmp,grid[i][j-1]+tmp)
+			grid[i][0] += grid[i-1][0]
+		for j in xrange(1,n):
+			grid[0][j] += grid[0][j-1]
+		for i in xrange(1,m):
+			for j in xrange(1,n):
+				grid[i][j] += min(grid[i-1][j], grid[i][j-1])
 		return grid[-1][-1]
