21c4f48df7f0dde7ccadf0d7b6eacaecf571dc8a
michaelwng <michaelwangg@qq.com>
2020-02-05 17:18:53 +0800

    增加解法

--- src/501_600/0518_coin-change-2/coin-change-2.py
+++ src/501_600/0518_coin-change-2/coin-change-2.py
@@ -13,3 +13,27 @@
             for i in xrange(c,amount+1):
                 dp[i] += dp[i-c]
         return dp[-1]
+		
+		
+		
+	# 回溯的解法，超时！！！	
+	def change(self, amount, coins):
+		"""
+		:type amount: int
+		:type coins: List[int]
+		:rtype: int
+		"""			
+		if not coins:
+			return 1 if amount==1 else 0
+		self.res = 0
+		n = len(coins)
+		def f(index,remain):
+			if remain==0:
+				self.res += 1
+				return
+			if index<n and remain<coins[index]:
+				return
+			for i in xrange(index,n):
+				f(i,remain-coins[i])
+		f(0,amount)
+		return self.res
