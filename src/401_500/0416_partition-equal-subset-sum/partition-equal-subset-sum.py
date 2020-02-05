b2588d715fcae98fb25e11b2570069b399e4b067
michaelwng <michaelwangg@qq.com>
2020-02-05 10:07:54 +0800

    修改python实现

--- src/401_500/0416_partition-equal-subset-sum/partition-equal-subset-sum.py
+++ src/401_500/0416_partition-equal-subset-sum/partition-equal-subset-sum.py
@@ -1,36 +1,26 @@
-﻿class Solution {
-	public boolean canPartition(int[] nums) {
-		if(nums==null){
-			return false;
-		}
-		int total = 0;
-		for(int i : nums) {
-			total += i;
-		}
-		if(total%2==1) {
-			return false;
-		}
-		total /= 2;
-		int[][] mem = new int[nums.length][total+1];
-		for(int i=0;i<nums.length;i++) {
-			java.util.Arrays.fill(mem[i],-1);
-		}
-		return dfs(mem,nums,0,total);
-	}
+﻿class Solution(object):
+	def canPartition(self, nums):
+		"""
+		:type nums: List[int]
+		:rtype: bool
+		"""				
+		if not nums:
+			return False
+		total = sum(nums)
+		if total%2==1:
+			return False
+		total /= 2
+		n = len(nums)
+		nums = sorted(nums,reverse=True)
+		def dfs(index, remain):
+			if remain==0:
+				return True
+			if index<n and remain<nums[index]:
+				return False
+			for i in xrange(index,n):
+				if dfs(i+1, remain-nums[i]):
+					return True
+			return False
+		return dfs(0,total)
+		
 	
-	boolean dfs(int[][] mem, int[] nums, int index, int sum) {
-		if(sum==0) {
-			return true;
-		}
-		if(sum<0 || index==nums.length) {
-			return false;
-		}
-		if(mem[index][sum]!=-1) {
-			return mem[index][sum]==1;
-		}
-		boolean a = dfs(mem, nums, index+1, sum-nums[index]);
-		boolean b = dfs(mem, nums, index+1, sum);
-		mem[index][sum] = (a||b)? 1 : 0;
-		return mem[index][sum]==1;
-	}
-}		
