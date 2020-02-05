b99b3281d7525b108563e58f25e3e8250cfd0970
michaelwng <michaelwangg@qq.com>
2020-02-05 10:09:12 +0800

    增加python实现

--- src/401_500/0416_partition-equal-subset-sum/partition-equal-subset-sum.py
+++ src/401_500/0416_partition-equal-subset-sum/partition-equal-subset-sum.py
@@ -1,4 +1,5 @@
 ﻿class Solution(object):
+	# 递归+剪枝
 	def canPartition(self, nums):
 		"""
 		:type nums: List[int]
@@ -24,3 +25,24 @@
 		return dfs(0,total)
 		
 	
+	# 动态规划实现
+	def canPartition(self, nums):
+		"""
+		:type nums: List[int]
+		:rtype: bool
+		"""				
+		if not nums:
+			return False
+		total = 0 
+		for i in nums:
+			total += i
+		if total%2==1:
+			return False
+		total /= 2
+		dp = [False]*(total+1)
+		dp[0] = True
+		for c in nums:
+			for i in xrange(total,c-1,-1):
+				if dp[i-c]:
+					dp[i] = True
+		return dp[-1]
