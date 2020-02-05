8a1b57c1d8d9b89a402c8c4662f6ea0798eccc95
michaelwng <michaelwangg@qq.com>
2020-02-05 10:03:38 +0800

    416增加java实现

---
+++ src/401_500/0416_partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -0,0 +1,36 @@
+﻿class Solution {
+	public boolean canPartition(int[] nums) {
+		if(nums==null){
+			return false;
+		}
+		int total = 0;
+		for(int i : nums) {
+			total += i;
+		}
+		if(total%2==1) {
+			return false;
+		}
+		total /= 2;
+		int[][] mem = new int[nums.length][total+1];
+		for(int i=0;i<nums.length;i++) {
+			java.util.Arrays.fill(mem[i],-1);
+		}
+		return dfs(mem,nums,0,total);
+	}
+	
+	boolean dfs(int[][] mem, int[] nums, int index, int sum) {
+		if(sum==0) {
+			return true;
+		}
+		if(sum<0 || index==nums.length) {
+			return false;
+		}
+		if(mem[index][sum]!=-1) {
+			return mem[index][sum]==1;
+		}
+		boolean a = dfs(mem, nums, index+1, sum-nums[index]);
+		boolean b = dfs(mem, nums, index+1, sum);
+		mem[index][sum] = (a||b)? 1 : 0;
+		return mem[index][sum]==1;
+	}
+}		
