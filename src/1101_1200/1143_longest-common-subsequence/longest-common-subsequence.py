2cae73096859ea50f208c2685845116191f95a83
michaelwng <michaelwangg@qq.com>
2020-02-08 17:08:46 +0800

    最长公共子序列，增加递归-备忘录 实现

--- src/1101_1200/1143_longest-common-subsequence/longest-common-subsequence.py
+++ src/1101_1200/1143_longest-common-subsequence/longest-common-subsequence.py
@@ -15,3 +15,29 @@
 				else:
 					dp[i][j] = max(dp[i-1][j], dp[i][j-1])
 		return dp[-1][-1]
+		
+	# 递归+备忘录
+	def longestCommonSubsequence(self, text1, text2):
+		"""
+		:type text1: str
+		:type text2: str
+		:rtype: int
+		"""		
+		n = len(text1)
+		m = len(text2)
+		mem = [ [-1]*(m) for _ in xrange(n) ]
+		def dfs(i,j):
+			if i==n or j==m:
+				return 0
+			if mem[i][j]>-1:
+				return mem[i][j]
+			if text1[i]==text2[j]:
+				mem[i][j] = dfs(i+1,j+1)+1
+				return mem[i][j]
+				
+			else:
+				a = dfs(i,j+1)
+				b = dfs(i+1,j)
+				mem[i][j] = max(a,b)
+				return mem[i][j]
+		return dfs(0,0)
