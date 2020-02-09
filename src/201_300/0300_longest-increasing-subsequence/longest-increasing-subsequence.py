2115d6666eec3c43673db91f2b6b994e0cfc768e
michaelwng <michaelwangg@qq.com>
2020-02-09 20:38:24 +0800

    增加递归+备忘录实现

--- src/201_300/0300_longest-increasing-subsequence/longest-increasing-subsequence.py
+++ src/201_300/0300_longest-increasing-subsequence/longest-increasing-subsequence.py
@@ -15,3 +15,27 @@
 		
 			res = max(res,dp[i])
 		return res
+		
+		
+
+	def lengthOfLIS(self, nums):
+		"""
+		:type nums: List[int]
+		:rtype: int
+		"""
+		if not nums:
+			return 0
+		n = len(nums)
+		mem = [ [-1]*(n) for _ in xrange(n+1) ]
+		def dfs(pre,cur):
+			if cur==len(nums):
+				return 0
+			if mem[pre+1][cur]>-1:
+				return mem[pre+1][cur]
+			a = 0
+			if pre<0 or nums[cur]>nums[pre]:
+				a = dfs(cur,cur+1)+1
+			b = dfs(pre,cur+1)
+			mem[pre+1][cur] = max(a,b)
+			return mem[pre+1][cur]
+		return dfs(-1,0)
