cefbe51f0c4d23f76cff993c7f8e60aa212ebc69
michaelwng <michaelwangg@qq.com>
2020-02-13 20:28:22 +0800

    买卖股票的最佳时机 II

---
+++ src/101_200/0122_best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
@@ -0,0 +1,41 @@
+﻿class Solution(object):
+	def maxProfit(self, prices):
+		"""
+		:type prices: List[int]
+		:rtype: int
+		"""
+		if not prices:
+			return 0
+		n = len(prices)
+		dp = [ [0]*2 for _ in xrange(n) ]
+		dp[0][0] = 0
+		dp[0][1] = -prices[0]
+		res = 0
+		for i in xrange(1,n):
+			dp[i][0] = max(dp[i-1][0], dp[i-1][1]+prices[i])
+			dp[i][1] = max(dp[i-1][1], dp[i-1][0]-prices[i])
+			res = max(res,dp[i][0],dp[i][1])
+		return res
+		
+	# 递归，超时
+	def maxProfit(self, prices):
+		"""
+		:type prices: List[int]
+		:rtype: int
+		"""
+		if not prices:
+			return 0
+		self.res = 0
+		n = len(prices)
+		def dfs(index,profit,status):
+			if index==n:
+				self.res = max(self.res,profit)
+				return
+			if status:
+				dfs(index+1,profit,status)
+				dfs(index+1,profit+prices[index],0)
+			else:
+				dfs(index+1,profit,status)
+				dfs(index+1,profit-prices[index],1)
+		dfs(0,0,0)
+		return self.res
