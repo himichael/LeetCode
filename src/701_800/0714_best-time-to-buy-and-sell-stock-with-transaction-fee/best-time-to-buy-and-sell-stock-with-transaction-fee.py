3ff1e15c7cf912335984529aac92731744f0070f
michaelwng <michaelwangg@qq.com>
2020-02-16 19:41:09 +0800

    买卖股票的最佳时机含手续费

---
+++ src/701_800/0714_best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.py
@@ -0,0 +1,17 @@
+﻿class Solution(object):
+	#动态规划一维数组
+	def maxProfit(self, prices, fee):
+		"""
+		:type prices: List[int]
+		:type fee: int
+		:rtype: int
+		"""
+		if not prices:
+			return 0
+		n = len(prices)
+		dp0 = 0
+		dp1 = -prices[0]
+		for i in xrange(1,n):
+			dp0 = max(dp0,dp1+prices[i]-fee)
+			dp1 = max(dp1,dp0-prices[i])
+		return max(0,dp0,dp1)
