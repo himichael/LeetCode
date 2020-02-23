4416c6d739aa44a6f4eb56ee5a0603eeb7ebe0e7
michaelwng <michaelwangg@qq.com>
2020-02-23 11:27:47 +0800

    第八题，增加题解

--- src/1_100/0008_String_to_Integer_(atoi)/String_to_Integer_(atoi).py
+++ src/1_100/0008_String_to_Integer_(atoi)/String_to_Integer_(atoi).py
@@ -33,3 +33,30 @@
             return -res
         return res
     
+	
+	
+	# 更符合题意的版本，题目要求存储空间是32位的
+	def myAtoi(self, str):
+		if not str:
+			return 0
+		i = 0
+		size = len(str)
+		while i<size and str[i]==' ':
+			i += 1
+		if i==size:
+			return 0
+		is_negative = True if str[i]=='-' else False
+		res = 0
+		if str[i] in ('-','+'):
+			i += 1
+		while i<size and str[i]>='0' and str[i]<='9':
+			n = ord(str[i])-ord('0')
+			if is_negative and (-res<-214748364 or (-res==-214748364 and n>=8)):
+				return -2147483648
+			if not is_negative and (res>214748364 or (res==214748364 and n>=7)):
+				return 2147483647
+			res = res*10 + n
+			i += 1
+		return -res if is_negative else res	
+	
+	
