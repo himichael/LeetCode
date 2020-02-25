6aad02529391953e0fc5d188a3317793b36f743a
michaelwng <michaelwangg@qq.com>
2020-02-25 23:24:17 +0800

    四数之和，优化代码

--- src/1_100/0018_4sum/4sum.py
+++ src/1_100/0018_4sum/4sum.py
@@ -32,3 +32,47 @@
                         end -=1
                         start += 1
         return res
+		
+		
+		
+		
+	# 优化代码
+	def fourSum(self, nums, target):
+		if not nums:
+			return []
+		nums = sorted(nums)
+		n = len(nums)
+		res = []
+		for k in xrange(n-3):
+			if nums[k]+nums[k+1]+nums[k+2]+nums[k+3]>target:
+				break
+			if k>0 and nums[k-1]==nums[k]:
+				continue
+			for h in xrange(k+1,n-2):
+				if nums[k]+nums[h]+nums[h+1]+nums[h+2]>target:
+					break
+				if h>k+1 and nums[h-1]==nums[h]:
+					continue
+				i = h+1
+				j = n-1
+				while i<j:
+					tmp = nums[i]+nums[j]+nums[k]+nums[h]
+					if tmp>target:
+						j -= 1
+						while i<j and nums[j+1]==nums[j]:
+							j -= 1
+					elif tmp<target:
+						i += 1
+						while i<j and nums[i-1]==nums[i]:
+							i += 1
+					else:
+						res.append([ nums[i],nums[j],nums[k],nums[h] ])
+						i += 1
+						j -= 1
+						while i<j and nums[i-1]==nums[i]:
+							i += 1
+						while i<j and nums[j+1]==nums[j]:
+							j -= 1
+		return res
+		
+		
