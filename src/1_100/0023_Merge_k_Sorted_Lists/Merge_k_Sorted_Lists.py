8da1d180f7c235bafa23f921bccd89bb34745c85
michaelwng <michaelwangg@qq.com>
2020-02-26 20:08:23 +0800

    合并K个有序链表，增加分治实现

--- src/1_100/0023_Merge_k_Sorted_Lists/Merge_k_Sorted_Lists.py
+++ src/1_100/0023_Merge_k_Sorted_Lists/Merge_k_Sorted_Lists.py
@@ -17,3 +17,27 @@
 			p = p.next
 		p.next = None
 		return dummy.next
+		
+		
+		
+	# 分治
+	def mergeKLists(self, lists):
+		if not lists or len(lists)==1:
+			return lists[0] if lists else None
+		def helper(begin,end):
+			if begin==end:
+				return lists[begin]
+			mid = begin+(end-begin)/2
+			left = helper(begin,mid)
+			right = helper(mid+1,end)
+			return merge(left,right)
+		def merge(a,b):
+			if not (a and b):
+				return a if a else b
+			if a.val<=b.val:
+				a.next = merge(a.next,b)
+				return a
+			else:
+				b.next = merge(a,b.next)
+				return b
+		return helper(0,len(lists)-1)
