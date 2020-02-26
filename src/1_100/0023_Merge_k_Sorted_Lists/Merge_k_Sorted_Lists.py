49add9c5f2d95b46236e4b53b110bb9b35629fac
michael <michaelwangg@qq.com>
2020-02-26 15:24:14 +0800

    合并k个排序链表增加python实现

---
+++ src/1_100/0023_Merge_k_Sorted_Lists/Merge_k_Sorted_Lists.py
@@ -0,0 +1,19 @@
+﻿class Solution(object):
+	def mergeKLists(self, lists):
+		if not lists or len(lists)==1:
+			return lists[0] if lists else None
+		import heapq
+		queue = []
+		dummy = ListNode(-1)
+		p = dummy
+		for i in xrange(len(lists)):
+			cur = lists[i]
+			while cur:
+				heapq.heappush(queue,cur.val)
+				cur = cur.next
+		while queue:
+			val = heapq.heappop(queue)
+			p.next = ListNode(val)
+			p = p.next
+		p.next = None
+		return dummy.next
