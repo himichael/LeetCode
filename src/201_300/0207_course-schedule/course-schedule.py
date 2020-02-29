ef175852f23aa63ef16fb4a495a13fe98ff31e3a
michaelwng <michaelwangg@qq.com>
2020-02-29 09:45:35 +0800

    重构代码

--- src/201_300/0207_course-schedule/course-schedule.py
+++ src/201_300/0207_course-schedule/course-schedule.py
@@ -1,14 +1,15 @@
 ﻿class Graph(object):
-	def __init__(self,v):
-		self.vertex = v
-		self.adj = [[] for _ in xrange(self.vertex)]
-	def add_edge(self,s,t):
-		self.adj[s].append(t)
-	def get_vertex_edge(self,v):
-		return self.adj[v]
+	def __init__(self,vertexNum):
+		self.__adjacencyTable = [[] for _ in xrange(vertexNum)]
+	
+	def addRelationOfVertices(self,src,dest):
+		self.__adjacencyTable[src].append(dest)
+	
+	def vertexReference(self,i):
+		return self.__adjacencyTable[i]
 	
 class Solution(object):
-	def canFinish(self, numCourses, prerequisites):
+	def canFinish(self, numCourses, prerequisites):		
 		"""
 		:type numCourses: int
 		:type prerequisites: List[List[int]]
@@ -17,24 +18,30 @@ class Solution(object):
 		if numCourses<=0 or not prerequisites:
 			return True
 		g = Graph(numCourses)
-		for arr in prerequisites:
-			g.add_edge(arr[0],arr[1])
-		
 		in_degree = [0 for _ in xrange(numCourses)]
-		for i in xrange(len(in_degree)):
-			vertexs = g.get_vertex_edge(i)
-			for j in vertexs:
-				in_degree[j] += 1
 		queue = []
+		count = 0
+		
+		for arr in prerequisites:
+			g.addRelationOfVertices(arr[1],arr[0])
+		for i in xrange(numCourses):
+			vertexes = g.vertexReference(i)
+			for j in vertexes:
+				in_degree[j] += 1
+				
 		for i in xrange(len(in_degree)):
 			if in_degree[i]==0:
 				queue.append(i)
+				
 		while queue:
 			i = queue.pop(0)
-			numCourses -= 1
-			vertexs = g.get_vertex_edge(i)
-			for j in vertexs:
-				in_degree[j] -= 1
-				if in_degree[j]==0:
-					queue.append(j)
-		return numCourses==0
+			count += 1
+			vertexes = g.vertexReference(i)
+			for k in vertexes:
+				in_degree[k] -= 1
+				if in_degree[k]==0:
+					queue.append(k)
+		return count==numCourses
+		
+		
+		
