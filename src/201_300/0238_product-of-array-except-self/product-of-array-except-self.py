10ab12740bea9b1977c7c152a0cbb6a2e1837d92
michaelwng <michaelwangg@qq.com>
2020-03-01 18:56:54 +0800

    增加第238题，除自身以外数组的乘积

---
+++ src/201_300/0238_product-of-array-except-self/product-of-array-except-self.py
@@ -0,0 +1,44 @@
+﻿class Solution(object):
+	def productExceptSelf(self, nums):
+		"""
+		:type nums: List[int]
+		:rtype: List[int]
+		"""
+		if not nums:
+			return []
+		n = len(nums)	
+		left = [0 for _ in xrange(n)]
+		right = [0 for _ in xrange(n)]
+		res = [0 for _ in xrange(n)]
+		left[0] = 1
+		right[n-1] = 1
+		for i in xrange(1,n):
+			left[i] = left[i-1]*nums[i-1]
+		for i in xrange(n-2,-1,-1):
+			right[i] = right[i+1]*nums[i+1]
+		for i in xrange(n):
+			res[i] = left[i]*right[i]
+		return res
+		
+		
+		
+	# 另一种解法	
+	def productExceptSelf(self, nums):
+		"""
+		:type nums: List[int]
+		:rtype: List[int]
+		"""
+		if not nums:
+			return []
+		n = len(nums)
+		res = [0 for _ in xrange(n)]
+		k = 1
+		for i in xrange(n):
+			res[i] = k
+			k = k*nums[i]
+		k = 1
+		for i in xrange(n-1,-1,-1):
+			res[i] *= k
+			k = k*nums[i]
+		return res
+		
