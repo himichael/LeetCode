739fb1f8ee4ce7486a31fda33a42b81f7afc1062
michaelwng <michaelwangg@qq.com>
2020-03-05 20:40:17 +0800

    39. 组合总和，增加新的DFS实现方式

--- src/1_100/0039_Combination_Sum/Combination_Sum.py
+++ src/1_100/0039_Combination_Sum/Combination_Sum.py
@@ -19,3 +19,49 @@
                 cur_list.pop()
         recursive(candidates, 0, [], target)
         return res
+		
+		
+			
+	# 优化递归代码
+	def combinationSum(self, candidates, target):
+		if not candidates:
+			return []
+		n = len(candidates)
+		res = []
+		candidates = sorted(candidates)
+		def dfs(index,arr,tmp):
+			if tmp==target:
+				res.append(arr)
+				return
+			for i in xrange(index,n):
+				val = candidates[i]
+				if tmp+val>target or val>target:
+					break
+				dfs(i,arr+[val],tmp+val)
+		dfs(0,[],0)
+		return res
+		
+		
+		
+	# 另一种DFS实现
+	def combinationSum(self, candidates, target):
+		if not candidates:
+			return []
+		n = len(candidates)
+		res = []
+		candidates = sorted(candidates)
+		def dfs(index,arr,tmp):
+			if tmp==target:
+				res.append(arr)
+				return
+			if index==n:
+				return
+			val = candidates[index]
+			if val>target or tmp+val>target:
+				return 
+			dfs(index,arr+[val],tmp+val)
+			dfs(index+1,arr,tmp)
+		dfs(0,[],0)
+		return res
+		
+		
