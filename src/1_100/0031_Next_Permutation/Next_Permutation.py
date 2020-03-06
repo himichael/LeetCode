aa1538878c223b41fa461fd794046d17f44a6824
michael <michaelwangg@qq.com>
2020-03-06 18:44:59 +0800

    重构31题代码

--- src/1_100/0031_Next_Permutation/Next_Permutation.py
+++ src/1_100/0031_Next_Permutation/Next_Permutation.py
@@ -22,3 +22,34 @@
 			nums[p],nums[q] = nums[q],nums[p]
 			p += 1
 			q -= 1
+	
+
+	
+	# 重构代码
+	def nextPermutation(self, nums):
+		"""
+		:type nums: List[int]
+		:rtype: None Do not return anything, modify nums in-place instead.
+		"""
+		if not nums:
+			return
+		first = -1
+		second = -1
+		n = len(nums)
+		for i in xrange(n-2,-1,-1):
+			if nums[i]<nums[i+1]:
+				first = i
+				break
+		if first>=0:
+			for i in xrange(n-1,first,-1):
+				if nums[i]>nums[first]:
+					second = i
+					break
+		nums[first],nums[second] = nums[second],nums[first]
+		self.reverse(nums,first+1,n-1)
+	
+	def reverse(self,nums,a,b):
+		i,j = a,b
+		while i<j:
+			nums[i],nums[j] = nums[j],nums[i]
+			i,j = i+1,j-1
