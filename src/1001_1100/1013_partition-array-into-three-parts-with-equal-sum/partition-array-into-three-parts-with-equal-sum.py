991aeb531d482fcdd329fa53579f83b34c1220e2
michael <michaelwangg@qq.com>
2020-03-11 09:49:42 +0800

    增加一题，将数组分成和相等的三个部分

---
+++ src/1001_1100/1013_partition-array-into-three-parts-with-equal-sum/partition-array-into-three-parts-with-equal-sum.py
@@ -0,0 +1,17 @@
+﻿class Solution(object):
+	def canThreePartsEqualSum(self, A):
+		if not A:
+			return False
+		total = 0
+		for i in A:
+			total += i
+		if total%3!=0:
+			return False
+		s = 0
+		count = 0
+		for i in A:
+			s += i
+			if s==total/3:
+				count += 1
+				s = 0
+		return count>=3
