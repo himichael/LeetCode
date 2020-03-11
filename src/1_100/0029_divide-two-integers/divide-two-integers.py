f2f8df130fac51090546b2f7fac83c1f3cb6b070
michaelwng <michaelwangg@qq.com>
2020-03-11 19:20:18 +0800

    修改29题，两数相除，增加正确的实现

--- src/1_100/0029_divide-two-integers/divide-two-integers.py
+++ src/1_100/0029_divide-two-integers/divide-two-integers.py
@@ -1,18 +1,30 @@
-﻿class Solution(object):
-    def divide(self, dividend, divisor):
-        """
-        :type dividend: int
-        :type divisor: int
-        :rtype: int
-        """
-        res = 0
-        if( (dividend>0 and divisor<0) or (dividend<0 and divisor>0) ):
-            res = -(abs(dividend)/abs(divisor))
-        else:
-            res= abs(dividend)/abs(divisor)
-        if(res < -2147483648):
-            return -2147483648
-        elif(res > 2147483647):
-            return 2147483647
-        else:
-            return res
+﻿class Solution(object):		
+	def divide(self, dividend, divisor):
+		"""
+		:type dividend: int
+		:type divisor: int
+		:rtype: int
+		"""
+		sign = (dividend>0) ^ (divisor>0)
+		res = 0
+		if divisor==1:
+			return dividend
+		if divisor==-1:
+			if dividend==(-2**31):
+				return 2**31-1
+			return -dividend
+		if dividend>0:
+			dividend = -dividend
+		if divisor>0:
+			divisor = -divisor
+		while dividend<=divisor:
+			tmp_res = 1
+			tmp = divisor
+			while dividend<(tmp<<1):
+				tmp = tmp<<1
+				tmp_res = tmp_res<<1
+			res += tmp_res
+			dividend -= tmp
+		if not sign:
+			return res
+		return -res
