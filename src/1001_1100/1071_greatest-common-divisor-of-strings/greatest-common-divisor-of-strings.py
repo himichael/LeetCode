953d73e3c58753111e54b0fe9f2e24825007ce15
michael <michaelwangg@qq.com>
2020-03-12 10:28:13 +0800

    字符串的最大公因子，增加新解法

--- src/1001_1100/1071_greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.py
+++ src/1001_1100/1071_greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.py
@@ -13,3 +13,18 @@
 			return ""
 		gdc_size = gdc(len(str1),len(str2))
 		return str1[:gdc_size]
+		
+		
+		
+	# 另一种解法
+	def gcdOfStrings(self, str1, str2):
+		if (str1+str2)!=(str2+str1):
+			return ""
+		diff = len(str1)-len(str2)
+		if diff==0:
+			return str1
+		elif diff>0:
+			str1 = str1[len(str2):]
+		else:
+			str2 = str2[len(str1):]
+		return self.gcdOfStrings(str1,str2)
