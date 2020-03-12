c23e9f4ad03601997f5d4be2a9601ec42c93ddbe
michael <michaelwangg@qq.com>
2020-03-12 09:57:25 +0800

    增加一题，字符串的最大公因子

---
+++ src/1001_1100/1071_greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.py
@@ -0,0 +1,15 @@
+﻿class Solution(object):
+	def gcdOfStrings(self, str1, str2):
+		"""
+		:type str1: str
+		:type str2: str
+		:rtype: str
+		"""
+		def gdc(a,b):
+			if not b:
+				return a
+			return gdc(b,a%b)
+		if (str1+str2)!=(str2+str1):
+			return ""
+		gdc_size = gdc(len(str1),len(str2))
+		return str1[:gdc_size]
