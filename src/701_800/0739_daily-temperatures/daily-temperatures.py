69536fd962acfcd9a4a581414231b186760353be
michaelwng <michaelwangg@qq.com>
2020-03-13 20:43:46 +0800

    增加一题，每日温度

---
+++ src/701_800/0739_daily-temperatures/daily-temperatures.py
@@ -0,0 +1,17 @@
+﻿class Solution(object):
+	def dailyTemperatures(self, T):
+		"""
+		:type T: List[int]
+		:rtype: List[int]
+		"""
+		if not T:
+			return []
+		n = len(T)
+		res = [0 for _ in xrange(n)]
+		stack = []
+		for i in xrange(n):
+			while stack and T[i]>T[stack[-1]]:
+				res[stack[-1]] = i-stack[-1]
+				stack.pop()
+			stack.append(i)
+		return res
