c7df0ea64a1f86a8edc9b7c9f20545970fa3c86c
michaelwng <michaelwangg@qq.com>
2020-03-14 16:01:57 +0800

    每日温度，增加一种题解

--- src/701_800/0739_daily-temperatures/daily-temperatures.py
+++ src/701_800/0739_daily-temperatures/daily-temperatures.py
@@ -15,3 +15,18 @@
 				stack.pop()
 			stack.append(i)
 		return res
+		
+
+
+	# O(N^2)解法，超时
+	def dailyTemperatures(self, T):
+		if not T:
+			return []
+		n = len(T)
+		res = [0 for _ in xrange(n)]
+		for i in xrange(n-1):
+			for j in xrange(i,n):
+				if T[j]>T[i]:
+					res[i] = j-i
+					break
+		return res
