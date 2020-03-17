55fe14a60c808f1b036b2474e4e80d9557480bb3
michael <michaelwangg@qq.com>
2020-03-17 10:08:18 +0800

    增加一题，拼写单词

---
+++ src/1101_1200/1160_find-words-that-can-be-formed-by-characters/find-words-that-can-be-formed-by-characters.py
@@ -0,0 +1,30 @@
+﻿class Solution(object):
+	def countCharacters(self, words, chars):
+		"""
+		:type words: List[str]
+		:type chars: str
+		:rtype: int
+		"""
+		if not (words and chars):
+			return 0
+		res = 0
+		d = dict()
+		for c in chars:
+			if c not in d:
+				d[c] = 1
+			else:
+				d[c] += 1
+		for word in words:
+			tmp = dict(d)
+			is_ok = True
+			for c in word:
+				if c in tmp:
+					tmp[c] -= 1
+					if tmp[c]==0:
+						del tmp[c]
+				else:
+					is_ok = False
+					break
+			if is_ok:
+				res += len(word)
+		return res
