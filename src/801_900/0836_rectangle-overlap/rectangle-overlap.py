aae9f21ca31d3c784d7b09f702a5bfb6f55f99c1
michael <michaelwangg@qq.com>
2020-03-18 11:29:51 +0800

    增加一题，矩形重叠

---
+++ src/801_900/0836_rectangle-overlap/rectangle-overlap.py
@@ -0,0 +1,10 @@
+﻿class Solution(object):
+	def isRectangleOverlap(self, rec1, rec2):
+		"""
+		:type rec1: List[int]
+		:type rec2: List[int]
+		:rtype: bool
+		"""
+		x = not (rec1[2]<=rec2[0] or rec2[2]<=rec1[0])
+		y = not (rec1[3]<=rec2[1] or rec2[3]<=rec1[1])
+		return x and y
