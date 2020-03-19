69a3b3f4429f4a1a9e0bfbd897925e1c792df655
michael <michaelwangg@qq.com>
2020-03-19 10:50:10 +0800

    最长回文串精简代码

--- src/401_500/0409_longest-palindrome/longest-palindrome.py
+++ src/401_500/0409_longest-palindrome/longest-palindrome.py
@@ -18,3 +18,17 @@
 				res += d[i]-1
 				is_include_even = True
 		return res+1 if is_include_even else res
+		
+		
+		
+	# 精简实现	
+	def longestPalindrome(self, s):
+		if not s:
+			return 0		
+		d = dict()
+		res = 0
+		for i in s:
+			d[i] = d.setdefault(i,0)+1
+		for i in d:
+			res += d[i]-(d[i]&1)
+		return res+1 if res<len(s) else res
