cc8ea431cfd6e25d2660b8c976e679062fdf30dc
michaelwng <michaelwangg@qq.com>
2020-03-21 14:08:39 +0800

    增加 水壶问题，前 K 个高频元素 题目索引

---
+++ src/301_400/0347_top-k-frequent-elements/top-k-frequent-elements.py
@@ -0,0 +1,19 @@
+﻿class Solution(object):
+	def topKFrequent(self, nums, k):
+		"""
+		:type nums: List[int]
+		:type k: int
+		:rtype: List[int]
+		"""		
+		if len(nums)<=k:
+			return nums
+		d = dict()
+		for c in nums:
+			if c not in d:
+				d[c] = [1,c]
+			else:
+				d[c][0] += 1
+		values = d.values()
+		values.sort(key=lambda x:x[0],reverse=True)
+		res = [values[i][1] for i in xrange(k)]
+		return res
