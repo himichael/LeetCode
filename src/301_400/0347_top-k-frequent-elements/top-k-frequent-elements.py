fb4f64246a10bb01087bfc4b51d0b358395fe5ee
michaelwng <michaelwangg@qq.com>
2020-03-22 20:47:06 +0800

    第347题，增加桶排序解法

--- src/301_400/0347_top-k-frequent-elements/top-k-frequent-elements.py
+++ src/301_400/0347_top-k-frequent-elements/top-k-frequent-elements.py
@@ -17,3 +17,28 @@
 		values.sort(key=lambda x:x[0],reverse=True)
 		res = [values[i][1] for i in xrange(k)]
 		return res
+		
+
+
+	# 桶排序
+	def topKFrequent(self, nums, k):
+		if not nums or k<=0:
+			return []
+		if len(nums)<=k:
+			return nums
+		d = dict()
+		for i in nums:
+			d[i] = d.setdefault(i,0)+1
+		arr = [[] for _ in xrange(len(nums)+1)]
+		for key in d.keys():
+			i = d[key]
+			arr[i].append(key)
+		res = []
+		j = len(arr)-1
+		while j>=0 and len(res)<k:
+			if len(arr[j])>0:
+				res.extend(arr[j])
+			j -= 1
+		return res
+		
+		
