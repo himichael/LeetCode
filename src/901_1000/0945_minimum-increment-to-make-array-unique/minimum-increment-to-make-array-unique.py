6951d5c501941b0f56f75c40860dd790ec41286b
michaelwng <michaelwangg@qq.com>
2020-03-22 12:30:52 +0800

    增加一题，使数组唯一的最小增量

---
+++ src/901_1000/0945_minimum-increment-to-make-array-unique/minimum-increment-to-make-array-unique.py
@@ -0,0 +1,19 @@
+﻿class Solution(object):
+	def minIncrementForUnique(self, A):
+		"""
+		:type A: List[int]
+		:rtype: int
+		"""
+		if not A or len(A)<=1:
+			return 0
+		A = sorted(A)
+		count = 0
+		pre = A[0]
+		for i in xrange(1,len(A)):
+			if A[i]<=pre:
+				increment = pre-A[i]+1
+				count += increment
+				pre = A[i] + increment
+			else:
+				pre = A[i]
+		return count
