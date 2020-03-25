f04389a3acce06f7d854cb278033833a3ba71d12
michael <michaelwangg@qq.com>
2020-03-25 12:37:27 +0800

    增加一题，三维形体的表面积

---
+++ src/801_900/0892_surface-area-of-3d-shapes/surface-area-of-3d-shapes.py
@@ -0,0 +1,25 @@
+﻿class Solution(object):
+	def uncommonFromSentences(self, A, B):
+		"""
+		:type A: str
+		:type B: str
+		:rtype: List[str]
+		"""
+		if not (A or B):
+			return []
+		d = dict()
+		res = []
+		for i in A.split(" "):
+			if i not in d:
+				d[i] = 1
+			else:
+				d[i] += 1
+		for i in B.split(" "):
+			if i not in d:
+				d[i] = 1
+			else:
+				d[i] += 1
+		for i in d:
+			if d[i]==1:
+				res.append(i)
+		return res
