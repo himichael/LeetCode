e7e7854d136a952b6d1fcb8ab0afda70b8daba45
michael <michaelwangg@qq.com>
2020-03-27 13:39:40 +0800

    增加一题，卡牌分组

---
+++ src/901_1000/0914_x-of-a-kind-in-a-deck-of-cards/x-of-a-kind-in-a-deck-of-cards.py
@@ -0,0 +1,13 @@
+﻿class Solution(object):
+	def hasGroupsSizeX(self, deck):
+		if not deck or len(deck)<2:
+			return False
+		N = len(deck)
+		count = [0 for _ in xrange(10000)]
+		for i in deck:
+			count[i] += 1
+		for x in xrange(2,N+1):
+			if N%x==0:
+				if all(v%x==0 for v in count):
+					return True
+		return False
