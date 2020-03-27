76c28b0db2583f5ae1bb85e494dadc2c33ba11ef
michael <michaelwangg@qq.com>
2020-03-27 13:55:57 +0800

    增加914题，卡牌实现的新实现

--- src/901_1000/0914_x-of-a-kind-in-a-deck-of-cards/x-of-a-kind-in-a-deck-of-cards.py
+++ src/901_1000/0914_x-of-a-kind-in-a-deck-of-cards/x-of-a-kind-in-a-deck-of-cards.py
@@ -11,3 +11,27 @@
 				if all(v%x==0 for v in count):
 					return True
 		return False
+		
+		
+		
+	# 最大公约数实现	
+	def hasGroupsSizeX(self, deck):
+		if not deck or len(deck)<2:
+			return False
+		def gdc(a,b):
+			return a if not b else gdc(b,a%b)
+		N = len(deck)
+		count = [0 for _ in xrange(10000)]
+		res = -1
+		for i in deck:
+			count[i] += 1
+		for i in xrange(10000):
+			if count[i]>0:
+				if res==-1:
+					res = count[i]
+				else:
+					res = gdc(res,count[i])
+		return res>=2
+		
+		
+		
