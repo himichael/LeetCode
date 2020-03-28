f0e4d5545176e5c0680c7c750595754676ea61f1
michaelwng <michaelwangg@qq.com>
2020-03-28 13:51:29 +0800

    820题精简代码

--- src/801_900/0820_short-encoding-of-words/short-encoding-of-words.py
+++ src/801_900/0820_short-encoding-of-words/short-encoding-of-words.py
@@ -38,3 +38,34 @@ class Solution(object):
                 self.res.add(cur_word+c)
                 continue
             self.iter(node[c],cur_word+c)
+			
+			
+			
+			
+			
+			
+			
+	# 精简代码
+    def minimumLengthEncoding(self, words):
+        if not words:
+            return 0
+        root = {}
+        self.res = 0
+        def insert(word):
+            node = root
+            for c in word:
+                node = node.setdefault(c,{})
+        def iteratorTree(node,index):
+            if not node:
+                self.res += index +1
+                return
+            for n in node.values():
+                iteratorTree(n,index+1)
+        for word in words:
+            insert(word[::-1])
+        for node in root.values():
+            iteratorTree(node,1)
+        return self.res
+			
+			
+			
