4fc698bdaf3c88d57bf04deaea8b9a1c23256232
michael <michaelwangg@qq.com>
2020-03-30 13:49:32 +0800

    第892题，三维形体的表面积 增加一种解法

--- src/801_900/0892_surface-area-of-3d-shapes/surface-area-of-3d-shapes.py
+++ src/801_900/0892_surface-area-of-3d-shapes/surface-area-of-3d-shapes.py
@@ -15,3 +15,24 @@
 							tmp = 0
 						res += max(grid[i][j]-tmp,0)
 		return res
+		
+		
+	# 做减法的思路，求出总表面，再减去未露出的面
+	def surfaceArea(self, grid):
+		if not grid:
+			return 0
+		N = len(grid)
+		cubes = 0
+		faces = 0
+		for i in xrange(N):
+			for j in xrange(N):
+				cubes += grid[i][j]
+				if grid[i][j]>0:
+					faces += grid[i][j]-1
+				if i>0:
+					faces += min(grid[i-1][j],grid[i][j])
+				if j>0:
+					faces += min(grid[i][j-1],grid[i][j])
+		return 6*cubes - 2*faces
+		
+		
