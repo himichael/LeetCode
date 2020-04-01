d098cbf3d8f96c9da3c422f76e1fac6f0e0aa1b2
michael <michaelwangg@qq.com>
2020-04-01 12:09:31 +0800

    增加一题，有效括号的嵌套深度

---
+++ src/1101_1200/1111_maximum-nesting-depth-of-two-valid-parentheses-strings/maximum-nesting-depth-of-two-valid-parentheses-strings.py
@@ -0,0 +1,40 @@
+﻿class Solution(object):
+	def maxDepthAfterSplit(self, seq):
+		"""
+		:type seq: str
+		:rtype: List[int]
+		"""	
+		if not seq:
+			return []
+		N = len(seq)
+		ans = []
+		idx = 1
+		for c in seq:
+			if c=="(":
+				idx += 1
+				ans.append(idx%2)
+			else:
+				ans.append(idx%2)
+				idx -= 1
+		return ans
+		
+		
+		
+	# 另一种实现	
+	def maxDepthAfterSplit(self, seq):
+		if not seq:
+			return []
+		N = len(seq)
+		ans = [0 for i in xrange(N)]
+		idx = 0
+		for c in seq:
+			if c=="(":
+				ans[idx] = idx&1
+			else:
+				ans[idx] = (idx+1)&1
+			idx += 1
+		return ans	
+
+
+
+		
