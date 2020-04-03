5da85955080fa29903e33e300aa388b9c8e20dc0
michaelwng <michaelwangg@qq.com>
2020-04-03 23:05:28 +0800

    第215题，精简代码

--- src/201_300/0215_kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
+++ src/201_300/0215_kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
@@ -29,3 +29,35 @@
 			else:
 				return select(left,poivt_index-1,k_smallest)
 		return select(0,len(nums)-1, len(nums)-k)
+		
+		
+		
+	# 精简代码
+	def findKthLargest(self, nums, k):
+		import random
+		k_th = len(nums)-k
+		def partition(begin,end):
+			idx = random.randint(begin,end)
+			nums[idx],nums[end] = nums[end],nums[idx]
+			povit = nums[end]
+			j = begin
+			for i in xrange(begin,end):
+				if nums[i]<povit:
+					nums[i],nums[j] = nums[j],nums[i]
+					j += 1
+			nums[j],nums[end] = nums[end],nums[j]
+			return j
+		def select(begin,end):
+			if begin==end:
+				return nums[begin]
+			pos = partition(begin,end)
+			if pos==k_th:
+				return nums[k_th]
+			elif pos>k_th:
+				return select(begin,pos-1)
+			else:
+				return select(pos+1,end)
+		return select(0,len(nums)-1)
+		
+		
+		
