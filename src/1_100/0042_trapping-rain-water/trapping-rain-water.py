645820c369f8f2d285cbb08c1c8cb7bd179ea8c3
michaelwng <michaelwangg@qq.com>
2020-04-04 18:02:55 +0800

    增加第42题的新解法

--- src/1_100/0042_trapping-rain-water/trapping-rain-water.py
+++ src/1_100/0042_trapping-rain-water/trapping-rain-water.py
@@ -22,3 +22,26 @@
                     res += cur_min - nums[right]
                     right -= 1
         return res
+		
+		
+	# 单调栈解法
+	def trap(self, height):
+		if not height:
+			return 0
+		stack = []
+		ans = 0
+		for i in xrange(len(height)):
+			while stack and height[stack[-1]]<height[i]:
+				pop_idx = stack.pop()
+				while stack and height[stack[-1]]==height[pop_idx]:
+					stack.pop()
+				if stack:
+					peek_idx = stack[-1]
+					diff = min(height[peek_idx],height[i])-height[pop_idx]
+					distance = i-peek_idx-1
+					ans += diff*distance
+			stack.append(i)
+		return ans	
+		
+		
+		
