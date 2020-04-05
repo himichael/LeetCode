5c1942671edf2583b9bdcae1b28161f6b1424436
michaelwng <michaelwangg@qq.com>
2020-04-05 13:21:37 +0800

    增加第42题，双指针实现

--- src/1_100/0042_trapping-rain-water/trapping-rain-water.py
+++ src/1_100/0042_trapping-rain-water/trapping-rain-water.py
@@ -79,5 +79,29 @@
 		return ans
 
 
-		
+
+	# 双指针
+	def trap(self, height):
+		if not height:
+			return 0
+		N = len(height)
+		ans = 0
+		left_max = 0
+		right_max = 0
+		left = 0
+		right = N-1
+		while left<right:
+			if height[left]<height[right]:
+				if height[left]>=left_max:
+					left_max = height[left]
+				else:
+					ans += left_max-height[left]
+				left += 1
+			else:
+				if height[right]>=right_max:
+					right_max = height[right]
+				else:
+					ans += right_max-height[right]
+				right -= 1
+		return ans
 		
