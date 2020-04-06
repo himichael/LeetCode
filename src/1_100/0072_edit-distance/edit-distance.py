c78e61973933ed11c4878e1b2e81d79a344e9ff9
michaelwng <michaelwangg@qq.com>
2020-04-06 10:00:40 +0800

    增加4月打卡题目，编辑距离

--- src/1_100/0072_edit-distance/edit-distance.py
+++ src/1_100/0072_edit-distance/edit-distance.py
@@ -20,14 +20,28 @@
 					dp[i][j] = min(dp[i-1][j],dp[i][j-1],dp[i-1][j-1])+1
 		return dp[-1][-1]
 		
+
+
+	# 递归(超时)
+	def minDistance(self, word1, word2):
+		N = len(word1)
+		M = len(word2)
+		def dfs(i,j):
+			if i==N or j==M:
+				return N-i + M-j
+			if word1[i]==word2[j]:
+				return dfs(i+1,j+1)
+			else:
+				a = dfs(i+1,j)
+				b = dfs(i,j+1)
+				c = dfs(i+1,j+1)
+				return min(a,b,c)+1
+		return dfs(0,0)
 		
+
+
 	# 递归+备忘录模式	
 	def minDistance(self, word1, word2):
-		"""
-		:type word1: str
-		:type word2: str
-		:rtype: int
-		"""	
 		n = len(word1)
 		m = len(word2)
 		mem = [ [-1]*m for _ in xrange(n) ]
@@ -46,3 +60,23 @@
 				mem[i][j] = min(a,b,c)+1
 				return mem[i][j]
 		return f(0,0)
+
+
+	# 动态规划，另一种初始化方式
+	def minDistance(self, word1, word2):
+		n = len(word1)
+		m = len(word2)
+		dp = [[-1 for _ in xrange(m+1)] for _ in xrange(n+1)]
+		for i in xrange(n+1):
+			dp[i][0] = i
+		for j in xrange(m+1):
+			dp[0][j] = j
+		for i in xrange(1,n+1):
+			for j in xrange(1,m+1):
+				if word1[i-1]==word2[j-1]:
+					dp[i][j] = dp[i-1][j-1]
+				else:
+					dp[i][j] = min(dp[i][j-1],dp[i-1][j],dp[i-1][j-1])+1
+		return dp[-1][-1]
+
+
