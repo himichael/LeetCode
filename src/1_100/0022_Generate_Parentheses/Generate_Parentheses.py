f879e383615eb774968b974780561a37709fc2a2
michaelwng <michaelwangg@qq.com>
2020-04-09 08:28:47 +0800

    22括号生成，增加BFS实现

--- src/1_100/0022_Generate_Parentheses/Generate_Parentheses.py
+++ src/1_100/0022_Generate_Parentheses/Generate_Parentheses.py
@@ -18,20 +18,39 @@
         recursion(0, 0, "")
         return res
 		
-    def generateParenthesis_2(self, n):
-        """
-        :type n: int
-        :rtype: List[str]
-        """
-        res = []
-        def recursion(left,right,content):
-            print "left->"+str(left)+"  right->"+str(right)+" content->"+content
-            if(left==n and right==n):
-                res.append(content)
-                return
-            if(left < n):
-                recursion(left+1, right, content+"(")
-            if(left>right and right<n):
-                recursion(left, right+1, content+")")
-        recursion(0,0,"")
-        return res
+		
+		
+	# 简化的 DFS实现
+	def generateParenthesis(self, n):
+		if n<=0:
+			return []
+		res = []
+		def dfs(left,right,tmp):
+			if left==n and right==n:
+				res.append(tmp)
+				return
+			if left<n:
+				dfs(left+1,right,tmp+"(")
+			if right<left and right<n:
+				dfs(left,right+1,tmp+")")
+		dfs(0,0,"")
+		return res	
+	
+	
+	
+	# BFS实现
+	def generateParenthesis(self, n):
+		if n<=0:
+			return []
+		res = []
+		queue = [(0,0,"")]
+		while queue:
+			left,right,tmp = queue.pop(0)
+			if left==n and right==n:
+				res.append(tmp)
+				continue
+			if left<n:
+				queue.append((left+1,right,tmp+"("))
+			if right<left and right<n:
+				queue.append((left,right+1,tmp+")"))
+		return res
