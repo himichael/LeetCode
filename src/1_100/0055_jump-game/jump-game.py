a42f5606e14e75d5e905fd4830ca3f59b9799638
michael <michaelwangg@qq.com>
2020-04-17 10:05:53 +0800

    增加打卡题，跳跃游戏

---
+++ src/1_100/0055_jump-game/jump-game.py
@@ -0,0 +1,12 @@
+﻿class Solution(object):
+	def canJump(self, nums):
+		if not nums:
+			return True
+		max_jump = 0
+		n = len(nums)
+		for i in xrange(n):
+			if max_jump>=i:
+				max_jump = max(max_jump,i+nums[i])
+		if max_jump>=n-1:
+			return True
+		return False
