aa2e59861b849d2a65bd87209f88eab86fa0c4fb
michael <michaelwangg@qq.com>
2020-04-17 10:53:07 +0800

    第55题，增加两种实现

--- src/1_100/0055_jump-game/jump-game.py
+++ src/1_100/0055_jump-game/jump-game.py
@@ -10,3 +10,54 @@
 		if max_jump>=n-1:
 			return True
 		return False
+		
+		
+		
+	# 递归(超时)
+	def canJump(self, nums):
+		if not nums:
+			return True
+		n = len(nums)
+		def dfs(index):
+			if index==n-1:
+				return True
+			jump = nums[index]+index
+			if jump>n-1:
+				jump = n-1
+			for i in xrange(index+1,jump+1):
+				if dfs(i):
+					return True
+			return False
+		return dfs(0)
+		
+		
+		
+	# 递归+记忆化(超时)	
+	def canJump(self, nums):
+		if not nums:
+			return True
+		n = len(nums)
+		cache = dict()
+		def dfs(index):
+			if index==n-1:
+				return True
+			if index in cache:
+				return cache[index]
+			jump = nums[index]+index
+			if jump>n-1:
+				jump = n-1
+			for i in xrange(index+1,jump+1):
+				if dfs(i):
+					cache[i] = True
+					return cache[i]
+			cache[index] = False
+			return cache[index]
+		return dfs(0)	
+
+
+
+
+		
+		
+		
+		
