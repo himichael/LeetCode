8d96bd4ff220e5eb958f2cf95f0a1ff284f3a722
michael <michaelwangg@qq.com>
2020-04-21 09:54:47 +0800

    增加一题，统计「优美子数组」

---
+++ src/1201_1300/1248_count-number-of-nice-subarrays/count-number-of-nice-subarrays.py
@@ -0,0 +1,21 @@
+﻿class Solution(object):
+	def numberOfSubarrays(self, nums, k):
+		"""
+		:type nums: List[int]
+		:type k: int
+		:rtype: int
+		"""
+		if not nums or k<=0:
+			return 0
+		n = len(nums)
+		dp = [0]*(n+1)
+		dp[0] = 1
+		ans = 0
+		odd = 0
+		for num in nums:
+			if num%2==1:
+				odd += 1
+			if odd>=k:
+				ans += dp[odd-k]
+			dp[odd] += 1
+		return ans
