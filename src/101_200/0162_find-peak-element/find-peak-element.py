7c92df9cb7adaa86edaa50a4bdf251e0aba14d52
michael <michaelwangg@qq.com>
2020-04-29 13:53:59 +0800

    第162题的另一种解法

--- src/101_200/0162_find-peak-element/find-peak-element.py
+++ src/101_200/0162_find-peak-element/find-peak-element.py
@@ -11,5 +11,23 @@
 				end = mid
 			else:
 				begin = mid+1
-	
 		return begin
+		
+		
+	# 另一种写法	
+    def findPeakElement(self, nums):
+        if not nums or len(nums)==1:
+            return -1 if not nums else 0
+        n = len(nums)
+        begin = 0
+        end = n-1
+        while begin<=end:
+            mid = begin+(end-begin)//2
+            if mid==n-1:
+                begin = mid
+                break
+            if nums[mid]>nums[mid+1]:
+                end = mid-1
+            else:
+                begin = mid+1
+        return begin
