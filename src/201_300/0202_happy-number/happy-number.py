c069c27cb84ee51d54c071550e12e97783537dd7
michael <michaelwangg@qq.com>
2020-04-30 10:46:45 +0800

    增加一题，快乐数

---
+++ src/201_300/0202_happy-number/happy-number.py
@@ -0,0 +1,20 @@
+﻿class Solution(object):
+	def isHappy(self, n):
+		"""
+		:type n: int
+		:rtype: bool
+		"""		
+		d = dict()
+		def cacl_sum(i):
+			num = 0
+			while i>0:
+				num = num + (i%10)*(i%10)
+				i //= 10
+			return num
+		d[n] = 1
+		while d[n]<2:
+			if n==1:
+				return True
+			n = cacl_sum(n)
+			d[n] = d.setdefault(n,0) + 1
+		return False
