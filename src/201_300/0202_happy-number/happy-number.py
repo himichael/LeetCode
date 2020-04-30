868f65acbbed1a67896378a0c714f2deeedc9fee
michael <michaelwangg@qq.com>
2020-04-30 11:00:33 +0800

    第202题，快乐数字增加一种解法

--- src/201_300/0202_happy-number/happy-number.py
+++ src/201_300/0202_happy-number/happy-number.py
@@ -18,3 +18,13 @@
 			n = cacl_sum(n)
 			d[n] = d.setdefault(n,0) + 1
 		return False
+		
+		
+		
+	# 另一种解法	
+	def isHappy(self, n):	
+		s = set()
+		while n not in s:
+			s.add(n)
+			n = sum(int(i)**2 for i in str(n))
+		return n==1
