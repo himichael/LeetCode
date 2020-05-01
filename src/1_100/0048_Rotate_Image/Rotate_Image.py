81444bec709acb8ece74c791412cd67d5c7ad20c
michaelwng <michaelwangg@qq.com>
2020-05-01 22:30:02 +0800

    第48题增加一种解法

--- src/1_100/0048_Rotate_Image/Rotate_Image.py
+++ src/1_100/0048_Rotate_Image/Rotate_Image.py
@@ -14,3 +14,22 @@
 				
 	def rotate(self, matrix):
 		matrix[:] = zip(*reversed(matrix))
+		
+		
+		
+	# 另一种原地解法
+	def rotate(self, matrix):
+		if not matrix:
+			return []
+		n = len(matrix)
+		r = n//2-1
+		c = (n-1)//2
+		a = matrix
+		for i in xrange(r+1):
+			for j in xrange(c+1):
+				a[i][j],a[j][n-i-1] = a[j][n-i-1],a[i][j]
+				a[i][j],a[n-i-1][n-j-1] = a[n-i-1][n-j-1],a[i][j]
+				a[i][j],a[n-j-1][i] = a[n-j-1][i],a[i][j]
+				
+				
+				
