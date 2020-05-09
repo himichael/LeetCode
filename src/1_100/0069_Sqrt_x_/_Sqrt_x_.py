9bbc794eb8b82f341d1c4bf59b2585f94973b974
michael <michaelwangg@qq.com>
2020-05-09 12:56:41 +0800

    第69题增加一种解法

--- src/1_100/0069_Sqrt_x_/_Sqrt_x_.py
+++ src/1_100/0069_Sqrt_x_/_Sqrt_x_.py
@@ -33,3 +33,29 @@
             else:
                 begin = mid
         return begin
+		
+		
+		
+	# <= 这种二分写法	
+	def mySqrt(self, x):
+		if x<=0:
+			return 0
+		begin = 0
+		end = x
+		while begin<=end:
+			mid = begin+(end-begin)//2
+			if mid*mid > x:
+				end = mid-1
+			else:
+				begin = mid+1
+		return begin-1
+		
+		
+		
+	# 数学公式 e^(0.5 * lnx)
+    def mySqrt(self, x):
+        if x<=0:
+            return 0
+        ans = int( math.exp(0.5*math.log(x)) )
+        return ans+1 if (ans+1)**2 <=x else ans	
+		
