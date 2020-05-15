120523c5e8e480ec86c3e1da8677b7a9dbd457e2
michael <michaelwangg@qq.com>
2020-05-15 12:55:19 +0800

    增加5月打卡题目，和为K的子数组

---
+++ src/501_600/0560_subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -0,0 +1,43 @@
+﻿class Solution(object):
+	# 暴力(超时)
+    def subarraySum(self, nums, k):
+        if not nums:
+            return 0
+        n = len(nums)
+        count = 0
+        for i in xrange(n):
+            sum = 0
+            for j in xrange(i,-1,-1):
+               sum += nums[j]
+               if sum==k:
+                   count += 1
+        return count
+
+		
+		
+	# hash+前缀和 	
+    def subarraySum(self, nums, k):
+        if not nums:
+            return 0
+        n = len(nums)
+        d = dict()
+        d[0] = 1
+        count = 0
+        pre = 0
+        for i in xrange(n):
+            pre += nums[i]
+            if pre-k in d:
+                count += d[pre-k]
+            d[pre] = d.setdefault(pre,0)+1
+        return count
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
