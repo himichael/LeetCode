86a1035f15daf05a8f1bfbb8087cd34652a49267
michaelwng <michaelwangg@qq.com>
2020-05-16 15:49:27 +0800

    增加5月打卡题，增加一道链表题 二进制链表转整数

---
+++ src/1201_1300/1290_convert-binary-number-in-a-linked-list-to-integer/convert-binary-number-in-a-linked-list-to-integer.py
@@ -0,0 +1,26 @@
+﻿class Solution(object):
+    def getDecimalValue(self, head):
+        """
+        :type head: ListNode
+        :rtype: int
+        """
+        if not head:
+            return 0
+        arr = []
+        while head:
+            arr.append(str(head.val))
+            head = head.next
+        x = "".join(arr)
+        return int(x,2)
+		
+		
+		
+	# 另一种实现，每次*2
+    def getDecimalValue(self, head):
+        if not head:
+            return 0
+        ans = 0
+        while head:
+            ans = ans*2 + head.val
+            head = head.next
+        return ans
