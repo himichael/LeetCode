998698d008633edcf3e54ca0433bd36bac9f6888
michael <michaelwangg@qq.com>
2020-05-19 11:46:09 +0800

    增加第680题

--- src/601_700/680_valid-palindrome-ii/valid-palindrome-ii.py
+++ src/601_700/680_valid-palindrome-ii/valid-palindrome-ii.py
@@ -1,68 +1,24 @@
-﻿class TrieTree(object):
-    def __init__(self):
-        self.root = {}
-        self.end_word = "#"
-    
-    def insert(self,word,value):
-        node = self.root
-        for c in word:
-            node = node.setdefault(c,{})
-        node[self.end_word] = value
-        return None
-
-    def find(self,prefix):
-        node = self.root
-        for c in prefix:
-            if(c not in node):
-                return None
-            node = node[c]
-        return node
-
-
-class MapSum(object):
-
-    def __init__(self):
+﻿class Solution(object):
+    def validPalindrome(self, s):
         """
-        Initialize your data structure here.
+        :type s: str
+        :rtype: bool
         """
-        self.trie = TrieTree()
-        
+        if not s:
+            return True
+        i = 0
+        j = len(s)-1
+        while i<j:
+            if s[i]!=s[j]:
+                return self.is_palindrome(s,i+1,j) or self.is_palindrome(s,i,j-1)
+            i += 1
+            j -= 1
+        return True
 
-    def insert(self, key, val):
-        """
-        :type key: str
-        :type val: int
-        :rtype: None
-        """
-        return self.trie.insert(key, val)
-        
-
-    def sum(self, prefix):
-        """
-        :type prefix: str
-        :rtype: int
-        """
-        node = self.trie.find(prefix)
-        self.count = 0
-        
-        def recursion(node):
-            if(not node):
-                return
-            for d in node:
-                if(d==self.trie.end_word):
-                    continue
-                if(self.trie.end_word in node[d]):
-                    self.count += node[d][self.trie.end_word]
-                recursion(node[d])
-        if(node):
-            recursion(node)
-            if(self.trie.end_word in node):
-                self.count += node[self.trie.end_word]
-        return self.count
-
-
-
-# Your MapSum object will be instantiated and called as such:
-# obj = MapSum()
-# obj.insert(key,val)
-# param_2 = obj.sum(prefix)
+    def is_palindrome(self,s,i,j):
+        while i<j:
+            if s[i]!=s[j]:
+                return False
+            i += 1
+            j -= 1
+        return True
