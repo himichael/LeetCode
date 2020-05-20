6147ea6f9b5fb09056b54038e369a3f7df60489f
michaelwng <michaelwangg@qq.com>
2020-05-20 14:11:24 +0800

    增加5月打卡题目，每个元音包含偶数次的最长子字符串

---
+++ src/1301_1400/1371_find-the-longest-substring-containing-vowels-in-even-counts/find-the-longest-substring-containing-vowels-in-even-counts.py
@@ -0,0 +1,25 @@
+﻿class Solution(object):
+	def findTheLongestSubstring(self, s):
+		if not s:
+			return 0
+		dp = [-1]*32
+		dp[0] = 0
+		ans = 0
+		status = 0
+		for i in xrange(len(s)):
+			if s[i]=="a":
+				status ^= 1
+			elif s[i]=="e":
+				status  ^= 2
+			elif s[i]=="i":
+				status ^= 4
+			elif s[i]=="o":
+				status ^= 8
+			elif s[i]=="u":
+				status ^= 16
+			
+			if dp[status]>=0:
+				ans = max(ans,i+1-dp[status])
+			else:
+				dp[status] = i+1
+		return ans
