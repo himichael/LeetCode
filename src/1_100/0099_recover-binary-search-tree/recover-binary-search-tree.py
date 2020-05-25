3723ee5ee662ea738a64dbb90a15947acd39ff45
michaelwng <michaelwangg@qq.com>
2020-05-25 21:28:09 +0800

    增加一题，恢复二叉搜索树

---
+++ src/1_100/0099_recover-binary-search-tree/recover-binary-search-tree.py
@@ -0,0 +1,30 @@
+﻿class Solution(object):
+    def recoverTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: None Do not return anything, modify root in-place instead.
+        """
+        res = []
+        val = []
+        def dfs(root):
+            if not root:
+                return
+            dfs(root.left)
+            res.append(root)
+            val.append(root.val)
+            dfs(root.right)
+            
+        dfs(root)    
+        index1 = -1
+        index2 = -1
+        for i in xrange(0,len(val)-1):
+            if val[i]>val[i+1]:
+                if index1==-1:
+                    index1 = i
+                    index2 = i+1
+                else:
+                    index2 = i+1
+        if index1>-1 and index2>-1:
+            tmp = res[index1].val
+            res[index1].val = res[index2].val
+            res[index2].val = tmp
