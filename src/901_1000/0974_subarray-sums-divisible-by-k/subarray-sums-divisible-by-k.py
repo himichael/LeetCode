8885c16906176fd7e9dedff8e3f47dbdbd67260b
michael <michaelwangg@qq.com>
2020-05-27 12:18:53 +0800

    增加5月打卡题目，和可被 K 整除的子数组

---
+++ src/901_1000/0974_subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.py
@@ -0,0 +1,15 @@
+﻿class Solution:
+	def subarraysDivByK(self, A, K):
+		if not A:
+			return 0
+		d = dict()
+		d[0] = 1
+		total = 0
+		ans = 0
+		for i in A:
+			total += i
+			modulus = total%K
+			same = d.get(modulus,0)
+			ans += same
+			d[modulus] = same + 1
+		return ans
