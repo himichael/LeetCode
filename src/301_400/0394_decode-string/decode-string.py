f93960cb84486713b9260ec5a61773b047ea20cd
michael <michaelwangg@qq.com>
2020-05-28 11:25:55 +0800

    增加5月打卡题目，字符串解码

---
+++ src/301_400/0394_decode-string/decode-string.py
@@ -0,0 +1,20 @@
+﻿class Solution:
+	def decodeString(self, s):
+		if not s:
+			return ""
+		res = ""
+		multi = 0
+		stack = []
+		for c in s:
+			if "0"<=c<="9":
+				multi = multi*10 + int(c)
+			elif c=="[":
+				stack.append( (res,multi) )
+				res = ""
+				multi = 0
+			elif c=="]":
+				pre_str,in_multi = stack.pop()
+				res = pre_str + in_multi*res
+			else:
+				res += c
+		return res
