e6fe694e1f2eec552c2729c68777a4ccf2e95269
michaelwng <michaelwangg@qq.com>
2020-05-30 12:34:52 +0800

    增加5月打卡题目， 柱状图中最大的矩形

---
+++ src/1_100/0084_largest-rectangle-in-histogram/largest-rectangle-in-histogram.py
@@ -0,0 +1,19 @@
+﻿class Solution(object):
+	def largestRectangleArea(self, heights):
+		"""
+		:type heights: List[int]
+		:rtype: int
+		"""
+		if not heights:
+			return 0
+		heights = [0] + heights + [0]
+		res = 0
+		n = len(heights)
+		stack = [0]
+		for i in xrange(1,n):
+			while heights[i]<heights[stack[-1]]:
+				height = heights[stack.pop()]
+				width = i-stack[-1]-1
+				res = max(res,height*width)
+			stack.append(i)
+		return res
