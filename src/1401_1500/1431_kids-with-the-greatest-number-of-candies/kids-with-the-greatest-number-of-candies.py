0933e47afea25cc50878746cdb2ef8d533171d0c
michael <michaelwangg@qq.com>
2020-06-02 12:12:48 +0800

    增加1431题，501题

---
+++ src/1401_1500/1431_kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.py
@@ -0,0 +1,5 @@
+﻿class Solution:
+    def kidsWithCandies(self, candies, extraCandies):
+        maxCandies = max(candies)
+        ret = [candy + extraCandies >= maxCandies for candy in candies]
+        return ret
---
+++ src/501_600/0501_find-mode-in-binary-search-tree/find-mode-in-binary-search-tree.py
@@ -0,0 +1,33 @@
+﻿# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def findMode(self, root):
+        if not root:
+            return []
+        d = dict()
+        def dfs(root):
+            if not root:
+                return
+            dfs(root.left)
+            val = root.val
+            if val not in d:
+                d[val] = [1,val]
+            else:
+                num = d[val][0]+1
+                d[val] = [num,val]
+            dfs(root.right)
+        dfs(root)
+        values = sorted(d.values(),key=lambda x:x[0])
+        max_num = values[-1][0]
+        ans = [values[-1][1]]
+        for i in xrange(len(values)-2,-1,-1):
+            if values[i][0]==max_num:
+                ans.append(values[i][1])
+        return ans
+
+
