ecec12d39d44a692b19591735772250864699d79
michael <michaelwangg@qq.com>
2020-06-03 10:36:23 +0800

    增加第837题

---
+++ src/801_900/0837_new-21-game/new-21-game.py
@@ -0,0 +1,17 @@
+﻿class Solution(object):
+    def new21Game(self, N, K, W):
+        """
+        :type N: int
+        :type K: int
+        :type W: int
+        :rtype: float
+        """
+        if K == 0:
+            return 1.0
+        dp = [0.0] * (K + W + 1)
+        for i in range(K, min(N, K + W - 1) + 1):
+            dp[i] = 1.0
+        dp[K - 1] = float(min(N - K + 1, W)) / W
+        for i in range(K - 2, -1, -1):
+            dp[i] = dp[i + 1] - (dp[i + W + 1] - dp[i + 1]) / W
+        return dp[0]
