8d331f507cbc59bf87b9d84e947a63d4fb68fe99
michael <michaelwangg@qq.com>
2020-06-04 17:27:26 +0800

    增加一题，783

---
+++ src/701_800/0783_minimum-distance-between-bst-nodes/minimum-distance-between-bst-nodes.py
@@ -0,0 +1,21 @@
+﻿class Solution(object):
+	def minDiffInBST(self, root):
+		"""
+		:type root: TreeNode
+		:rtype: int
+		"""
+		res =  []
+		def dfs(root):
+			if not root:
+				return
+			dfs(root.left)
+			res.append(root.val)
+			dfs(root.right)
+		dfs(root)
+		res = sorted(res)
+		ans = float("inf")
+		n = len(res)
+		for i in xrange(1,n):
+			if res[i]-res[i-1]<ans:
+				ans = res[i]-res[i-1]
+		return ans 
