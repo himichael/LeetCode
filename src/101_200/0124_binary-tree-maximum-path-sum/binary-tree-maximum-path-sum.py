4449e89863acb98d3fc8dc5c1de0b78392e5ba31
michael <michaelwangg@qq.com>
2020-06-04 16:52:02 +0800

    增加一题124

---
+++ src/101_200/0124_binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.py
@@ -0,0 +1,18 @@
+﻿class Solution(object):
+	def maxPathSum(self, root):
+		"""
+		:type root: TreeNode
+		:rtype: int
+		"""
+		if not root:
+			return 0
+		self.ans = float("-inf")
+		def dfs(root):
+			if not root:
+				return 0
+			left = max(dfs(root.left),0)
+			right = max(dfs(root.right),0)
+			self.ans = max(root.val+left+right,self.ans)
+			return root.val + max(left,right)
+		dfs(root)
+		return self.ans
