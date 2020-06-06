79c6318a25db6095ba965cc57574ecd2e5b0a959
michaelwng <michaelwangg@qq.com>
2020-06-06 18:17:30 +0800

    增加一题，打家劫舍2

---
+++ src/201_300/0213_house-robber-ii/house-robber-ii.py
@@ -0,0 +1,22 @@
+﻿class Solution(object):
+	def rob(self, nums):
+		if not nums:
+			return 0
+		n = len(nums)
+		if n<=2:
+			return max(nums)
+		d = dict()
+		def dfs(index,status,arr,d):
+			if index==n-1:
+				return 0
+			if (index,status)in d:
+				return d[index,status]
+			a,b,c = 0,0,0
+			a = dfs(index+1,status,arr,d)
+			if status:
+				b = dfs(index+1,0,arr,d)
+			else:
+				c = dfs(index+1,1,arr,d)+arr[index]
+			d[index,status] = max(a,b,c)
+			return d[index,status]
+		return max(dfs(0,0,nums[0:n-1],dict()), dfs(0,0,nums[1:],dict()))
