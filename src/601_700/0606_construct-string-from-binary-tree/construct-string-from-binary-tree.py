bce685dc4c9d2119d4a54d12f8c8955f1480669c
michaelwng <michaelwangg@qq.com>
2020-06-15 22:57:50 +0800

    增加一题，606

---
+++ src/601_700/0606_construct-string-from-binary-tree/construct-string-from-binary-tree.py
@@ -0,0 +1,25 @@
+﻿class Solution(object):
+	def tree2str(self, t):
+		"""
+		:type t: TreeNode
+		:rtype: str
+		"""
+		if not t:
+			return ""
+		res = []
+		def dfs(root):
+			if not root:
+				return
+			res.append(str(root.val))
+			if root.left:
+				res.append("(")
+				dfs(root.left)
+				res.append(")")
+			if not root.left and root.right:
+				res.append("()")
+			if root.right:
+				res.append("(")
+				dfs(root.right)
+				res.append(")")
+		dfs(t)
+		return "".join(res)
