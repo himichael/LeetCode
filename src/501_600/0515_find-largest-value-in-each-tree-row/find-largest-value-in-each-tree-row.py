ea629bfd6de89ce8127d22c3022e2e8fe4183e32
michaelwng <michaelwangg@qq.com>
2020-06-15 22:38:57 +0800

    增加两道题，515,530

---
+++ src/501_600/0515_find-largest-value-in-each-tree-row/find-largest-value-in-each-tree-row.py
@@ -0,0 +1,19 @@
+﻿class Solution(object):
+	def largestValues(self, root):
+		"""
+		:type root: TreeNode
+		:rtype: List[int]
+		"""
+		res = []
+		def dfs(root,index):
+			if not root:
+				return
+			if len(res)<index:
+				res.append([float("-inf")])
+			#print "prepare -> "+str(root.val)+" index->"+str(index)
+			if res[index-1][0]<root.val:
+				res[index-1][0] = root.val
+			dfs(root.left,index+1)
+			dfs(root.right,index+1)
+		dfs(root,1)
+		return [res[i][0] for i in xrange(len(res))]
---
+++ src/501_600/0530_minimum-absolute-difference-in-bst/minimum-absolute-difference-in-bst.py
@@ -0,0 +1,22 @@
+﻿class Solution(object):
+	def getMinimumDifference(self, root):
+		"""
+		:type root: TreeNode
+		:rtype: int
+		"""	
+		if not root:
+			return 0
+		self.pre = None
+		self.diff = float("inf")
+		def dfs(root):
+			if not root:
+				return
+			dfs(root.left)
+			if self.pre:
+				t = abs(self.pre.val-root.val)
+				if t<self.diff:
+					self.diff = t
+			self.pre = root
+			dfs(root.right)
+		dfs(root)
+		return self.diff
