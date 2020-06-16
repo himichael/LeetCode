27472864af0b3326bd7d390e4ea7aaf2b40409fa
michael <michaelwangg@qq.com>
2020-06-16 18:55:16 +0800

    增加一题，508

---
+++ src/501_600/0508_most-frequent-subtree-sum/most-frequent-subtree-sum.py
@@ -0,0 +1,31 @@
+﻿class Solution(object):
+	def findFrequentTreeSum(self, root):
+		"""
+		:type root: TreeNode
+		:rtype: List[int]
+		"""
+		if not root:
+			return []
+		d = dict()
+		def dfs(root):
+			if not root:
+				return 0
+			left = dfs(root.left)
+			right = dfs(root.right)
+			t = left+right+root.val
+			if t not in d:
+				d[t] = [1,t]
+			else:
+				d[t] = [d[t][0]+1,t]
+			return t
+		dfs(root)
+		vals = d.values()
+		vals.sort(key=lambda x:x[0],reverse=True)
+		index = vals[0][0]
+		res = []
+		for i in vals:
+			if index==i[0]:
+				res.append(i[1])
+			else:
+				break
+		return res
