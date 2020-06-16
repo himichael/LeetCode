7ac8a3dddb88a126829281a142e7133251358fe7
michael <michaelwangg@qq.com>
2020-06-16 15:23:04 +0800

    增加一题，662

---
+++ src/601_700/0662_maximum-width-of-binary-tree/maximum-width-of-binary-tree.py
@@ -0,0 +1,26 @@
+﻿class Solution(object):
+	def widthOfBinaryTree(self, root):
+		"""
+		:type root: TreeNode
+		:rtype: int
+		"""		
+		from collections import deque
+		queue = deque()
+		queue.append(root)
+		max_len = float("-inf")
+		while queue:
+			while queue and queue[0]==None:
+				queue.popleft()
+			while queue and queue[-1]==None:
+				queue.pop()
+			size = len(queue)
+			max_len = max(max_len,size)
+			for _ in xrange(size):
+				node = queue.popleft()
+				if node:
+					queue.append(node.left)
+					queue.append(node.right)
+				else:
+					queue.append(None)
+					queue.append(None)
+		return max_len
