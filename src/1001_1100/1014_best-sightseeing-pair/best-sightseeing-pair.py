84272303578f7cc480d39e9d5e2ecc6ee6919040
michael <michaelwangg@qq.com>
2020-06-17 11:29:22 +0800

    增加一题，最佳观光组合

---
+++ src/1001_1100/1014_best-sightseeing-pair/best-sightseeing-pair.py
@@ -0,0 +1,15 @@
+﻿class Solution(object):
+	def maxScoreSightseeingPair(self, A):
+		"""
+		:type A: List[int]
+		:rtype: int
+		"""
+		if not A:
+			return 0
+		pre = A[0]+0
+		n = len(A)
+		res = 0
+		for j in xrange(1,n):
+			res = max(res,A[j]-j+pre)
+			pre = max(pre,A[j]+j)
+		return res
