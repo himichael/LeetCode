9e0f162ea8beefdb347ad1dcbd892765ae54cd99
michael <michaelwangg@qq.com>
2020-06-17 12:59:56 +0800

    第671题增加一种实现

--- src/601_700/0671_second-minimum-node-in-a-binary-tree/second-minimum-node-in-a-binary-tree.py
+++ src/601_700/0671_second-minimum-node-in-a-binary-tree/second-minimum-node-in-a-binary-tree.py
@@ -17,3 +17,25 @@
 		if len(s)<2:
 			return -1
 		return sorted(list(s))[1]
+		
+		
+		
+	# 另一种解法	
+	def findSecondMinimumValue(self, root):
+		if not root:
+			return 0
+		first = root.val
+		second = float("inf")
+		def dfs(root):
+			if not root:
+				return float("inf")
+			if root.val==first:
+				return min( dfs(root.left),dfs(root.right) )
+			return min( root.val,dfs(root.left),dfs(root.right) )
+		second = dfs(root)
+		if second==float("inf"):
+			return -1
+		return second
+
+			
+			
