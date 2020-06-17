431ac87003e83354635785d8cffba5accbb8b654
michael <michaelwangg@qq.com>
2020-06-17 16:27:08 +0800

    增加一题，899

---
+++ src/801_900/0889_construct-binary-tree-from-preorder-and-postorder-traversal/construct-binary-tree-from-preorder-and-postorder-traversal.py
@@ -0,0 +1,18 @@
+﻿class Solution(object):
+	def constructFromPrePost(self, pre, post):
+		"""
+		:type pre: List[int]
+		:type post: List[int]
+		:rtype: TreeNode
+		"""	
+		def dfs(pre,post):
+			if not pre:
+				return None
+			if len(pre)==1:
+				return TreeNode(pre[0])
+			root = TreeNode(pre[0])
+			left_count = post.index(pre[1])+1
+			root.left = dfs(pre[1:left_count+1],post[:left_count])
+			root.right = dfs(pre[left_count+1:],post[left_count:-1])
+			return root
+		return dfs(pre,post)
