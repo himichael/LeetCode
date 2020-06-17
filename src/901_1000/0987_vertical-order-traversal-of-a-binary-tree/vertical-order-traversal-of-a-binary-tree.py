66cf640f030d78f34b42bd46dbf5b34477c58ced
michael <michaelwangg@qq.com>
2020-06-17 15:18:26 +0800

    增加一题，987

---
+++ src/901_1000/0987_vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.py
@@ -0,0 +1,23 @@
+﻿class Solution(object):
+	def verticalTraversal(self, root):
+		"""
+		:type root: TreeNode
+		:rtype: List[List[int]]
+		"""
+		if not root:
+			return []
+		d = collections.defaultdict(lambda: collections.defaultdict(list))
+		def dfs(root,x,y):
+			if not root:
+				return
+			d[x][y].append(root.val)
+			dfs(root.left,x-1,y+1)
+			dfs(root.right,x+1,y+1)
+		dfs(root,0,0)
+		res = []
+		for x in sorted(d):
+			report = []
+			for y in sorted(d[x]):
+				report.extend(sorted(v for v in d[x][y]))
+			res.append(report)
+		return res
