51a125ff5c3f97cc131f2f63817865158596bac9
michael <michaelwangg@qq.com>
2020-06-17 11:48:48 +0800

    增加一题，671

---
+++ src/601_700/0671_second-minimum-node-in-a-binary-tree/second-minimum-node-in-a-binary-tree.py
@@ -0,0 +1,19 @@
+﻿class Solution(object):
+	def findSecondMinimumValue(self, root):
+		"""
+		:type root: TreeNode
+		:rtype: int
+		"""
+		if not root:
+			return -1
+		s = set()
+		def dfs(root):
+			if not root:
+				return
+			s.add(root.val)
+			dfs(root.left)
+			dfs(root.right)
+		dfs(root)
+		if len(s)<2:
+			return -1
+		return sorted(list(s))[1]
