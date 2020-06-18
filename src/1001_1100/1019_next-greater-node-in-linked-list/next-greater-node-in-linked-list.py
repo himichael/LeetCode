58243948744a8bf64f04d3b69ff6dc98ad229c95
michael <michaelwangg@qq.com>
2020-06-18 14:50:16 +0800

    增加一题，1019

---
+++ src/1001_1100/1019_next-greater-node-in-linked-list/next-greater-node-in-linked-list.py
@@ -0,0 +1,21 @@
+﻿class Solution(object):
+	def nextLargerNodes(self, head):
+		"""
+		:type head: ListNode
+		:rtype: List[int]
+		"""	
+		if not head:
+			return []
+		stack = []
+		arr = []
+		while head:
+			arr.append(head.val)
+			head = head.next
+		n = len(arr)
+		res = [0 for _ in xrange(n)]
+		for i in xrange(n):
+			while stack and arr[i]>arr[stack[-1]]:
+				res[stack[-1]] = arr[i]
+				stack.pop()
+			stack.append(i)
+		return res
