8fe5e322e68ced2fa556cf84c868357ca592a7a7
michael <michaelwangg@qq.com>
2020-06-18 11:42:34 +0800

    增加一题，1028

---
+++ src/1001_1100/1028_recover-a-tree-from-preorder-traversal/recover-a-tree-from-preorder-traversal.py
@@ -0,0 +1,30 @@
+﻿class Solution(object):
+	def recoverFromPreorder(self, S):
+		"""
+		:type S: str
+		:rtype: TreeNode
+		"""	
+		if not S:
+			return None
+		n = len(S)
+		i = 0
+		ans = []
+		while i<n:
+			level = 0
+			while i<n and S[i]=="-":
+				level += 1
+				i += 1
+			val = 0
+			while i<n and S[i]>='0' and S[i]<='9':
+				val = val*10 + ord(S[i])-48
+				i += 1
+			node = TreeNode(val)
+			if level==len(ans):
+				if ans:
+					ans[-1].left = node
+			else:
+				while level!=len(ans):
+					ans.pop()
+				ans[-1].right = node
+			ans.append(node)
+		return ans[0]
