dd484897044333d0113d57cd44175c7105b86322
michael <michaelwangg@qq.com>
2020-06-19 14:08:22 +0800

    增加一题，1302

---
+++ src/1301_1400/1302_deepest-leaves-sum/deepest-leaves-sum.py
@@ -0,0 +1,19 @@
+﻿class Solution(object):
+	def deepestLeavesSum(self, root):
+		"""
+		:type root: TreeNode
+		:rtype: int
+		"""
+		if not root:
+			return 0
+		res = []
+		def dfs(root,index):
+			if not root:
+				return
+			if len(res)<index:
+				res.append([])
+			res[index-1].append(root.val)
+			dfs(root.left,index+1)
+			dfs(root.right,index+1)
+		dfs(root,1)
+		return sum(res[-1])
