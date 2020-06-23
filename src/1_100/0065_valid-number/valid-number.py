07307bea61dd791736cac6c19700d14830d5d154
michaelwng <michaelwangg@qq.com>
2020-06-23 15:02:45 +0800

    增加第65题的测试用例

--- src/1_100/0065_valid-number/valid-number.py
+++ src/1_100/0065_valid-number/valid-number.py
@@ -57,3 +57,31 @@
             return False
         else:
             return False
+			
+			
+			
+			
+p=Solution()
+print"8..e4 => false "+str(p.isNumber("8..e4"))
+print" => false "+str(p.isNumber(" "))
+print" 0 => true "+str(p.isNumber(" 0 "))
+print" 0.1 => true "+str(p.isNumber(" 0.1"))
+print" abc => false "+str(p.isNumber(" abc"))
+print" 1a => false "+str(p.isNumber(" 1a"))
+print" 2e10 => true "+str(p.isNumber(" 2e10"))
+print" -90e3 => true "+str(p.isNumber(" -90e3"))
+print" 1e => false "+str(p.isNumber(" 1e"))
+print" e3 => false "+str(p.isNumber(" e3"))
+print" 6e-1 => true "+str(p.isNumber("6e-1"))
+print" 99e2.5 => false "+str(p.isNumber("99e2.5"))
+print" 53.5e93 => true "+str(p.isNumber("53.5e93"))
+print" --6 => false "+str(p.isNumber("--6"))
+print" -+3 => false "+str(p.isNumber("-+3"))
+print" 95a54e53 => false "+str(p.isNumber("95a54e53"))
+print" .1 => true "+str(p.isNumber(".1"))
+print" 3. => true "+str(p.isNumber("3."))
+print" . => false "+str(p.isNumber("."))
+print" -. => false "+str(p.isNumber("-."))
+print" -222.33. => false "+str(p.isNumber("-22.33."))
+print" -3.1415e99999 => true "+str(p.isNumber("-3.1415e99999"))
+print" -3.1415e99a88 => false "+str(p.isNumber("-3.1415e99a88"))
