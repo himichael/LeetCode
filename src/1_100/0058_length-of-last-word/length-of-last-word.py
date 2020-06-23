e7f89b6703a9f0c91b62ce4803b0b4668357644f
michaelwng <michaelwangg@qq.com>
2020-06-23 16:49:09 +0800

    增加一题，58

---
+++ src/1_100/0058_length-of-last-word/length-of-last-word.py
@@ -0,0 +1,10 @@
+﻿class Solution(object):
+	def lengthOfLastWord(self, s):
+		"""
+		:type s: str
+		:rtype: int
+		"""
+		if not s:
+			return 0
+		arr = s.strip().split(" ")
+		return len(arr[-1])
