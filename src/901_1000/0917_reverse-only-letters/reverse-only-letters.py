9bd8061b0b4ad55aaa44b371b268938abf9bd63b
michaelwng <michaelwangg@qq.com>
2020-06-23 15:28:28 +0800

    增加一题，917

---
+++ src/901_1000/0917_reverse-only-letters/reverse-only-letters.py
@@ -0,0 +1,22 @@
+﻿class Solution(object):
+	def reverseOnlyLetters(self, S):
+		"""
+		:type S: str
+		:rtype: str
+		"""
+		if not S:
+			return S
+		n = len(S)
+		i = 0
+		j = n-1
+		arr = list(S)
+		while i<j:
+			if not arr[i].isalpha():
+				i += 1
+			elif not arr[j].isalpha():
+				j -= 1
+			else:
+				arr[i],arr[j] = arr[j],arr[i]
+				i += 1
+				j -= 1
+		return "".join(arr)
