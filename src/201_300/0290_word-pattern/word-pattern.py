ed42673bc83eafee11ec9cd1970894c44d350bea
michaelwng <michaelwangg@qq.com>
2020-06-24 18:21:31 +0800

    增加一题，单词规律

---
+++ src/201_300/0290_word-pattern/word-pattern.py
@@ -0,0 +1,24 @@
+﻿class Solution(object):
+	def wordPattern(self, pattern, str):
+		"""
+		:type pattern: str
+		:type str: str
+		:rtype: bool
+		"""
+		if not (pattern and str):
+			return False
+		arr = str.split(" ")
+		d = dict()
+		n = len(pattern)
+		if n!=len(arr):
+			return False
+		for i in xrange(n):
+			if pattern[i] not in d:
+				if arr[i] in d.values():
+					return False
+				else:
+					d[pattern[i]] = arr[i]
+			else:
+				if d[pattern[i]]!=arr[i]:
+					return False
+		return True
