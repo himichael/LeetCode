a0b3951601a78c07f6047da172dfc2344eb6b41b
michaelwng <michaelwangg@qq.com>
2020-06-24 17:34:09 +0800

    增加一题，反转字符串中的元音字母

---
+++ src/301_400/0345_reverse-vowels-of-a-string/reverse-vowels-of-a-string.py
@@ -0,0 +1,22 @@
+﻿class Solution(object):
+	def reverseVowels(self, s):
+		"""
+		:type s: str
+		:rtype: str
+		"""
+		if not s:
+			return s
+		n = len(s)
+		i = 0
+		j = n-1
+		d = set(['a','e','i','o','u','A','E','I','O','U'])
+		s = list(s)
+		while i<j:
+			while i<j and s[i] not in d:
+				i += 1
+			while i<j and s[j] not in d:
+				j -= 1
+			s[i],s[j] = s[j],s[i]
+			i += 1
+			j -= 1
+		return "".join(s)
