b2f92588c3d5758a45201bf3be43b88be0074252
michaelwng <michaelwangg@qq.com>
2020-06-24 17:53:32 +0800

    增加一题，赎金信

---
+++ src/301_400/0383_ransom-note/ransom-note.py
@@ -0,0 +1,21 @@
+﻿class Solution(object):
+	def canConstruct(self, ransomNote, magazine):
+		"""
+		:type ransomNote: str
+		:type magazine: str
+		:rtype: bool
+		"""
+		if not ransomNote:
+			return True
+		r_dict = dict()
+		m_dict = dict()
+		for i in ransomNote:
+			r_dict[i] = r_dict.setdefault(i,0)+1
+		for i in magazine:
+			m_dict[i] = m_dict.setdefault(i,0)+1
+		for key in r_dict.keys():
+			if key not in m_dict:
+				return False
+			if r_dict[key]>m_dict[key]:
+				return False
+		return True
