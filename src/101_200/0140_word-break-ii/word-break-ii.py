aa3b9b5e3487306db8ede977f234364865495096
michaelwng <michaelwangg@qq.com>
2020-06-25 22:31:04 +0800

    第140题增加一种实现

--- src/101_200/0140_word-break-ii/word-break-ii.py
+++ src/101_200/0140_word-break-ii/word-break-ii.py
@@ -27,3 +27,23 @@
             d[index] = res
             return res
         return dfs(0)
+		
+		
+		
+	# 动态规划(超时)
+	def wordBreak(self, s, wordDict):
+		if not s:
+			return []
+		sets = set(wordDict)
+		n = len(s)
+		dp = [[] for _ in xrange(n+1)]
+		dp[0] = [""]
+		for i in xrange(1,n+1):
+			tmp = []
+			for j in xrange(i):
+				word = s[j:i]
+				if dp[j] and word in sets:
+					for li in dp[j]:
+						tmp.append( (li+" "+word).strip() )
+			dp[i] = tmp
+		return dp[-1]	
