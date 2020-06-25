4644c14a70a78d8a1f6a4944db3b5e006e644e0f
michaelwng <michaelwangg@qq.com>
2020-06-25 16:39:15 +0800

    第139题增加一种实现

--- src/101_200/0139_word-break/word-break.py
+++ src/101_200/0139_word-break/word-break.py
@@ -16,3 +16,27 @@
 				if dp[j] and s[j:i] in sets:
 					dp[i] = True
 		return dp[-1]
+		
+		
+	# 递归+记忆化
+	def wordBreak(self, s, wordDict):
+		if not s:
+			return True
+		n = len(s)
+		d = dict()
+		sets = set(wordDict)
+		def dfs(s):
+			if s in d:
+				return d[s]
+			if not s:
+				return True
+			for i in xrange(1,len(s)+1):
+				word = s[:i]
+				if word in sets:
+					if dfs(s[i:]):
+						return True
+			d[s] = False
+			return False
+		return dfs(s)
+
+		
