b788acbbc54d2903e5dd3aa6e91348326f3f4df7
michaelwng <michaelwangg@qq.com>
2020-06-25 14:11:39 +0800

    增加一题，单词拆分

---
+++ src/101_200/0139_word-break/word-break.py
@@ -0,0 +1,18 @@
+﻿class Solution(object):
+	def wordBreak(self, s, wordDict):
+		"""
+		:type s: str
+		:type wordDict: List[str]
+		:rtype: bool
+		"""
+		if not s:
+			return True
+		n = len(s)
+		sets = set(wordDict)
+		dp = [False for _ in xrange(n+1)]
+		dp[0] = True
+		for i in xrange(1,n+1):
+			for j in xrange(0,i):
+				if dp[j] and s[j:i] in sets:
+					dp[i] = True
+		return dp[-1]
