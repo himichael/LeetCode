e1843905dde9151a954ee980954d03a5ff8dbe94
michaelwng <michaelwangg@qq.com>
2020-06-25 18:48:31 +0800

    增加一题，单词拆分 II

---
+++ src/101_200/0140_word-break-ii/word-break-ii.py
@@ -0,0 +1,29 @@
+﻿class Solution(object):
+    def wordBreak(self, s, wordDict):
+        """
+        :type s: str
+        :type wordDict: List[str]
+        :rtype: List[str]
+        """
+        if not s:
+            return []
+        sets = set(wordDict)
+        d = dict()
+        def dfs(index):
+            if index in d:
+                return d[index]
+            res = []
+            n = len(s)
+            if index==n:
+                res.append("")
+            for end in xrange(index+1,n+1):
+                word = s[index:end]
+                if word in sets:
+                    arr = dfs(end)
+                    for li in arr:
+                        space = "" if li=="" else " "
+                        tmp = s[index:end]+space+li
+                        res.append(tmp)
+            d[index] = res
+            return res
+        return dfs(0)
