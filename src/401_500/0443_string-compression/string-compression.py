532cfc9d94d5c42833949640afe5b58f7c473f7b
michaelwng <michaelwangg@qq.com>
2020-06-26 13:45:15 +0800

    增加一题，压缩字符串

---
+++ src/401_500/0443_string-compression/string-compression.py
@@ -0,0 +1,15 @@
+﻿class Solution(object):
+	def compress(self, chars):
+		start = 0
+		write = 0
+		n = len(chars)
+		for read,c in enumerate(chars):
+			if read+1==n or chars[read+1]!=c:
+				chars[write] = chars[start]
+				write += 1
+				if read>start:
+					for digit in str(read-start+1):
+						chars[write] = digit
+						write += 1
+				start = read + 1
+		return write
