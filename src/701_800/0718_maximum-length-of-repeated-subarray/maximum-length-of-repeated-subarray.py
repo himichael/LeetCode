5be5d8f0427e09f2d078c71019b5872a0aefe4b4
michaelwng <michaelwangg@qq.com>
2020-06-28 12:27:42 +0800

    增加六月打卡题目

--- src/701_800/0718_maximum-length-of-repeated-subarray/maximum-length-of-repeated-subarray.py
+++ src/701_800/0718_maximum-length-of-repeated-subarray/maximum-length-of-repeated-subarray.py
@@ -5,7 +5,6 @@
 		:type B: List[int]
 		:rtype: int
 		"""
-		
 		arr = [[-1]*len(B) for i in xrange(len(A))]
 		res = 0
 		for i in xrange(len(A)):
@@ -21,3 +20,19 @@
 						arr[i][j] = 1
 						res = max(res,arr[i][j])
 		return res
+		
+		
+		
+	#精简的动态规划
+	def findLength(self, A, B):
+		dp = [[0 for _ in xrange(len(B)+1)] for _ in xrange(len(A)+1)]
+		res = 0
+		for i in xrange(1,len(A)+1):
+			for j in xrange(1,len(B)+1):
+				if A[i-1]==B[j-1]:
+					dp[i][j] = dp[i-1][j-1]+1
+				if dp[i][j]>res:
+					res = dp[i][j]
+		return res
+		
+		
