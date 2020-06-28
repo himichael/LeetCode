0145012736c3f2d697150f8a71c5200fbb76fda5
michaelwng <michaelwangg@qq.com>
2020-06-28 15:34:19 +0800

    增加一题，同构字符串

---
+++ src/201_300/0205_isomorphic-strings/isomorphic-strings.py
@@ -0,0 +1,19 @@
+﻿class Solution(object):
+	def isIsomorphic(self, s, t):
+		"""
+		:type s: str
+		:type t: str
+		:rtype: bool
+		"""
+		d = dict()
+		n = len(s)
+		for i in xrange(n):
+			if s[i] not in d:
+				if t[i] in d.values():
+					return False
+				else:
+					d[s[i]] = t[i]
+			else:
+				if d[s[i]]!=t[i]:
+					return False
+		return True
