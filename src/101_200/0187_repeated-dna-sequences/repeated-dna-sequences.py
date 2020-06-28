f302dcf7201b286241d7897ee35d9bbd25201ce8
michaelwng <michaelwangg@qq.com>
2020-06-28 14:41:45 +0800

    修改第187题的实现

--- src/101_200/0187_repeated-dna-sequences/repeated-dna-sequences.py
+++ src/101_200/0187_repeated-dna-sequences/repeated-dna-sequences.py
@@ -16,3 +16,34 @@
 				res.add(tmp)
 			seen.add(tmp)
 		return list(res)
+		
+		
+		
+	# 利用 Rabin-Karp算法思想
+	def findRepeatedDnaSequences(self, s):
+		"""
+		:type s: str
+		:rtype: List[str]
+		"""
+		if not s:
+			return []
+		n = len(s)
+		L = 10
+		aL = 4**10
+		hash = 0
+		seen = set()
+		res = set()
+		to_int = {'A': 0, 'C': 1, 'G': 2, 'T': 3}
+		nums = [to_int[s[i]] for i in xrange(n)]
+		for i in xrange(n-L+1):
+			if i!=0:
+				hash = hash*4 - nums[i-1]*aL + nums[i+L-1]
+			else:
+				for j in xrange(L):
+					hash = hash * 4 + nums[j]
+			if hash in seen:
+				res.add(s[i:i+L])
+			seen.add(hash)
+		return list(res)	
+		
+		
