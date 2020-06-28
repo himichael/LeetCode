d13feb13de0b8c6d1ada0dda1b4c7a3611b8bd9b
michaelwng <michaelwangg@qq.com>
2020-06-28 14:15:12 +0800

    增加一题，重复的DNA序列

---
+++ src/101_200/0187_repeated-dna-sequences/repeated-dna-sequences.py
@@ -0,0 +1,18 @@
+﻿class Solution(object):
+	def findRepeatedDnaSequences(self, s):
+		"""
+		:type s: str
+		:rtype: List[str]
+		"""
+		if not s:
+			return []
+		n = len(s)
+		L = 10
+		seen = set()
+		res = set()
+		for i in xrange(n-L+1):
+			tmp = s[i:i+L]
+			if tmp in seen:
+				res.add(tmp)
+			seen.add(tmp)
+		return list(res)
