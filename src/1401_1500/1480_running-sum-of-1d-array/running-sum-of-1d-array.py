cdb0e26efb5b772f089a02b656ed8d4243671c9c
michaelwng <michaelwangg@qq.com>
2020-06-28 22:51:02 +0800

    增加一题，一维数组的动态和

---
+++ src/1401_1500/1480_running-sum-of-1d-array/running-sum-of-1d-array.py
@@ -0,0 +1,15 @@
+﻿class Solution(object):
+	def runningSum(self, nums):
+		"""
+		:type nums: List[int]
+		:rtype: List[int]
+		"""
+		if not nums:
+			return []
+		n = len(nums)
+		total = 0
+		res = []
+		for i in xrange(n):
+			total += nums[i]
+			res.append(total)
+		return res
