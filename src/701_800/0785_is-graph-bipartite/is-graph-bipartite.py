7f21615daca7e279078ddefc3287cf7d0b27ce6e
michaelwng <michaelwangg@qq.com>
2020-07-18 14:35:06 +0800

    增加一题，判断二分图

---
+++ src/701_800/0785_is-graph-bipartite/is-graph-bipartite.py
@@ -0,0 +1,26 @@
+﻿class Solution:
+    def isBipartite(self, graph):
+        n = len(graph)
+        UNCOLORED, RED, GREEN = 0, 1, 2
+        color = [UNCOLORED] * n
+        self.valid = True
+
+        def dfs(node, c):
+            color[node] = c
+            cNei = (GREEN if c == RED else RED)
+            for neighbor in graph[node]:
+                if color[neighbor] == UNCOLORED:
+                    dfs(neighbor, cNei)
+                    if not self.valid:
+                        return
+                elif color[neighbor] != cNei:
+                    self.valid = False
+                    return
+
+        for i in range(n):
+            if color[i] == UNCOLORED:
+                dfs(i, RED)
+                if not self.valid:
+                    break
+        
+        return self.valid
