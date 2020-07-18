cbaf7218849afb3b4d087b372821610b935a255f
michaelwng <michaelwangg@qq.com>
2020-07-18 18:58:18 +0800

    第97题增加dp解法

--- src/1_100/0097_interleaving-string/interleaving-string.py
+++ src/1_100/0097_interleaving-string/interleaving-string.py
@@ -31,3 +31,26 @@
 			d[i,j,k] = False
 			return False
 		return dfs(0,0,0)
+		
+		
+		
+	# 动态规划，二维数组	
+    def isInterleave(self, s1, s2, s3):
+        s1_len = len(s1)
+        s2_len = len(s2)
+        s3_len = len(s3)
+        if s1_len+s2_len != s3_len:
+            return False
+        dp = [[False for _ in xrange(s2_len+1)] for _ in xrange(s1_len+1)]
+        dp[0][0] = True
+        for i in xrange(1,s1_len+1):
+            dp[i][0] = dp[i-1][0] and s1[i-1]==s3[i-1]
+        for j in xrange(1,s2_len+1):
+            dp[0][j] = dp[0][j-1] and s2[j-1]==s3[j-1]
+        for i in xrange(1,s1_len+1):
+            for j in xrange(1,s2_len+1):
+                k = i+j-1
+                dp[i][j] = (dp[i-1][j] and s1[i-1]==s3[k]) or (dp[i][j-1] and s2[j-1]==s3[k])
+        return dp[-1][-1]
+		
+		
