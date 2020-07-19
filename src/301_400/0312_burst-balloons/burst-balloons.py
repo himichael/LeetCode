a538e63c7f1df40f10cb2d4f2d27a9823311d74d
michaelwng <michaelwangg@qq.com>
2020-07-19 22:46:13 +0800

    增加第312题

---
+++ src/301_400/0312_burst-balloons/burst-balloons.py
@@ -0,0 +1,20 @@
+﻿class Solution:
+    def maxCoins(self, nums):
+        if not nums:
+            return 0
+        nums = [1] + nums + [1]
+        n = len(nums)
+        d = dict()
+        def dfs(left,right):
+            if (left,right) in d:
+                return d[left,right]
+            if left>=right-1:
+                return 0
+            res = 0
+            for i in xrange(left+1,right):
+                tmp = nums[left]*nums[i]*nums[right]
+                tmp += dfs(left,i) + dfs(i,right)
+                res = max(tmp,res)
+            d[left,right] = res
+            return res
+        return dfs(0,n-1)
