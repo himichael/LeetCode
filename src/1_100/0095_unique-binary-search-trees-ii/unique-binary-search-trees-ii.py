ed6366020b47c67c4026d828a5dbb7b17870ba89
michaelwng <michaelwangg@qq.com>
2020-07-21 10:01:20 +0800

    增加7月打卡题目

---
+++ src/1_100/0095_unique-binary-search-trees-ii/unique-binary-search-trees-ii.py
@@ -0,0 +1,29 @@
+﻿# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def generateTrees(self, n):
+        """
+        :type n: int
+        :rtype: List[TreeNode]
+        """
+        if n<=0:
+            return []
+        def helper(begin,end):
+            if begin>end:
+                return [None]
+            all_trees = []
+            for i in xrange(begin,end+1):
+                left_trees = helper(begin,i-1)
+                right_trees = helper(i+1,end)
+                for left in left_trees:
+                    for right in right_trees:
+                        root = TreeNode(i)
+                        root.left = left
+                        root.right = right
+                        all_trees.append(root)
+            return all_trees
+        return helper(1,n)
