7b8aab902d567aa3f9a5feba0649794e5a6bc67f
michaelwng <michaelwangg@qq.com>
2020-07-24 12:03:38 +0800

    第1025题增加动态规划实现

--- src/1001_1100/1025_divisor-game/divisor-game.py
+++ src/1001_1100/1025_divisor-game/divisor-game.py
@@ -5,3 +5,17 @@
         :rtype: bool
         """
         return N%2==0
+		
+		
+		
+	# 动态规划
+    def divisorGame(self, N):
+        dp = [False for _ in xrange(N+3)]
+        dp[1] = False
+        dp[2] = True
+        for i in xrange(3,N+1):
+            for j in xrange(1,i):
+                if i%j==0 and not dp[i-j]:
+                    dp[i] = True
+                    break
+        return dp[N] 
