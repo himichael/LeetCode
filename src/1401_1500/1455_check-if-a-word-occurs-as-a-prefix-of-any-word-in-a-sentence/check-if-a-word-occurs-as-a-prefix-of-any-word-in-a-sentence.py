45496b11aff82e979c33dded62b5447a67b9d5ee
michaelwng <michaelwangg@qq.com>
2020-07-26 22:52:21 +0800

    增加两道周赛题目

---
+++ src/1401_1500/1455_check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.py
@@ -0,0 +1,17 @@
+﻿class Solution(object):
+    def isPrefixOfWord(self, sentence, searchWord):
+        """
+        :type sentence: str
+        :type searchWord: str
+        :rtype: int
+        """
+        if not (sentence and searchWord):
+            return -1
+        arr = sentence.split(" ")
+        index = 0
+        n = len(arr)
+        while index<n:
+            if arr[index].startswith(searchWord):
+                return index+1
+            index += 1
+        return -1    
---
+++ src/1401_1500/1456_maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.py
@@ -0,0 +1,29 @@
+﻿class Solution(object):
+    def maxVowels(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: int
+        """
+        if not s or k<=0:
+            return 0
+        left = 0
+        right = 0
+        d = set(['a','e','i','o','u'])
+        total = 0
+        res = 0
+        n = len(s)
+        while right<k:
+            if s[right] in d:
+                total += 1
+            right += 1
+        res = max(res,total)
+        while right<n:
+            if s[left] in d:
+                total -= 1
+            if s[right] in d:
+                total += 1
+                res = max(res,total)
+            right += 1
+            left += 1
+        return res
