f017647184ed0fa2d7a7646d42f5f5b6bfc3f5a9
michaelwng <michaelwangg@qq.com>
2020-07-28 12:12:09 +0800

    增加188周竞赛题目

---
+++ src/1401_1500/1441_build-an-array-with-stack-operations/build-an-array-with-stack-operations.py
@@ -0,0 +1,21 @@
+﻿class Solution(object):
+    def buildArray(self, target, n):
+        """
+        :type target: List[int]
+        :type n: int
+        :rtype: List[str]
+        """
+        if not target:
+            return []
+        s = set()
+        for i in target:
+            s.add(i)
+        max_size = min(target[-1],n)
+        res = []
+        for i in xrange(1,max_size+1):
+            if i not in s:
+                res.append("Push")
+                res.append("Pop")
+            else:
+                res.append("Push")
+        return res
---
+++ src/1401_1500/1442_count-triplets-that-can-form-two-arrays-of-equal-xor/count-triplets-that-can-form-two-arrays-of-equal-xor.py
@@ -0,0 +1,17 @@
+﻿class Solution(object):
+    def countTriplets(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: int
+        """
+        n = len(arr)
+        if n<2:
+            return 0
+        res = 0
+        for i in xrange(n-1):
+            tmp = arr[i]
+            for j in xrange(i+1,n):
+                tmp ^= arr[j]
+                if tmp==0:
+                    res += (j-i)
+        return res    
