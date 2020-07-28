bc60a6009b5508d7fe51a779256d6b9373ec5bd7
michaelwng <michaelwangg@qq.com>
2020-07-28 19:34:01 +0800

    增加187周赛两道题

---
+++ src/1401_1500/1436_destination-city/destination-city.py
@@ -0,0 +1,13 @@
+﻿class Solution(object):
+    def destCity(self, paths):
+        """
+        :type paths: List[List[str]]
+        :rtype: str
+        """
+        d = dict()
+        for arr in paths:
+            d[arr[0]] = arr[1]
+        for arr in paths:
+            if arr[1] not in d:
+                return arr[1]
+        return ""    
