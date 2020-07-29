db3bc427a9ec96f63ea800d2c3928657cf57acc1
michaelwng <michaelwangg@qq.com>
2020-07-29 14:43:55 +0800

    增加第186场周赛的两道题目，7月的打卡题目

---
+++ src/1401_1500/1422_maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.py
@@ -0,0 +1,38 @@
+﻿class Solution(object):
+    def maxScore(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        n = len(s)
+        res = 0
+        for i in xrange(1,n):
+            a = 0
+            b = 0
+            for j in xrange(0,i):
+                if s[j]=="0":
+                    a += 1
+            for k in xrange(i,n):
+                if s[k]=="1":
+                    b += 1
+            res = max(res,a+b)
+        return res
+		
+		
+		
+	# O(N)实现	
+    def maxScore(self, s):
+        n = len(s)
+        res = 0
+        cnt0 = 0
+        cnt1 = 0
+        for i in s:
+            if i=="1":
+                cnt1 += 1
+        for i in xrange(n-1):
+            if s[i]=="0":
+                cnt0 += 1
+            else:
+                cnt1 -= 1
+            res = max(res,cnt0+cnt1)
+        return res
---
+++ src/1401_1500/1423_maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.py
@@ -0,0 +1,19 @@
+﻿class Solution(object):
+    def maxScore(self, cardPoints, k):
+        """
+        :type cardPoints: List[int]
+        :type k: int
+        :rtype: int
+        """
+        n = len(cardPoints)
+        total = sum(cardPoints)
+        min_size = float("inf")
+        size = n-k
+        tmp = 0
+        for i in xrange(n):
+            tmp += cardPoints[i]
+            if i>=size:
+                tmp -= cardPoints[i-size]
+            if i>=size-1:
+                min_size = min(min_size,tmp)
+        return total-min_size
