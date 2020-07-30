d48d86ae89a245577ff4f92df6461ba8f952e4a5
michaelwng <michaelwangg@qq.com>
2020-07-30 19:14:17 +0800

    增加第185场周赛的题目

---
+++ src/1401_1500/1417_reformat-the-string/reformat-the-string.py
@@ -0,0 +1,33 @@
+﻿class Solution(object):
+    def reformat(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        digit = []
+        letter = []
+        for i in s:
+            if i.isdigit():
+                digit.append(i)
+            else:
+                letter.append(i)
+        n = len(digit)
+        m = len(letter)
+        if abs(n-m)>1:
+            return ""
+        i,j = 0,0
+        res = []
+        while i<n and j<m:
+            if n<=m:
+                res.append(letter[j])
+                res.append(digit[i])
+            else:
+                res.append(digit[i])
+                res.append(letter[j])
+            i += 1
+            j += 1
+        if i<n:
+            res.append(digit[i])
+        if j<m:
+            res.append(letter[j])
+        return "".join(res)
---
+++ src/1401_1500/1418_display-table-of-food-orders-in-a-restaurant/display-table-of-food-orders-in-a-restaurant.py
@@ -0,0 +1,41 @@
+﻿class Solution(object):
+    def displayTable(self, orders):
+        """
+        :type orders: List[List[str]]
+        :rtype: List[List[str]]
+        """
+        s = set()
+        d = dict()
+        n = len(orders)
+        for arr in orders:
+            s.add(arr[2])
+            i = int(arr[1])
+            if i not in d:
+                d[i] = dict()
+                d[i][arr[2]] = 1
+            else:
+                if arr[2] in d[i]:
+                    d[i][arr[2]] += 1
+                else:
+                    d[i][arr[2]] = 1
+        res = []
+        foods = sorted(s)
+        first = ["Table"]
+        for f in foods:
+            first.append(f)
+        res.append(first)
+        for key in sorted(d.keys()):
+            tmp = [str(key)]
+            for f in foods:
+                if f not in d[key]:
+                    tmp.append("0")
+                else:
+                    tmp.append(str(d[key][f]))
+            res.append(tmp)
+        return res
+        
+
+
+
+
+
