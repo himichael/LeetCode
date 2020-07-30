42f39e4580aaea7a7949be692df3c4e1eb8c78f5
michaelwng <michaelwangg@qq.com>
2020-07-30 13:55:50 +0800

    增加两种实现

--- src/301_400/0343_integer-break/integer-break.py
+++ src/301_400/0343_integer-break/integer-break.py
@@ -15,3 +15,35 @@
             print dp
         return dp[-1]
         
+		
+		
+	# DFS实现
+    def integerBreak(self, n):
+        def dfs(n):
+            if n<=2:
+                return 1
+            res = 0
+            for i in xrange(1,n):
+                res = max( res, i*(n-i), i*dfs(n-i) )
+            return res
+        return dfs(n)
+		
+		
+		
+	# DFS+记忆化	
+    def integerBreak(self, n):
+        d = dict()
+        def dfs(n):
+            if n in d:
+                return d[n]
+            if n<=2:
+                d[2] = 1
+                return 1
+            res = 0
+            for i in xrange(1,n):
+                res = max( res, i*(n-i), i*dfs(n-i) )
+            d[n] = res
+            return res
+        return dfs(n)
+		
+		
