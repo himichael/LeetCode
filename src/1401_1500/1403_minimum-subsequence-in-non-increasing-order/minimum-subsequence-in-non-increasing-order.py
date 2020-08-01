3e832e1d0efdd635fa18f8d0937d5ae2af5dfcac
michaelwng <michaelwangg@qq.com>
2020-08-01 18:16:15 +0800

    增加183场周赛的两道题

---
+++ src/1401_1500/1403_minimum-subsequence-in-non-increasing-order/minimum-subsequence-in-non-increasing-order.py
@@ -0,0 +1,16 @@
+﻿class Solution(object):
+    def minSubsequence(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        nums = sorted(nums)
+        total = sum(nums)
+        i = len(nums)-1
+        save = 0
+        res = []
+        while i>=0 and save*2<=total:
+            res.append(nums[i])
+            save += nums[i]
+            i -= 1
+        return res
---
+++ src/1401_1500/1404_number-of-steps-to-reduce-a-number-in-binary-representation-to-one/number-of-steps-to-reduce-a-number-in-binary-representation-to-one.py
@@ -0,0 +1,16 @@
+﻿class Solution(object):
+    def numSteps(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        n = int(s,2)
+        res = 0
+        while n!=1:
+            if n&1==1:
+                n += 1
+                res += 1
+            else:
+                n //= 2
+                res += 1
+        return res
