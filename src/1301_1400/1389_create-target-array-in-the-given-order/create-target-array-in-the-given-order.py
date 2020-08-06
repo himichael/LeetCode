0af1ec383250c4a18b473896a792a86454c3be14
michaelwng <michaelwangg@qq.com>
2020-08-06 21:52:31 +0800

    增加第181场周赛的两道题目

---
+++ src/1301_1400/1389_create-target-array-in-the-given-order/create-target-array-in-the-given-order.py
@@ -0,0 +1,8 @@
+﻿class Solution(object):
+    def createTargetArray(self, nums, index):
+        res = []
+        n = len(nums)
+        for i in xrange(n):
+            res.insert(index[i],nums[i])
+        return res
+    
---
+++ src/1301_1400/1390_four-divisors/four-divisors.java
@@ -0,0 +1,26 @@
+﻿class Solution {
+    public int sumFourDivisors(int[] nums) {
+    	int res = 0;
+    	for(int n : nums) {
+    		if(n==1) {
+    			continue;
+    		}
+    		ArrayList<Integer> list = new ArrayList<Integer>();
+    		list.add(1);
+    		int size = (n/2)+1;
+    		for(int j=2;j<size;++j) {
+    			if(n%j==0) {
+    				list.add(j);
+    			}
+    			if(list.size()>4) {
+    				break;
+    			}
+    		}
+    		list.add(n);
+    		if(list.size()==4) {
+    			res += list.get(0)+list.get(1)+list.get(2)+list.get(3);
+    		}
+    	}
+    	return res;
+    }
+}
