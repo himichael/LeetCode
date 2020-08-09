0cf1a2b626c7651b04e95448d3dc2f8cdcf4b1f3
michaelwng <michaelwangg@qq.com>
2020-08-09 12:13:39 +0800

    增加8月打卡题目，复原IP地址

--- src/1_100/0093_restore-ip-addresses/restore-ip-addresses.py
+++ src/1_100/0093_restore-ip-addresses/restore-ip-addresses.py
@@ -27,3 +27,36 @@
                     recursion(level+1, index+j, arr+[x])
         recursion(0,0,[])
         return res
+		
+	
+	
+	# 循环实现
+	def restoreIpAddresses(self, s):
+		if not s:
+			return []
+		n = len(s)
+		i,j,k = 1,1,1
+		res = []
+		def check(s):
+			if not s:
+				return False
+			num = int(s)
+			if len(s)>1 and s.startswith("0"):
+				return False
+			if 0<=num<=255:
+				return True
+		while i<n and i<=3:
+			j = i
+			while j<n and j<=i+3:
+				k = j
+				while k<n and k<=j+3:
+					s1 = s[:i]
+					s2 = s[i:j]
+					s3 = s[j:k]
+					s4 = s[k:]
+					if check(s1) and check(s2) and check(s3) and check(s4):
+						res.append(s1+"."+s2+"."+s3+"."+s4)
+					k += 1
+				j += 1
+			i += 1
+		return res
