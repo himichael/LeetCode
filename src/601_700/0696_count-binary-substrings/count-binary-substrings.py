de0b9941a938a844cc3ac9f20f79a8a2870e8432
michaelwng <michaelwangg@qq.com>
2020-08-10 11:09:15 +0800

    增加8月打卡题目

---
+++ src/601_700/0696_count-binary-substrings/count-binary-substrings.py
@@ -0,0 +1,22 @@
+﻿class Solution(object):
+    def countBinarySubstrings(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        if not s:
+            return 0
+        n = len(s)
+        arr = []
+        i = 0
+        res = 0
+        while i<n:
+            c = s[i]
+            count = 0
+            while i<n and s[i]==c:
+                i += 1
+                count += 1
+            arr.append(count)
+        for i in xrange(1,len(arr)):
+            res += min(arr[i],arr[i-1])
+        return res
