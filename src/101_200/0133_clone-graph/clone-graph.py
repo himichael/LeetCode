2a639d36b77947a62e1d486aea38f1b84e66e51e
michaelwng <michaelwangg@qq.com>
2020-08-12 10:43:27 +0800

    增加8月打卡题目，克隆图

---
+++ src/101_200/0133_clone-graph/clone-graph.py
@@ -0,0 +1,19 @@
+﻿class Solution(object):
+	def cloneGraph(self, node):
+		"""
+		:type node: Node
+		:rtype: Node
+		"""
+		if not node:
+			return None
+		d = dict()
+		def dfs(node):
+			if not node:
+				return
+			if node in d:
+				return d[node]
+			d[node] = Node(node.val,[])
+			for i in node.neighbors:
+				d[node].neighbors.append( dfs(i) )
+			return d[node]
+		return dfs(node)
