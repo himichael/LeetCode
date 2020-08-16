4632a040773bdbf6c43e5168543391b6b88820af
michaelwng <michaelwangg@qq.com>
2020-08-16 12:22:37 +0800

    增加8月打卡题目，图像渲染

---
+++ src/701_800/0733_flood-fill/flood-fill.py
@@ -0,0 +1,24 @@
+﻿class Solution(object):
+    def floodFill(self, image, sr, sc, newColor):
+        """
+        :type image: List[List[int]]
+        :type sr: int
+        :type sc: int
+        :type newColor: int
+        :rtype: List[List[int]]
+        """
+        if not image:
+            return image
+        n = len(image)
+        m = len(image[0])
+        val = image[sr][sc]
+        def dfs(i,j):
+            if 0<=i<n and 0<=j<m and image[i][j]==val:
+                image[i][j] = newColor
+                dfs(i,j+1)
+                dfs(i,j-1)
+                dfs(i+1,j)
+                dfs(i-1,j)
+        if newColor!=val:
+            dfs(sr,sc)
+        return image
