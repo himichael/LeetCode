ff0556fffa84a9eb612791cb50005465be81b18a
michaelwng <michaelwangg@qq.com>
2020-08-19 09:20:21 +0800

    增加8月打卡题目，回文子串

---
+++ src/601_700/0647_palindromic-substrings/palindromic-substrings.py
@@ -0,0 +1,21 @@
+﻿class Solution(object):
+	def countSubstrings(self, s):
+		"""
+		:type s: str
+		:rtype: int
+		"""
+		if not s:
+			return 0
+		n = len(s)
+		res = 0
+		def check(i,j):
+			cnt = 0
+			while i>=0 and j<n and s[i]==s[j]:
+				i -= 1
+				j += 1
+				cnt += 1
+			return cnt
+		for i in xrange(n):
+			res += check(i,i)
+			res += check(i,i+1)
+		return res
