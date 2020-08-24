e85918c7fb59ecda2b7a3134c95504cd1e47af6a
michaelwng <michaelwangg@qq.com>
2020-08-24 21:18:03 +0800

    第201题增加一种解法

--- src/201_300/0201_bitwise-and-of-numbers-range/bitwise-and-of-numbers-range.py
+++ src/201_300/0201_bitwise-and-of-numbers-range/bitwise-and-of-numbers-range.py
@@ -5,3 +5,13 @@
             n = n & (n - 1)
         return n
 
+
+
+	# 求前缀
+    def rangeBitwiseAnd(self, m,n):
+        cnt = 0
+        while m!=n:
+            m >>= 1
+            n >>= 1
+            cnt += 1
+        return n << cnt
