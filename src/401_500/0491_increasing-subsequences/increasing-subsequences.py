7042ce01205021da0d85b0864bc441a9c56fb6d8
michaelwng <michaelwangg@qq.com>
2020-08-25 20:42:35 +0800

    增加8月打卡题目，递增子序列

---
+++ src/401_500/0491_increasing-subsequences/increasing-subsequences.py
@@ -0,0 +1,23 @@
+﻿class Solution(object):
+    def findSubsequences(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        res = []
+        tmp = []
+        n = len(nums)
+        def dfs(index,last):
+            if index==n:
+                if len(tmp)>=2:
+                    res.append(list(tmp))
+                return
+            if nums[index]>=last:
+                tmp.append(nums[index])
+                dfs(index+1,nums[index])
+                tmp.pop()
+            if nums[index]!=last:
+                dfs(index+1,last)
+        dfs(0,float("-inf"))
+        return res
+
