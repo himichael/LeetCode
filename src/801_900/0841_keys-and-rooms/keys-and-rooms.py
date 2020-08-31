4834e14c0f16177b168577b85c282da621a6629f
michaelwng <michaelwangg@qq.com>
2020-08-31 09:58:42 +0800

    增加8月打卡题目

---
+++ src/801_900/0841_keys-and-rooms/keys-and-rooms.py
@@ -0,0 +1,18 @@
+﻿class Solution(object):
+    def canVisitAllRooms(self, rooms):
+        """
+        :type rooms: List[List[int]]
+        :rtype: bool
+        """
+        n = len(rooms)
+        visit = set()
+        def dfs(index):
+            visit.add(index)
+            for i in rooms[index]:
+                if 0<=i<n and i not in visit:
+                    dfs(i)
+        dfs(0)
+        for i in xrange(0,n):
+            if i not in visit:
+                return False
+        return True
