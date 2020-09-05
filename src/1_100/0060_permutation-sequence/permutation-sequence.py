c381d1c533031b9e420e90492980ceea041328b6
michaelwng <michaelwangg@qq.com>
2020-09-05 10:32:01 +0800

    增加9月打卡题目

---
+++ src/1_100/0060_permutation-sequence/permutation-sequence.py
@@ -0,0 +1,20 @@
+﻿class Solution:
+    def getPermutation(self, n, k):
+        factorial = [1]
+        for i in range(1, n):
+            factorial.append(factorial[-1] * i)
+        
+        k -= 1
+        ans = list()
+        valid = [1] * (n + 1)
+        for i in range(1, n + 1):
+            order = k // factorial[n - i] + 1
+            for j in range(1, n + 1):
+                order -= valid[j]
+                if order == 0:
+                    ans.append(str(j))
+                    valid[j] = 0
+                    break
+            k %= factorial[n - i]
+
+        return "".join(ans)
---
+++ src/401_500/0486_predict-the-winner/predict-the-winner.py
@@ -0,0 +1,24 @@
+﻿class Solution(object):
+    def PredictTheWinner(self, nums):
+        n = len(nums)
+        def dfs(i,j):
+            if i==j:
+                return nums[i]
+            left = nums[i] - dfs(i+1,j)
+            right = nums[j] - dfs(i,j-1)
+            return max(left,right)
+        return dfs(0,n-1)>=0
+		
+
+
+    def PredictTheWinner(self, nums):
+        n = len(nums)
+        dp = [[0 for _ in xrange(n)] for _ in xrange(n)]
+        for i in xrange(n):
+            dp[i][i] = nums[i]
+        for i in xrange(n-2,-1,-1):
+            for j in xrange(i+1,n):
+                a = nums[i] - dp[i+1][j]
+                b = nums[j] - dp[i][j-1]
+                dp[i][j] = max(a,b)
+        return dp[0][-1]>=0
