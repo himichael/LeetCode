4733a5eb1e68883f3b71399cd8b068420fd944c2
michael <michaelwangg@qq.com>
2020-09-10 12:54:54 +0800

    增加9月打卡题目

---
+++ src/1_100/0040_combination-sum-ii/combination-sum-ii.py
@@ -0,0 +1,21 @@
+﻿class Solution(object):
+    def combinationSum2(self, candidates, target):
+        if not candidates:
+            return []
+        res = []
+        n = len(candidates)
+        candidates = sorted(candidates)
+        def dfs(index,arr,num):
+            if num>target or index>n:
+                return
+            if num==target:
+                res.append(arr)
+                return
+            if index<n and (candidates[index]>target or num+candidates[index]>target):
+                return
+            for i in xrange(index,n):
+                if i>index and candidates[i]==candidates[i-1]:
+                    continue
+                dfs(i+1,arr+[candidates[i]],num+candidates[i])
+        dfs(0,[],0)
+        return res
