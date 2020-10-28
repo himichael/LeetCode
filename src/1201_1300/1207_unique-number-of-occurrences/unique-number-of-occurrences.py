9a667dc95ffa02e8632fb0c08459a0d689e9cb9a
michaelwng <michaelwangg@qq.com>
2020-10-28 08:46:34 +0800

    增加一题

---
+++ src/1201_1300/1207_unique-number-of-occurrences/unique-number-of-occurrences.py
@@ -0,0 +1,13 @@
+﻿class Solution(object):
+    def uniqueOccurrences(self, arr):
+        if not arr:
+            return True
+        d1 = dict()
+        for i in arr:
+            d1[i] = d1.setdefault(i,0)+1
+        s = set()
+        for k in d1:
+            s.add(d1[k])
+        return len(s)==len(d1)
+
+
