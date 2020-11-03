7735e1082b5607c0f63a38a8fb5c86964e70a30c
michaelwng <michaelwangg@qq.com>
2020-11-03 08:39:56 +0800

    增加一题，【有效的山脉数组】

---
+++ src/901_1000/0941_valid-mountain-array/valid-mountain-array.py
@@ -0,0 +1,9 @@
+﻿class Solution(object):
+    def validMountainArray(self, A):
+        if not A:
+            return False
+        N = len(A)
+        if N<3:
+            return False
+        i = 0
+        while i<N and A[i]
