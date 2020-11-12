60a3c6ed7aa68593de242e4eadb0019ba57cf2df
michaelwng <michaelwangg@qq.com>
2020-11-12 08:49:14 +0800

    增加两道题

---
+++ src/901_1000/0922_sort-array-by-parity-ii/sort-array-by-parity-ii.py
@@ -0,0 +1,17 @@
+﻿class Solution(object):
+    def sortArrayByParityII(self, A):
+        if not A:
+            return []
+        n = len(A)
+        res = [0] * n
+        i = 0
+        for x in A:
+            if x%2==0:
+                res[i] = x
+                i += 2
+        i = 1
+        for x in A:
+            if x%2==1:
+                res[i] = x
+                i += 2
+        return res
