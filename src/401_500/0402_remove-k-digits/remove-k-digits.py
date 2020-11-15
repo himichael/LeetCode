54cd2320fd5c4d2cca68e5583f3e356094899c38
michaelwng <michaelwangg@qq.com>
2020-11-15 12:29:09 +0800

    增加两道题

---
+++ src/401_500/0402_remove-k-digits/remove-k-digits.py
@@ -0,0 +1,10 @@
+﻿class Solution(object):
+    def removeKdigits(self, num, k):
+        stack = []
+        remain = len(num) - k
+        for i in num:
+            while k>0 and stack and stack[-1]>i:
+                stack.pop()
+                k -= 1
+            stack.append(i)
+        return "".join(stack[:remain]).lstrip("0") or '0'
