bfac73803a8147d71dcbb4b813a3ff921914a709
michaelwng <michaelwangg@qq.com>
2020-11-29 10:43:47 +0800

    增加两道题，493、976

---
+++ src/901_1000/0976_largest-perimeter-triangle/largest-perimeter-triangle.cpp
@@ -0,0 +1,12 @@
+﻿class Solution {
+public:
+    int largestPerimeter(vector<int>& A) {
+        sort(A.begin(),A.end());
+        for(int i=(int)A.size()-1;i>=2;--i) {
+            if(A[i - 2] + A[i - 1] > A[i]) {
+                return A[i - 2] + A[i - 1] + A[i];
+            }
+        }
+        return 0;
+    }
+};
