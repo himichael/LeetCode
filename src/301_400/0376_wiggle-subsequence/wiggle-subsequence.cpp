0faa7590ba44271f8757ee602cc447099c07cc10
michaelwng <michaelwangg@qq.com>
2020-12-12 10:56:13 +0800

    增加一题，376题

---
+++ src/301_400/0376_wiggle-subsequence/wiggle-subsequence.cpp
@@ -0,0 +1,19 @@
+﻿class Solution {
+public:
+    int wiggleMaxLength(vector<int>& nums) {
+        int n = nums.size();
+        if(n < 2) {
+            return n;
+        }
+        int pre_diff = nums[1] - nums[0];
+        int res = pre_diff != 0 ? 2 : 1;
+        for(int i = 2; i < n; ++i) {
+            int diff = nums[i] - nums[i - 1];
+            if((diff > 0 && pre_diff <= 0) || (diff < 0 && pre_diff >= 0)) {
+                ++res;
+                pre_diff = diff;
+            }
+        }
+        return res;
+    }
+};
