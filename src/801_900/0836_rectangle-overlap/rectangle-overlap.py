c9b18b7f922c3f3d6ba5d0b7284efc72eb9cb049
michaelwng <michaelwangg@qq.com>
2020-12-20 22:41:59 +0800

    第836题的测试用例变了，原题解不能通过，增加新实现

--- src/801_900/0836_rectangle-overlap/rectangle-overlap.py
+++ src/801_900/0836_rectangle-overlap/rectangle-overlap.py
@@ -18,3 +18,16 @@
         if rec2[0] >= rec1[2] or rec2[2] <= rec1[0] or rec2[1] >= rec1[3] or rec2[3] <= rec1[1]: 
             return False
         return True
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
