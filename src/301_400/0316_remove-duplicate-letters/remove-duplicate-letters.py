095bdaea04fe3b6dcf4b480e5e3c4865196d3cb6
michaelwng <michaelwangg@qq.com>
2020-12-20 12:45:27 +0800

    增加12月打卡题目，去除重复字母

---
+++ src/301_400/0316_remove-duplicate-letters/remove-duplicate-letters.py
@@ -0,0 +1,13 @@
+﻿class Solution:
+    def removeDuplicateLetters(self, s):
+        stack = []
+        remain_counter = collections.Counter(s)
+
+        for c in s:
+            if c not in stack:
+                while stack and c < stack[-1] and  remain_counter[stack[-1]] > 0:
+                    stack.pop()
+                stack.append(c)
+            remain_counter[c] -= 1
+        return ''.join(stack)
+ 
