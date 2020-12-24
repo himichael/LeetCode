2ce111e19a9ba80b6ff2ab0daec9da903af3fdd1
michaelwng <michaelwangg@qq.com>
2020-12-24 21:16:18 +0800

    增加一题，【135-分发糖果】

---
+++ src/101_200/0135_candy/candy.py
@@ -0,0 +1,21 @@
+﻿class Solution(object):
+    def candy(self, ratings):
+        if not ratings:
+            return 0
+        n = len(ratings)
+        dp = [0] * n
+        left = 0
+        right = 1
+        res = 0
+        for i in xrange(0, n):
+            if i > 0 and ratings[i] > ratings[i - 1]:
+                dp[i] = dp[i - 1] + 1
+            else:
+                dp[i] = 1
+        for i in xrange(n - 1, -1, -1):
+            if i < n - 1 and ratings[i] > ratings[i + 1]:
+                right += 1
+            else:
+                right = 1
+            res += max(dp[i], right)
+        return res
