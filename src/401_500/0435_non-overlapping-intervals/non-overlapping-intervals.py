ad7ae8be331880091c209f1d6e8555ccd5900480
michaelwng <michaelwangg@qq.com>
2020-12-31 08:36:00 +0800

    增加12月打卡题目，无重叠区间

---
+++ src/401_500/0435_non-overlapping-intervals/non-overlapping-intervals.py
@@ -0,0 +1,13 @@
+﻿class Solution(object):
+    def eraseOverlapIntervals(self, intervals):
+        if not intervals:
+            return 0
+        intervals = sorted(intervals, key=lambda x:x[1])
+        n = len(intervals)
+        ans = 1
+        right = intervals[0][1]
+        for i in xrange(1,n):
+            if right <= intervals[i][0]:
+                right = intervals[i][1]
+                ans += 1
+        return n - ans
