b4d4c1dfbc39eaf71a49af049736d4e74fc37aeb
michaelwng <michaelwangg@qq.com>
2021-01-02 21:10:35 +0800

    增加一道hard题，滑动窗口最大值

---
+++ src/201_300/0239_sliding-window-maximum/sliding-window-maximum.py
@@ -0,0 +1,19 @@
+﻿class Solution(object):
+	def maxSlidingWindow(self, nums, k):
+		if not nums or k <= 0:
+			return []
+		q = collections.deque()
+		n = len(nums)
+		for i in xrange(k):
+			while q and nums[q[-1]] <= nums[i]:
+				q.pop()
+			q.append(i)
+		ans = [nums[q[0]]]
+		for i in xrange(k, n):
+			while q and nums[q[-1]] <= nums[i]:
+				q.pop()
+			q.append(i)
+			while q[0] <= i - k:
+				q.popleft()
+			ans.append(nums[q[0]])
+		return ans
