27931a9669816f7b734a36827953d351eedeba47
michaelwng <michaelwangg@qq.com>
2021-02-27 10:35:03 +0800

    增加第995题

---
+++ src/901_1000/0995_minimum-number-of-k-consecutive-bit-flips/minimum-number-of-k-consecutive-bit-flips.py
@@ -0,0 +1,15 @@
+﻿class Solution(object):
+    def minKBitFlips(self, A, K):
+        n = len(A)
+        dp = [0] * (n + 1)
+        cnt = 0
+        res = 0
+        for i in xrange(n):
+            cnt += dp[i]
+            if (cnt + A[i]) % 2 == 0:
+                if i + K > n:
+                    return -1
+                dp[i + K] -= 1
+                cnt += 1
+                res += 1
+        return res
