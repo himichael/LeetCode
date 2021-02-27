40a51e61bc14c07e3a50663004cb71a7df2beea3
michaelwng <michaelwangg@qq.com>
2021-02-27 20:39:48 +0800

    第91题，增加一种解法

--- src/1_100/0091_decode-ways/decode-ways.py
+++ src/1_100/0091_decode-ways/decode-ways.py
@@ -15,3 +15,31 @@
             d[index] = a + b
             return d[index]
         return f(0)
+		
+		
+    def numDecodings(self, s):    
+        if not s or s[0] == '0':
+            return 0
+        if len(s) == 1:
+            return 0 if s[0] == '0' else 1
+        dp = [0] * len(s)
+        dp[0] = 1
+        if s[1] == '0':
+            if 10 <= int(s[:2]) <= 26:
+                dp[1] = 1
+            else:
+                return 0
+        else:
+            dp[1] = 2 if 10 <= int(s[:2]) <= 26 else 1
+        for i in range(2, len(s)):
+            if s[i] == '0':
+                if s[i - 1] != '0' and 10 <= int(s[i - 1] + s[i]) <= 26:
+                    dp[i] = dp[i - 2]
+                else:
+                    return 0
+            else:
+                if s[i - 1] != '0' and 10 <= int(s[i - 1] + s[i]) <= 26:
+                    dp[i] = dp[i - 1] + dp[i - 2]
+                else:
+                    dp[i] = dp[i - 1]
+        return dp[-1]
