1f4fca16cf63e65a613181ad50edf5bb6f98e31f
michaelwng <michaelwangg@qq.com>
2021-02-27 10:44:49 +0800

    增加第862题

---
+++ src/801_900/0862_shortest-subarray-with-sum-at-least-k/shortest-subarray-with-sum-at-least-k.py
@@ -0,0 +1,14 @@
+﻿class Solution(object):
+    def shortestSubarray(self, A, K):
+        p = [0]
+        for x in A:
+            p.append(p[-1] + x)
+        queue = collections.deque()
+        res = float("inf")
+        for i in xrange(len(p)):
+            while queue and p[queue[-1]] >= p[i]:
+                queue.pop()
+            while queue and p[i] - p[queue[0]] >= K:
+                res = min(res, i - queue.popleft())
+            queue.append(i)
+        return res if res <= len(A) else -1
