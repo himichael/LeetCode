0cba86fc79f8b660acb04584c8227d50541c17cb
michaelwng <michaelwangg@qq.com>
2021-02-27 10:47:12 +0800

    增加第91题

---
+++ src/1_100/0091_decode-ways/decode-ways.py
@@ -0,0 +1,17 @@
+﻿class Solution(object):
+    def numDecodings(self, s):
+        n = len(s)
+        d = {}
+        def f(index):
+            if index >= n:
+                return 1
+            if index in d:
+                return d[index]
+            a,b = 0,0
+            if s[index] != '0':
+                a = f(index + 1)
+            if index + 2 <= n and s[index] != '0' and int(s[index : index + 2]) <= 26:
+                b = f(index + 2)
+            d[index] = a + b
+            return d[index]
+        return f(0)
