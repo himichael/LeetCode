608e0621b2c256f3bb9466aaacc9cfb8c6c23a92
michael <michaelwangg@qq.com>
2021-03-10 12:34:18 +0800

    增加 354题

---
+++ src/301_400/0354_russian-doll-envelopes/russian-doll-envelopes.py
@@ -0,0 +1,22 @@
+﻿class Solution(object):
+    def maxEnvelopes(self, envelopes):
+        s = sorted(envelopes, key = lambda x : (x[0], -x[1]))
+        n = len(s)
+        dp = []
+        for i in range(n):
+            target = s[i][1]
+            if not dp or dp[-1] < target:
+                dp.append(target)
+                continue
+            begin, end = 0, len(dp) - 1
+            while begin <= end:
+                mid = begin + (end - begin) // 2
+                if dp[mid] > target:
+                    end = mid - 1
+                elif dp[mid] < target:
+                    begin = mid + 1
+                else:
+                    begin = mid
+                    break
+            dp[begin] = target
+        return len(dp)
