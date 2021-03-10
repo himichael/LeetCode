04accbc5ebfa67a42ff33bec09d45062c8186c1a
michael <michaelwangg@qq.com>
2021-03-10 12:30:51 +0800

    增加303、304题

---
+++ src/301_400/0303_range-sum-query-immutable/range-sum-query-immutable.py
@@ -0,0 +1,18 @@
+﻿class NumArray(object):
+
+    def __init__(self, nums):
+        n = len(nums)
+        self.dp = [0] * (n + 1)
+        for i in range(n):
+            self.dp[i + 1] = self.dp[i] + nums[i]
+
+
+    def sumRange(self, i, j):
+        return self.dp[j + 1] - self.dp[i]
+
+
+
+
+# Your NumArray object will be instantiated and called as such:
+# obj = NumArray(nums)
+# param_1 = obj.sumRange(i,j)
---
+++ src/301_400/0304_range-sum-query-2d-immutable/range-sum-query-2d-immutable.py
@@ -0,0 +1,20 @@
+﻿class NumMatrix(object):
+
+    def __init__(self, matrix):
+        m = len(matrix)
+        n = len(matrix[0]) if matrix else 0
+        self.dp = [[0] * (n + 1) for _ in range(m + 1)]
+        d = self.dp
+        for i in range(m):
+            for j in range(n):
+                d[i + 1][j + 1] = d[i + 1][j] + d[i][j + 1] - d[i][j] + matrix[i][j]
+ 
+
+    def sumRegion(self, row1, col1, row2, col2):
+        d = self.dp
+        return d[row2 + 1][col2 + 1] - d[row1][col2 + 1] - d[row2 + 1][col1] + d[row1][col1]
+
+
+# Your NumMatrix object will be instantiated and called as such:
+# obj = NumMatrix(matrix)
+# param_1 = obj.sumRegion(row1,col1,row2,col2)
