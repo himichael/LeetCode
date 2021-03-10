12c8f07e1d84ff58f1afa2b6facd310d972ef56f
michael <michaelwangg@qq.com>
2021-03-10 12:43:57 +0800

    增加 224 题

---
+++ src/201_300/0224_basic-calculator/basic-calculator.java
@@ -0,0 +1,41 @@
+﻿class Solution {
+    public int calculate(String s) {
+        Deque<Integer> stack = new LinkedList<Integer>();
+        stack.push(1);
+        int res = 0;
+        int i = 0;
+		int sign = 1;
+        int n = s.length();
+        while(i < n) {
+			char c = s.charAt(i);
+            if(c == ' ') {
+				++i;
+			}
+			else if(c == '+') {
+				sign = stack.peek();
+				++i;
+			}
+			else if(c == '-') {
+				sign = -stack.peek();
+				++i;
+			}
+			else if(c == '(') {
+				stack.push(sign);
+				++i;
+			}
+			else if(c == ')') {
+				stack.pop();
+				++i;
+			}
+			else {
+				int tmp = 0;
+				while(i < n && Character.isDigit(s.charAt(i))) {
+					tmp = tmp * 10 + s.charAt(i) - '0';
+					++i;
+				}
+				res += sign * tmp;
+ 			}
+         }
+		 return res;
+    }
+}
