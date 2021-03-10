57058a31c7a4f1e9fdb26d10f7ebe44853929e8f
michael <michaelwangg@qq.com>
2021-03-10 12:38:34 +0800

    增加131、132题

---
+++ src/101_200/0131_palindrome-partitioning/palindrome-partitioning.py
@@ -0,0 +1,19 @@
+﻿class Solution(object):
+    def partition(self, s):
+        n = len(s)
+        dp = [[False] * n for _ in range(n)]
+        for i in range(n - 1, -1, -1):
+            dp[i][i] = True
+            for j in range(i + 1, n):
+                if s[i] == s[j] and (j - i <= 2 or dp[i + 1][j - 1]):
+                    dp[i][j] = True
+        res = []
+        def dfs(i, arr):
+            if i == n:
+                res.append(arr[:])
+                return
+            for j in range(i, n):
+                if dp[i][j]:
+                    dfs(j + 1, arr + [s[i : j + 1]])
+        dfs(0, [])
+        return res
---
+++ src/101_200/0132_palindrome-partitioning-ii/palindrome-partitioning-ii.py
@@ -0,0 +1,18 @@
+﻿class Solution(object):
+    def minCut(self, s):
+        n = len(s)
+        dp = [[False] * n for _ in range(n)]
+        for i in range(n - 1, -1, -1):
+            dp[i][i] = True
+            for j in range(i + 1, n):
+                if s[i] == s[j] and (j - i <= 2 or dp[i + 1][j - 1]):
+                    dp[i][j] = True
+        f = [float("inf")] * n
+        for i in range(n):
+            if dp[0][i]:
+                f[i] = 0
+            else:
+                for j in range(i):
+                    if dp[j + 1][i]:
+                        f[i] = min(f[i], f[j] + 1)
+        return f[-1]
