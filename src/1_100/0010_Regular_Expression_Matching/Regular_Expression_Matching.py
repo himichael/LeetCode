5c0d7a7fac56cca35128c921f7d888d8d33692d8
michael <michaelwangg@qq.com>
2021-03-16 18:44:27 +0800

    增加第10题的一种实现方式

--- src/1_100/0010_Regular_Expression_Matching/Regular_Expression_Matching.py
+++ src/1_100/0010_Regular_Expression_Matching/Regular_Expression_Matching.py
@@ -13,3 +13,25 @@
 		else:
 			return first_match and self.isMatch(text[1:],pattern[1:])
 			
+			
+	# 递归 + 记忆化，可以AC
+	# 如果使用字符串截取的方式，比如s[1:],p[1:]这种就会超时，必须用i,j下标的方式
+    def isMatch(self, s, p):
+        mem = {}
+        n = len(s)
+        m = len(p)
+        def f(i,j):
+            if (i,j) in mem:
+                return mem[i,j]
+            if i == n:
+                return j == m or (p[j] == "*" and f(i, j + 1))
+            if j == m:
+                return False
+            if p[j] == "*":
+                res =  f(i + 1, j) or f(i, j + 1)
+            else:
+                first = i < n and p[j] in (s[i], "?")
+                res = first and f(i + 1, j + 1)
+            mem[i,j] = res
+            return mem[i,j]
+        return f(0, 0)
