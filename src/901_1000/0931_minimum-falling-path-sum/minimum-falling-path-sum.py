8200df9328788e9cc555010cf6aecd0d5f58572c
michael <michaelwangg@qq.com>
2021-04-05 10:17:11 +0800

    增加第931题

---
+++ src/901_1000/0931_minimum-falling-path-sum/minimum-falling-path-sum.py
@@ -0,0 +1,17 @@
+﻿class Solution(object):
+    def minFallingPathSum(self, matrix):
+        n = len(matrix)
+        m = len(matrix[0])
+        dp = [[0] * m for _ in range(n)]
+        for j in range(m):
+            dp[0][j] = matrix[0][j]
+        for i in range(1, n):
+            for j in range(m):
+                dp[i][j] = dp[i - 1][j] + matrix[i][j]
+                if j - 1 >= 0:
+                    dp[i][j] = min(dp[i][j], dp[i - 1][j - 1] + matrix[i][j])
+                if j + 1 < m:
+                    dp[i][j] = min(dp[i][j], dp[i - 1][j + 1] + matrix[i][j])
+        return min(dp[-1])
+        
+        
