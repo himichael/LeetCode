692d412af1f8553d9cec7f384fab5356b24931a8
michael <michaelwangg@qq.com>
2021-04-05 18:53:34 +0800

    增加LCP题目，第30题

---
+++ src/LCP/0030/p0NxJO.py
@@ -0,0 +1,17 @@
+﻿class Solution(object):
+    def magicTower(self, nums):
+        if sum(nums) < 0:
+            return -1
+        import heapq
+        n = len(nums)
+        arr = []
+        total = 1
+        ans = 0
+        for i in range(n):
+            if nums[i] < 0:
+                heapq.heappush(arr, nums[i])
+            total += nums[i]
+            while total <= 0:
+                total += -heapq.heappop(arr)
+                ans += 1
+        return ans
