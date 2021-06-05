e5d398c8916fa9f4e550c050341e80c5a54d1f68
michael <michaelwangg@qq.com>
2021-06-05 17:39:44 +0800

    增加第 639题的 DP解法

--- src/601_700/0639_decode-ways-ii/decode-ways-ii.py
+++ src/601_700/0639_decode-ways-ii/decode-ways-ii.py
@@ -30,4 +30,38 @@
             return cache[i]
         return dfs(0)
 
+	
+
+
+	
+# 动态规划
+    def numDecodings(self, s):
+        if not s or s[0] == '0' or len(s) == 1:
+            return 0 if s[0] == '0' else (9 if s[0] == '*' else 1)
+        n = len(s)
+        M = 10 ** 9 + 7
+        dp = [0] * (n + 1)
+        dp[0] = 1
+        dp[1] = 9 if s[0] == '*' else (1 if s[0] != '0' else 0)
+        for i in range(1, n):
+            if s[i] == '*':
+                dp[i + 1] = 9 * dp[i] % M
+                if s[i - 1] == '*':
+                    dp[i + 1] = (dp[i + 1] + 15 * dp[i -1]) % M
+                elif s[i - 1] == '1':
+                    dp[i + 1] = (dp[i + 1] + 9 * dp[i - 1]) % M
+                elif s[i - 1] == '2':
+                    dp[i + 1] = (dp[i + 1] + 6 * dp[i - 1]) % M
+            else:
+                dp[i + 1] = dp[i] if s[i] != '0' else 0
+                if s[i - 1] == '*':
+                    if s[i] <= '6':
+                        dp[i + 1] = (dp[i + 1] + 2 * dp[i - 1]) % M
+                    elif s[i] > '6':
+                        dp[i + 1] = (dp[i + 1] + dp[i - 1]) % M
+                elif 10 <= int(s[i - 1] + s[i]) <= 26:
+                    dp[i + 1] = (dp[i + 1] + dp[i - 1]) % M
+        return dp[-1]
+		
+		
 
