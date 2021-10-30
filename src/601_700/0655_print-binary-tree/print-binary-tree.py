670d10d20e067f2d48442b5c687578ba57648b04
michael <michaelwangg@qq.com>
2021-10-30 12:46:10 +0800

    增加【输出二叉树】

---
+++ src/601_700/0655_print-binary-tree/print-binary-tree.py
@@ -0,0 +1,35 @@
+﻿# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def printTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[str]]
+        """
+        if not root:
+            return []
+        def getHigh(root):
+            if not root:
+                return 0
+            return max(getHigh(root.left), getHigh(root.right)) + 1
+
+        high = getHigh(root)
+        ans = [["" for _ in range(2 ** high -1)] for _ in range(high)]
+        def fill(root, i, left, right):
+            if not root:
+                return
+            ans[i][(left + right) / 2] = str(root.val)
+            fill(root.left, i + 1, left, (left + right) / 2)
+            fill(root.right, i + 1, (left + right + 1) / 2, right)
+        fill(root, 0, 0 , len(ans[0]))
+        return ans
+
+
+
+            
+
+
