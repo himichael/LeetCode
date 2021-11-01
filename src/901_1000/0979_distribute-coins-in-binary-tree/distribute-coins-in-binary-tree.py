1eb7fcd0ae9850e8267718fb8273082ae50b390d
michael <michaelwangg@qq.com>
2021-11-01 09:29:35 +0800

    增加【在二叉树上分片硬币】

---
+++ src/901_1000/0979_distribute-coins-in-binary-tree/distribute-coins-in-binary-tree.py
@@ -0,0 +1,18 @@
+﻿# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def distributeCoins(self, root):
+        self.ans = 0
+        def dfs(root):
+            if not root:
+                return 0
+            left = dfs(root.left)
+            right = dfs(root.right)
+            self.ans += abs(left) + abs(right)
+            return root.val + left + right - 1
+        dfs(root)
+        return self.ans
