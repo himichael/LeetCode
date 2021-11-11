aed355ff06f808a8c1b9cccca1fefeeec578256b
michael <michaelwangg@qq.com>
2021-11-11 12:32:56 +0800

    增加一题，【翻转等价二叉树】

---
+++ src/901_1000/0951_flip-equivalent-binary-trees/flip-equivalent-binary-trees.py
@@ -0,0 +1,23 @@
+﻿# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def flipEquiv(self, root1, root2):
+        """
+        :type root1: TreeNode
+        :type root2: TreeNode
+        :rtype: bool
+        """
+        def dfs(p, q):
+            if not p and not q:
+                return True
+            if not (p and q):
+                return False
+            if p.val != q.val:
+                return False
+            return (dfs(p.left, q.left) or dfs(p.left, q.right)) and \
+            (dfs(p.right, q.right) or dfs(p.right, q.left))
+        return dfs(root1, root2)
