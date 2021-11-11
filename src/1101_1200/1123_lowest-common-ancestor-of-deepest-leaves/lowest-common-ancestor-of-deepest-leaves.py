1f216316de3fe06ec199d1c22a05617e8cf9c910
michael <michaelwangg@qq.com>
2021-11-11 09:13:17 +0800

    增加一题，【最深叶节点的最近公共祖先】

---
+++ src/1101_1200/1123_lowest-common-ancestor-of-deepest-leaves/lowest-common-ancestor-of-deepest-leaves.py
@@ -0,0 +1,45 @@
+﻿# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def lcaDeepestLeaves(self, root):
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+        """
+        d = {None : -1}
+        def dfs(root, parent):
+            if not root:
+                return
+            d[root] = d[parent] + 1
+            dfs(root.left, root)
+            dfs(root.right, root)
+        dfs(root, None)
+        max_depth = max(d.itervalues())
+        def dfs2(root):
+            if not root or d[root] == max_depth:
+                return root
+            left = dfs2(root.left)
+            right = dfs2(root.right)
+            if left and right:
+                return root
+            if left:
+                return left
+            if right:
+                return right
+            return None
+        return dfs2(root)
+
+
+
+
+
+
+
+
+
+
+
