f9f4b85cdee9162c84714aa3182c60e358339fd0
michael <michaelwangg@qq.com>
2022-04-03 09:00:07 +0800

    增加一题

---
+++ src/201_300/0279_perfect-squares/perfect-squares.py
@@ -0,0 +1,14 @@
+﻿class Solution(object):
+    def numSquares(self, n):
+        if n <= 0:
+            return 0
+        dp = [0] * (n + 1)
+        dp[0] = 0
+        for i in range(1, n + 1):
+            tmp = 2 ** 31 - 1
+            j = 1
+            while j * j <= i:
+                tmp = min(tmp, dp[i - j * j] + 1)
+                j += 1
+            dp[i] = tmp
+        return dp[-1]
